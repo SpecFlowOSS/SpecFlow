94a93bd1eeb15aa527704d25691eb0c2da542074
Gaspar Nagy <gaspar.nagy@gmail.com>
2009-11-11 15:22:00 +0100

    ﻿update version number
    small fixes in the installer

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
