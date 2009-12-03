0c0b268b7e538b25de1bba3bf71c25ed3bc18ca8
Xerxes Battiwalla <xerxesb@gmail.com>
2009-12-04 07:57:23 +1100

    Implemented Context injection so that contexts could be shared across multiple scenario implementations

---
+++ Tests/FeatureTests/ContextInjection/NestedContext.cs
@@ -0,0 +1,17 @@
+﻿namespace FeatureTests.ContextInjection
+{
+    public class NestedContext
+    {
+        private readonly SingleContext _context;
+
+        public NestedContext(SingleContext context)
+        {
+            _context = context;
+        }
+
+        public SingleContext TheNestedContext
+        {
+            get { return _context; }
+        }
+    }
+}
