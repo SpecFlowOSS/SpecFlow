c09925f5e6035f1128d4c79c43098340b212bf1e
jbandi <dev@jonasbandi.net>
2010-04-14 22:03:21 +0200

    Implementing feature "Calling steps from step definitions"

---
+++ Runtime/Steps.cs
@@ -0,0 +1,27 @@
+﻿namespace TechTalk.SpecFlow
+{
+    public abstract class Steps
+    {
+        private static ITestRunner _testRunner;
+
+        protected Steps()
+        {
+            _testRunner = TestRunnerManager.GetTestRunner();
+        }
+
+        public void Given(string step)
+        {
+            _testRunner.Given(step);
+        }   
+        
+        public void When(string step)
+        {
+            _testRunner.When(step);
+        }   
+
+        public void And(string step)
+        {
+            _testRunner.And(step);
+        }
+    }
+}
