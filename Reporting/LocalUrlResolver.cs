33c3d13f2da5c223ad04a7489cd9e40660835f5a
Gaspar Nagy <gaspar.nagy@gmail.com>
2010-05-18 09:37:36 +0200

    add todo for localurlresolver

--- Reporting/LocalUrlResolver.cs
+++ Reporting/LocalUrlResolver.cs
@@ -3,6 +3,7 @@ using System.Xml;
 
 namespace TechTalk.SpecFlow.Reporting
 {
+    //TODO: check whether this is needed + somehow make it possible that it can fall back to the resource resolver, if one uses the resource:// prefix.
     class LocalUrlResolver : XmlUrlResolver
     {
         public override Uri ResolveUri(Uri baseUri, string relativeUri)
