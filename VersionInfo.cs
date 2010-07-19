3667ea7fc53941905df0a8b8c34d3e35f54f5416
gaspar.nagy <gaspar.nagy@gmail.com>
2010-07-19 11:42:10 +0200

    update version numbers to 1.3.3

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.3.0")]
