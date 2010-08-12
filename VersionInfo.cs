07eb83e2c98b9ccbe573427c4154750ccc8aaaac
Dale Ragan <dale.ragan@sinesignal.com>
2010-08-11 21:05:56 -0400

    Release 1.3.5.1

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.5.0")]
-[assembly: AssemblyFileVersion("1.3.5.0")]
+[assembly: AssemblyVersion("1.3.5.1")]
+[assembly: AssemblyFileVersion("1.3.5.1")]
