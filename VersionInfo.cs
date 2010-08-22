7a58408db61e0d2aecb6648e2da2f2b2b4d82b5a
jbandi <dev@jonasbandi.net>
2010-08-22 23:22:51 +0200

    Increasing version numbers, updating changelog

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.5.2")]
-[assembly: AssemblyFileVersion("1.3.5.2")]
+[assembly: AssemblyVersion("1.3.5.3")]
+[assembly: AssemblyFileVersion("1.3.5.3")]
