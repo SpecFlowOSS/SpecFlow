999ce93f247f07797cde1e32f8c9becacbb64fe8
Jason Jarrett <jason@elegantcode.com>
2010-09-05 14:41:02 -0700

    Replaced the StepDefinitionSkeletonProvider with IStep...Provider and a CS, VB and Base classes to support generating steps in VB. - NOTE: the ObjectContainer class is currently hard coded to return the VB provider - TODO: make that either configurable or 'figure out what is right' automatically.

---
+++ Runtime/Tracing/IStepDefinitionSkeletonProvider.cs
@@ -0,0 +1,10 @@
+﻿using TechTalk.SpecFlow.Bindings;
+
+namespace TechTalk.SpecFlow.Tracing
+{
+    internal interface IStepDefinitionSkeletonProvider
+    {
+        string GetStepDefinitionSkeleton(StepArgs stepArgs);
+        string GetBindingClassSkeleton(string stepDefinitions);
+    }
+}
---
+++ Runtime/Tracing/StepDefinitionSkeletonProviderBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using TechTalk.SpecFlow.Bindings;
+
+namespace TechTalk.SpecFlow.Tracing
+{
+    internal abstract class StepDefinitionSkeletonProviderBase : IStepDefinitionSkeletonProvider
+    {
+        public const string CODEINDENT = "    ";
+        public abstract string GetStepDefinitionSkeleton(StepArgs stepArgs);
+        public abstract string GetBindingClassSkeleton(string stepDefinitions);
+
+        protected static string GetAttributeName(Type attributeType)
+        {
+            return attributeType.Name.Substring(0, attributeType.Name.Length - "Attribute".Length);
+        }
+
+        protected static string EscapeRegex(string text)
+        {
+            return Regex.Escape(text).Replace("\"", "\"\"").Replace("\\ ", " ");
+        }
+    }
+}
