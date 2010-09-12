b2cae8ad89f17490630b62d488d88b2cbc07a294
Darren Cauthon <darren@cauthon.com>
2010-09-11 19:51:52 -0500

    Add basic get and set generic methods.

---
+++ Runtime/DictionaryHelpers.cs
@@ -0,0 +1,17 @@
+﻿namespace TechTalk.SpecFlow
+{
+    public static class DictionaryHelpers
+    {
+        public static void Set<T>(this ScenarioContext scenarioContext, T data)
+        {
+            var id = typeof (T).ToString();
+            scenarioContext[id] = data;
+        }
+
+        public static T Get<T>(this ScenarioContext scenarioContext) where T : class
+        {
+            var id = typeof (T).ToString();
+            return scenarioContext[id] as T;
+        }
+    }
+}
