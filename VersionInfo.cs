57b2d92a0b0f72ceec4dba734d87dca7195be83e
Gaspar Nagy <gaspar.nagy@gmail.com>
2010-10-07 15:40:12 +0200

    Release 1.4.0

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.5.3")]
-[assembly: AssemblyFileVersion("1.3.5.3")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
