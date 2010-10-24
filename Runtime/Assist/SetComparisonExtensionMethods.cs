0d2295f6ae3239089552e33435222b21207c7e97
Darren Cauthon <darren@cauthon.com>
2010-10-24 18:05:36 -0500

    Refactor.

--- Runtime/Assist/SetComparisonExtensionMethods.cs
+++ Runtime/Assist/SetComparisonExtensionMethods.cs
@@ -7,52 +7,91 @@ namespace TechTalk.SpecFlow.Assist
     {
         public static void CompareToSet<T>(this Table table, IEnumerable<T> set)
         {
-            foreach (var id in table.Header)
-                if (typeof (T).GetProperties().Select(x => x.Name).Contains(id) == false)
-                    throw new ComparisonException("");
+            AssertThatAllColumnsInTheTableMatchToPropertiesOnTheType<T>(table);
 
-            if (set.Count() != table.Rows.Count())
-                throw new ComparisonException("");
+            AssertThatTheTableAndSetHaveTheSameNumberOfRows(table, set);
 
-            if (set.Count() == 0)
+            if (ThereAreNoItems(set))
                 return;
 
-            var tableItems = table.CreateSet<T>().ToList();
+            var expectedItems = GetTheExpectedItems<T>(table);
+            var actualItems = GetTheActualItems(set);
 
-            var setItems = set.ToList();
-
-            for (var index = 0; index < tableItems.Count(); index++)
+            for (var expectedItemIndex = 0; expectedItemIndex < expectedItems.Count(); expectedItemIndex++)
             {
                 var successfulCheck = false;
                 var indexOfItemToRemove = -1;
-                for(var secondIndex = 0; secondIndex < setItems.Count(); secondIndex++)
+                for(var actualItemIndex = 0; actualItemIndex < actualItems.Count(); actualItemIndex++)
                 {
-                    var failHit = false;
-                    foreach (var id in table.Header)
+                    var thisItemDoesNotMatchTheExpectedItem = false;
+                    foreach (var property in GetAllPropertiesToTest(table))
                     {
-                        var tableValue = tableItems[index].GetPropertyValue(id);
-                        var propertyValue = setItems[secondIndex].GetPropertyValue(id);
+                        var expectedValue = GetTheExpectedValue(expectedItemIndex, expectedItems, property);
+                        var actualValue = GetTheActualValue(actualItemIndex, actualItems, property);
 
-                        if (propertyValue != null && tableValue == null)
-                            failHit = true;
+                        if (TheseValuesDoNotMatch(actualValue, expectedValue))
+                            thisItemDoesNotMatchTheExpectedItem = true;
 
-                        if (propertyValue == null && tableValue != null)
-                            failHit = true;
-
-                        if (propertyValue != null && tableValue != null)
-                            if (propertyValue.ToString() != tableValue.ToString())
-                                failHit = true;
-
-                        if (failHit == false && indexOfItemToRemove == -1)
-                            indexOfItemToRemove = secondIndex;
+                        if (thisItemDoesNotMatchTheExpectedItem == false && indexOfItemToRemove == -1)
+                            indexOfItemToRemove = actualItemIndex;
                     }
-                    if (failHit == false)
+                    if (thisItemDoesNotMatchTheExpectedItem == false)
                         successfulCheck = true;
                 }
                 if (successfulCheck == false)
                     throw new ComparisonException("");
-                setItems.RemoveAt(indexOfItemToRemove);
+                actualItems.RemoveAt(indexOfItemToRemove);
             }
+        }
+
+        private static object GetTheActualValue<T>(int actualItemIndex, List<T> actualItems, string fieldToCheck)
+        {
+            return actualItems[actualItemIndex].GetPropertyValue(fieldToCheck);
+        }
+
+        private static object GetTheExpectedValue<T>(int expectedItemIndex, List<T> expectedItems, string fieldToCheck)
+        {
+            return expectedItems[expectedItemIndex].GetPropertyValue(fieldToCheck);
+        }
+
+        private static IEnumerable<string> GetAllPropertiesToTest(Table table)
+        {
+            return table.Header;
+        }
+
+        private static bool TheseValuesDoNotMatch(object actualValue, object expectedValue)
+        {
+            return (actualValue != null && expectedValue == null) || 
+                   (actualValue == null && expectedValue != null) ||
+                   ((actualValue != null && expectedValue != null) && (actualValue.ToString() != expectedValue.ToString()));
+        }
+
+        private static List<T> GetTheActualItems<T>(IEnumerable<T> set)
+        {
+            return set.ToList();
+        }
+
+        private static List<T> GetTheExpectedItems<T>(Table table)
+        {
+            return table.CreateSet<T>().ToList();
+        }
+
+        private static bool ThereAreNoItems<T>(IEnumerable<T> set)
+        {
+            return set.Count() == 0;
+        }
+
+        private static void AssertThatTheTableAndSetHaveTheSameNumberOfRows<T>(Table table, IEnumerable<T> set)
+        {
+            if (set.Count() != table.Rows.Count())
+                throw new ComparisonException("");
+        }
+
+        private static void AssertThatAllColumnsInTheTableMatchToPropertiesOnTheType<T>(Table table)
+        {
+            foreach (var id in table.Header)
+                if (typeof (T).GetProperties().Select(x => x.Name).Contains(id) == false)
+                    throw new ComparisonException("");
         }
     }
 }
