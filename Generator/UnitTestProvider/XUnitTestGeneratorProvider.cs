718f48e96a2f88e2a13be55e4d16c4f750fab0ef
KerryJ <kerry.jones@withThem.info>
2011-02-25 18:05:40 +0200

    The TheoryAttribute and the InlineDataAttribute should use the Xunit.Extensions namespace and not Xunit.

--- Generator/UnitTestProvider/XUnitTestGeneratorProvider.cs
+++ Generator/UnitTestProvider/XUnitTestGeneratorProvider.cs
@@ -13,8 +13,8 @@ namespace TechTalk.SpecFlow.Generator.UnitTestProvider
         private const string FEATURE_TITLE_PROPERTY_NAME = "FeatureTitle";
         private const string FACT_ATTRIBUTE = "Xunit.FactAttribute";
         private const string FACT_ATTRIBUTE_SKIP_PROPERTY_NAME = "Skip";
-        private const string THEORY_ATTRIBUTE = "Xunit.TheoryAttribute";
-        private const string INLINEDATA_ATTRIBUTE = "Xunit.InlineDataAttribute";
+        private const string THEORY_ATTRIBUTE = "Xunit.Extensions.TheoryAttribute";
+        private const string INLINEDATA_ATTRIBUTE = "Xunit.Extensions.InlineDataAttribute";
         private const string SKIP_REASON = "Ignored";
         private const string TRAIT_ATTRIBUTE = "Xunit.TraitAttribute";
         private const string IUSEFIXTURE_INTERFACE = "Xunit.IUseFixture";
