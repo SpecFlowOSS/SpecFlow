45e0541babd08ef63c6d641bdc99626a79fa263e
Darren Cauthon <darren@cauthon.com>
2011-04-22 07:09:11 -0500

    Move the project helper methods into their own file.

--- Runtime/Assist/TableExtensionMethods.cs
+++ Runtime/Assist/TableExtensionMethods.cs
@@ -34,26 +34,6 @@ namespace TechTalk.SpecFlow.Assist
             return list;
         }
 
-        public static IEnumerable<Projection<T>> ToProjection<T>(this IEnumerable<T> collection, Table table = null)
-        {
-            return new EnumerableProjection<T>(table, collection);
-        }
-
-        public static IEnumerable<Projection<T>> ToProjection<T>(this Table table)
-        {
-            return new EnumerableProjection<T>(table);
-        }
-
-        public static IEnumerable<Projection<T>> ToProjectionOfSet<T>(this Table table, IEnumerable<T> collection)
-        {
-            return new EnumerableProjection<T>(table);
-        }
-
-        public static IEnumerable<Projection<T>> ToProjectionOfInstance<T>(this Table table, T instance)
-        {
-            return new EnumerableProjection<T>(table);
-        }
-
         private static T CreateInstanceWithActivator<T>(Table table)
         {
             var instance = (T)Activator.CreateInstance(typeof(T));
