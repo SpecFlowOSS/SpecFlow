936ba69900bce89eff020a3a210d9d8375d3b0f7
Gaspar Nagy <gaspar.nagy@gmail.com>
2011-05-20 12:42:11 +0200

    SharpDevelop integration fixes

---
+++ IdeIntegration/SharpDevelop4Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TechTalk.SpecFlow.SharpDevelop4Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
