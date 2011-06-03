e2b1c922a9857952430d1d27290f73849d8a30b9
Darren Cauthon <darren@cauthon.com>
2011-06-03 14:24:14 -0500

    Refactor the new tests into their own test fixtures.

---
+++ Tests/RuntimeTests/AssistTests/TableHelperExtensionMethods/CreateSetHelperMethodTests_WithFunc.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using TechTalk.SpecFlow.Assist;
+using TechTalk.SpecFlow.RuntimeTests.AssistTests.ExampleEntities;
+using ObjectAssertExtensions = Should.ObjectAssertExtensions;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.TableHelperExtensionMethods
+{
+    [TestFixture]
+    public class CreateSetHelperMethodTests_WithFunc
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        }
+
+        [Test]
+        public void Uses_the_instance_creation_method_when_passed_one_row()
+        {
+            var table = new Table("FirstName");
+            table.AddRow("John");
+
+            var expectedPerson = new Person();
+            
+            var people = table.CreateSet(() => expectedPerson);
+
+            ObjectAssertExtensions.ShouldBeSameAs(people.Single(), expectedPerson);
+        }
+
+        [Test]
+        public void Calls_the_instance_creation_method_for_each_row()
+        {
+            var table = new Table("FirstName");
+            table.AddRow("one");
+            table.AddRow("two");
+
+            var first = new Person();
+            var second = new Person();
+
+            var queue = new Queue<Person>();
+            queue.Enqueue(first);
+            queue.Enqueue(second);
+
+            var people = table.CreateSet(queue.Dequeue);
+
+            ObjectAssertExtensions.ShouldEqual(people.Count(), 2);
+            ObjectAssertExtensions.ShouldBeSameAs(people.First(), first);
+            ObjectAssertExtensions.ShouldBeSameAs(people.Last(), second);
+        }
+
+        [Test]
+        public void Still_loads_the_instance_with_the_values_from_the_table()
+        {
+            var table = new Table("FirstName");
+            table.AddRow("John");
+            table.AddRow("Howard");
+
+            var john = new Person();
+            var howard = new Person();
+
+            var queue = new Queue<Person>();
+            queue.Enqueue(john);
+            queue.Enqueue(howard);
+
+            var people = table.CreateSet(queue.Dequeue);
+
+            ObjectAssertExtensions.ShouldEqual(people.First().FirstName, "John");
+            ObjectAssertExtensions.ShouldEqual(people.Last().FirstName, "Howard");
+        }
+    }
+}
