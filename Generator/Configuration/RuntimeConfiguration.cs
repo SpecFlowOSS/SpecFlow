59ba160b8d7b7843de153f8143b3a039dedcfab8
Gaspar Nagy <gaspar.nagy@gmail.com>
2011-08-23 16:01:24 +0200

    cleanup RuntimeConfigurationForGenerator

--- Generator/Configuration/RuntimeConfiguration.cs
+++ Generator/Configuration/RuntimeConfiguration.cs
@@ -9,52 +9,19 @@ using System.Reflection;
 
 namespace TechTalk.SpecFlow.Configuration
 {
-    //TODO: merge it with the real RuntimeConfiguration
+    /// <summary>
+    /// Represents the subset of runtime configuration that might be interesing for the tooling
+    /// </summary>
     public class RuntimeConfigurationForGenerator
     {
-        private List<string> _additionalStepAssemblies = new List<string>();
-
-        //language settings
-        public CultureInfo ToolLanguage { get; set; }
-        public CultureInfo BindingCulture { get; set; }
-
-        //unit test framework settings
-        //public Type RuntimeUnitTestProviderType { get; set; }
-
-        //runtime settings
-        public bool DetectAmbiguousMatches { get; set; }
-        public bool StopAtFirstError { get; set; }
-        public MissingOrPendingStepsOutcome MissingOrPendingStepsOutcome { get; set; }
-
-        //tracing settings
-        //public Type TraceListenerType { get; set; }
-        public bool TraceSuccessfulSteps { get; set; }
-        public bool TraceTimings { get; set; }
-        public TimeSpan MinTracedDuration { get; set; }
+        private readonly List<string> additionalStepAssemblies = new List<string>();
 
         public IEnumerable<string> AdditionalStepAssemblies
         {
             get
             {
-                return _additionalStepAssemblies;
+                return additionalStepAssemblies;
             }
-        }
-
-        public RuntimeConfigurationForGenerator()
-        {
-            ToolLanguage = CultureInfo.GetCultureInfo(ConfigDefaults.FeatureLanguage);
-            BindingCulture = null;
-
-            //SetUnitTestDefaultsByName(ConfigDefaults.UnitTestProviderName);
-
-            DetectAmbiguousMatches = ConfigDefaults.DetectAmbiguousMatches;
-            StopAtFirstError = ConfigDefaults.StopAtFirstError;
-            MissingOrPendingStepsOutcome = ConfigDefaults.MissingOrPendingStepsOutcome;
-
-            //TraceListenerType = typeof(DefaultListener);
-            TraceSuccessfulSteps = ConfigDefaults.TraceSuccessfulSteps;
-            TraceTimings = ConfigDefaults.TraceTimings;
-            MinTracedDuration = TimeSpan.Parse(ConfigDefaults.MinTracedDuration);
         }
 
         internal RuntimeConfigurationForGenerator UpdateFromConfigFile(ConfigurationSectionHandler configSection)
@@ -62,49 +29,11 @@ namespace TechTalk.SpecFlow.Configuration
             if (configSection == null) throw new ArgumentNullException("configSection");
 
             var config = this;
-            if (configSection.Language != null)
-            {
-                config.ToolLanguage = string.IsNullOrEmpty(configSection.Language.Tool) ?
-                    CultureInfo.GetCultureInfo(configSection.Language.Feature) :
-                    CultureInfo.GetCultureInfo(configSection.Language.Tool);
-            }
-
-            if (configSection.BindingCulture.ElementInformation.IsPresent)
-            {
-                config.BindingCulture = CultureInfo.GetCultureInfo(configSection.BindingCulture.Name);
-            }
-
-//            if (configSection.UnitTestProvider != null)
-//            {
-//                config.SetUnitTestDefaultsByName(configSection.UnitTestProvider.Name);
-//
-//                if (!string.IsNullOrEmpty(configSection.UnitTestProvider.RuntimeProvider))
-//                    config.RuntimeUnitTestProviderType = GetTypeConfig(configSection.UnitTestProvider.RuntimeProvider);
-//
-                //TODO: config.CheckUnitTestConfig();
-//            }
-
-            if (configSection.Runtime != null)
-            {
-                config.DetectAmbiguousMatches = configSection.Runtime.DetectAmbiguousMatches;
-                config.StopAtFirstError = configSection.Runtime.StopAtFirstError;
-                config.MissingOrPendingStepsOutcome = configSection.Runtime.MissingOrPendingStepsOutcome;
-            }
-
-            if (configSection.Trace != null)
-            {
-//                if (!string.IsNullOrEmpty(configSection.Trace.Listener))
-//                    config.TraceListenerType = GetTypeConfig(configSection.Trace.Listener);
-
-                config.TraceSuccessfulSteps = configSection.Trace.TraceSuccessfulSteps;
-                config.TraceTimings = configSection.Trace.TraceTimings;
-                config.MinTracedDuration = configSection.Trace.MinTracedDuration;
-            }
 
             foreach (var element in configSection.StepAssemblies)
             {
                 var stepAssembly = ((StepAssemblyConfigElement)element).Assembly;
-                config._additionalStepAssemblies.Add(stepAssembly);
+                config.additionalStepAssemblies.Add(stepAssembly);
             }
 
             return config;
@@ -116,7 +45,7 @@ namespace TechTalk.SpecFlow.Configuration
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return other._additionalStepAssemblies.SequenceEqual(_additionalStepAssemblies) && Equals(other.ToolLanguage, ToolLanguage) && Equals(other.BindingCulture, BindingCulture) && other.DetectAmbiguousMatches.Equals(DetectAmbiguousMatches) && other.StopAtFirstError.Equals(StopAtFirstError) && Equals(other.MissingOrPendingStepsOutcome, MissingOrPendingStepsOutcome) && other.TraceSuccessfulSteps.Equals(TraceSuccessfulSteps) && other.TraceTimings.Equals(TraceTimings) && other.MinTracedDuration.Equals(MinTracedDuration);
+            return other.additionalStepAssemblies.SequenceEqual(additionalStepAssemblies);
         }
 
         public override bool Equals(object obj)
@@ -131,14 +60,7 @@ namespace TechTalk.SpecFlow.Configuration
         {
             unchecked
             {
-                int result = (ToolLanguage != null ? ToolLanguage.GetHashCode() : 0);
-                result = (result*397) ^ (BindingCulture != null ? BindingCulture.GetHashCode() : 0);
-                result = (result*397) ^ DetectAmbiguousMatches.GetHashCode();
-                result = (result*397) ^ StopAtFirstError.GetHashCode();
-                result = (result*397) ^ MissingOrPendingStepsOutcome.GetHashCode();
-                result = (result*397) ^ TraceSuccessfulSteps.GetHashCode();
-                result = (result*397) ^ TraceTimings.GetHashCode();
-                result = (result*397) ^ MinTracedDuration.GetHashCode();
+                int result = additionalStepAssemblies.Count.GetHashCode();
                 return result;
             }
         }
