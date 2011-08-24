a8a8b42aae5a4083ec2e8b98a74b75d8d9ee6be3
Matthew Ellis <matthew.ellis@uk.bnpparibas.com>
2011-08-24 11:10:16 +0100

    The silverlight generator now registers the current test instance in the ScenarioContext, after the context is initialised, but before any steps (inc. Background) are run. The generated code now always derives from SilverlightTest rather than just when generating async tests.

--- Generator/UnitTestProvider/MsTestSilverlightGeneratorProvider.cs
+++ Generator/UnitTestProvider/MsTestSilverlightGeneratorProvider.cs
@@ -8,10 +8,18 @@ namespace TechTalk.SpecFlow.Generator.UnitTestProvider
     public class MsTestSilverlightGeneratorProvider : MsTestGeneratorProvider
     {
         private const string TAG_ATTR = "Microsoft.Silverlight.Testing.TagAttribute";
-        private const string ASYNCTEST_BASE = "Microsoft.Silverlight.Testing.SilverlightTest";
+        private const string SILVERLIGHTTEST_BASE = "Microsoft.Silverlight.Testing.SilverlightTest";
         private const string ASYNCTEST_ATTR = "Microsoft.Silverlight.Testing.AsynchronousAttribute";
+        private const string ASYNCTEST_INTERFACE = "TechTalk.SpecFlow.Async.ISilverlightTestInstance";
 
         public override bool SupportsAsyncTests { get { return true; } }
+
+        public override void SetTestClass(TestClassGenerationContext generationContext, string featureTitle, string featureDescription)
+        {
+            base.SetTestClass(generationContext, featureTitle, featureDescription);
+
+            generationContext.TestClass.BaseTypes.Add(new CodeTypeReference(SILVERLIGHTTEST_BASE));
+        }
 
         public override void SetTestClassCategories(TestClassGenerationContext generationContext, IEnumerable<string> featureCategories)
         {
@@ -29,6 +37,21 @@ namespace TechTalk.SpecFlow.Generator.UnitTestProvider
         protected override void FixTestRunOrderingIssue(TestClassGenerationContext generationContext)
         {
             //nop; hopefully the issue is not present on Silverlight
+        }
+
+        public override void SetTestInitializeMethod(TestClassGenerationContext generationContext)
+        {
+            base.SetTestInitializeMethod(generationContext);
+
+            // SenarioContext.Current.SetTestInstance(this);
+            var scenarioContext = new CodeTypeReferenceExpression("ScenarioContext");
+            var currentContext = new CodePropertyReferenceExpression(scenarioContext, "Current");
+            var scenarioContextExtensions = new CodeTypeReferenceExpression("ScenarioContextExtensions");
+            var setTestInstance = new CodeMethodInvokeExpression(scenarioContextExtensions, "SetTestInstance",
+                currentContext, new CodeThisReferenceExpression());
+
+            // Add it to ScenarioSetup
+            generationContext.ScenarioInitializeMethod.Statements.Add(new CodeExpressionStatement(setTestInstance));
         }
 
         public override void SetTestMethod(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle)
@@ -54,8 +77,7 @@ namespace TechTalk.SpecFlow.Generator.UnitTestProvider
 
         private void SetupAsyncTestClass(TestClassGenerationContext generationContext)
         {
-            generationContext.TestClass.BaseTypes.Add(new CodeTypeReference(ASYNCTEST_BASE));
-            generationContext.TestClass.BaseTypes.Add(new CodeTypeReference("TechTalk.SpecFlow.Async.ISilverlightTestInstance"));
+            generationContext.TestClass.BaseTypes.Add(new CodeTypeReference(ASYNCTEST_INTERFACE));
 
             //AsyncTestRunner.RegisterAsyncTestExecutor(testRunner, new TechTalk.SpecFlow.Async.SilverlightAsyncTestExecutor(this));
 
