189976906b207f614946fc245746ee73fa18fa27
Darren Cauthon <darren@cauthon.com>
2011-10-23 06:32:17 -0500

    Bring back the marking of the rows that were not matched.

--- Runtime/Assist/TableDiffExceptionBuilder.cs
+++ Runtime/Assist/TableDiffExceptionBuilder.cs
@@ -1,24 +1,27 @@
-﻿namespace TechTalk.SpecFlow.Assist
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TechTalk.SpecFlow.Assist
 {
     public class TableDiffExceptionBuilder<T>
     {
         public string GetTheTableDiffExceptionMessage(TableDifferenceResults<T> tableDifferenceResults)
         {
-            return "  " + tableDifferenceResults.Table;
-            //var realData = new StringBuilder();
-            //var index = 0;
-            //var everyLineInTheTable = table.ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            //foreach (var line in everyLineInTheTable)
-            //{
-            //    var prefix = "  ";
-            //    if (listOfMissingItems.Contains(index))
-            //        prefix = "- ";
-            //    realData.AppendLine(prefix + line);
-            //    index++;
-            //}
+            var realData = new StringBuilder();
+            var index = 0;
+            var everyLineInTheTable = tableDifferenceResults.Table.ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in everyLineInTheTable)
+            {
+                var prefix = "  ";
+                if (tableDifferenceResults.IndexesOfTableRowsThatWereNotMatched.Contains(index))
+                    prefix = "- ";
+                realData.AppendLine(prefix + line);
+                index++;
+            }
             //foreach (var item in itemsThatWereNotFoundInTheTable)
             //    realData.AppendLine(string.Format("+ | {0} |", ((item.GetPropertyValue("StringProperty") as string) ?? "").PadRight(14)));
-            //return realData.ToString();
+            return realData.ToString();
         }
     }
 }
--- Tests/RuntimeTests/AssistTests/TableDiffExceptionBuilderTests.cs
+++ Tests/RuntimeTests/AssistTests/TableDiffExceptionBuilderTests.cs
@@ -8,22 +8,45 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests
     public class TableDiffExceptionBuilderTests
     {
         [Test]
-        public void Returning_a_table_with_no_missing_items_or_remaining_data_returns_the_table_with_two_character_prefix()
+        public void Adds_a_two_character_prefix_to_the_table()
         {
             var table = new Table("One", "Two", "Three");
 
             var builder = new TableDiffExceptionBuilder<TestObject>();
 
-            var message = builder.GetTheTableDiffExceptionMessage(new TableDifferenceResults<TestObject>(table, new int[] {}, new TestObject[] {}));
+            var tableDifferenceResults = new TableDifferenceResults<TestObject>(table, new int[] {}, new TestObject[] {});
+            var message = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
 
             message.ShouldEqual(@"  | One | Two | Three |
+");
+        }
+
+        [Test]
+        public void Prepends_a_dash_next_to_any_table_rows_that_were_missing()
+        {
+            var table = new Table("One", "Two", "Three");
+            table.AddRow("testa", "1", "W");
+            table.AddRow("testb", "2", "X");
+            table.AddRow("testc", "3", "Y");
+            table.AddRow("testd", "4", "Z");
+
+            var builder = new TableDiffExceptionBuilder<TestObject>();
+
+            var tableDifferenceResults = new TableDifferenceResults<TestObject>(table, new[] {2, 3}, new TestObject[] { });
+            var message = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
+
+            message.ShouldEqual(@"  | One   | Two | Three |
+  | testa | 1   | W     |
+- | testb | 2   | X     |
+- | testc | 3   | Y     |
+  | testd | 4   | Z     |
 ");
         }
 
         public class TestObject
         {
             public string One { get; set; }
-            public string Two { get; set; }
+            public int Two { get; set; }
             public string Three { get; set; }
         }
     }
