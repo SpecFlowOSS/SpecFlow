ac8cdaa3e9a365239c0dfeb30ebeed02e1c65038
Darren Cauthon <darren@cauthon.com>
2011-10-23 08:02:54 -0500

    Reformat a table so all column widths match.

--- Tests/RuntimeTests/AssistTests/TableDiffExceptionBuilderTests.cs
+++ Tests/RuntimeTests/AssistTests/TableDiffExceptionBuilderTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using Should;
 using TechTalk.SpecFlow.Assist;
 
@@ -79,7 +80,7 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests
                                          new TestObject {One = "A", Two = 1, Three = "Z"},
                                          new TestObject {One = null, Two = null, Three = null}
                                      };
-            var tableDifferenceResults = new TableDifferenceResults<TestObject>(table, new int[] { }, remainingItems);
+            var tableDifferenceResults = new TableDifferenceResults<TestObject>(table, new int[] {}, remainingItems);
             var message = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
 
             message.ShouldEqual(@"  | One   | Two | Three |
@@ -101,7 +102,7 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests
                                      {
                                          new TestObject {One = "A", Two = 1, TheFourthProperty = "Z"},
                                      };
-            var tableDifferenceResults = new TableDifferenceResults<TestObject>(table, new int[] { }, remainingItems);
+            var tableDifferenceResults = new TableDifferenceResults<TestObject>(table, new int[] {}, remainingItems);
             var message = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
 
             message.ShouldEqual(@"  | one   | TWO | The fourth property |
@@ -116,6 +117,67 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests
             public int? Two { get; set; }
             public string Three { get; set; }
             public string TheFourthProperty { get; set; }
+        }
+    }
+
+    public class FormattingTableDiffExceptionBuilderTests
+    {
+        [Test]
+        public void Returns_null_if_parent_passes_null()
+        {
+            var tableDifferenceResults = GetATestDiffResult();
+
+            var builder = GetABuilderThatReturnsThisString(null, tableDifferenceResults);
+
+            var result = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
+
+            result.ShouldBeNull();
+        }
+
+        [Test]
+        public void Returns_empty_if_parent_returns_empty()
+        {
+            var tableDifferenceResults = GetATestDiffResult();
+
+            var builder = GetABuilderThatReturnsThisString("", tableDifferenceResults);
+
+            var result = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
+
+            result.ShouldEqual("");
+        }
+
+        [Test]
+        public void Makes_width_of_columns_match()
+        {
+            var tableDifferenceResults = GetATestDiffResult();
+
+            var builder = GetABuilderThatReturnsThisString(@"| One | Two | Three |
+| 1234567 | 1 | 1234567890 |
+| 1| 2 | 3 |", tableDifferenceResults);
+
+            var result = builder.GetTheTableDiffExceptionMessage(tableDifferenceResults);
+
+            result.ShouldEqual(@"| One     | Two | Three      |
+| 1234567 | 1   | 1234567890 |
+| 1       | 2   | 3          |");
+        }
+
+        private static FormattingTableDiffExceptionBuilder<TestObject> GetABuilderThatReturnsThisString(string returnValue, TableDifferenceResults<TestObject> tableDifferenceResults)
+        {
+            var parentFake = new Mock<ITableDiffExceptionBuilder<TestObject>>();
+            parentFake.Setup(x => x.GetTheTableDiffExceptionMessage(tableDifferenceResults))
+                .Returns(() => returnValue);
+
+            return new FormattingTableDiffExceptionBuilder<TestObject>(parentFake.Object);
+        }
+
+        private static TableDifferenceResults<TestObject> GetATestDiffResult()
+        {
+            return new TableDifferenceResults<TestObject>(null, null, null);
+        }
+
+        public class TestObject
+        {
         }
     }
 }
