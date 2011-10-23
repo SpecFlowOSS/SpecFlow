f7e9fca07937919d90a10e3ce9a8551a5b4c9f97
Darren Cauthon <darren@cauthon.com>
2011-10-23 06:02:33 -0500

    Start writing tests for the diff builder, by itself.

--- Runtime/Assist/TableDiffExceptionBuilder.cs
+++ Runtime/Assist/TableDiffExceptionBuilder.cs
@@ -9,20 +9,21 @@ namespace TechTalk.SpecFlow.Assist
     {
         public string GetTheTableDiffExceptionMessage(IEnumerable<int> listOfMissingItems, IEnumerable<T> itemsThatWereNotFoundInTheTable, Table table)
         {
-            var realData = new StringBuilder();
-            var index = 0;
-            var everyLineInTheTable = table.ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            foreach (var line in everyLineInTheTable)
-            {
-                var prefix = "  ";
-                if (listOfMissingItems.Contains(index))
-                    prefix = "- ";
-                realData.AppendLine(prefix + line);
-                index++;
-            }
-            foreach (var item in itemsThatWereNotFoundInTheTable)
-                realData.AppendLine(string.Format("+ | {0} |", ((item.GetPropertyValue("StringProperty") as string) ?? "").PadRight(14)));
-            return realData.ToString();
+            return "  " + table;
+            //var realData = new StringBuilder();
+            //var index = 0;
+            //var everyLineInTheTable = table.ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            //foreach (var line in everyLineInTheTable)
+            //{
+            //    var prefix = "  ";
+            //    if (listOfMissingItems.Contains(index))
+            //        prefix = "- ";
+            //    realData.AppendLine(prefix + line);
+            //    index++;
+            //}
+            //foreach (var item in itemsThatWereNotFoundInTheTable)
+            //    realData.AppendLine(string.Format("+ | {0} |", ((item.GetPropertyValue("StringProperty") as string) ?? "").PadRight(14)));
+            //return realData.ToString();
         }
     }
 }
---
+++ Tests/RuntimeTests/AssistTests/TableDiffExceptionBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Should;
+using TechTalk.SpecFlow.Assist;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests
+{
+    [TestFixture]
+    public class TableDiffExceptionBuilderTests
+    {
+        [Test]
+        public void Returning_a_table_with_no_missing_items_or_remaining_data_returns_the_table_with_two_character_prefix()
+        {
+            var table = new Table("One", "Two", "Three");
+
+            var builder = new TableDiffExceptionBuilder<TestObject>();
+
+            var message = builder.GetTheTableDiffExceptionMessage(new int[] {}, new TestObject[] {}, table);
+
+            message.ShouldEqual(@"  | One | Two | Three |
+");
+        }
+
+        public class TestObject
+        {
+            public string One { get; set; }
+            public string Two { get; set; }
+            public string Three { get; set; }
+        }
+    }
+}
