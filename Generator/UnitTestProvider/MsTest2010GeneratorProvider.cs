06d33deb799a31bbab83686a1d4f498c48b40251
Nithin Shenoy <nithin_shenoy@hotmail.com>
2012-02-03 21:50:25 -0500

    Adding support for MSTest OwnerAttribute By specifying the @owner=foo tag on the feature or scenario, the OwnerAttribute will be added to the respective scenario generated code.

--- Generator/UnitTestProvider/MsTest2010GeneratorProvider.cs
+++ Generator/UnitTestProvider/MsTest2010GeneratorProvider.cs
@@ -1,31 +1,71 @@
 ﻿using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System;
+using System.Diagnostics;
 
 namespace TechTalk.SpecFlow.Generator.UnitTestProvider
 {
-    public class MsTest2010GeneratorProvider : MsTestGeneratorProvider
-    {
-        private const string CATEGORY_ATTR = "Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute";
+	public class MsTest2010GeneratorProvider : MsTestGeneratorProvider
+	{
+		private const string CATEGORY_ATTR = "Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute";
+		private const string OWNER_ATTR = "Microsoft.VisualStudio.TestTools.UnitTesting.OwnerAttribute";
 
-        public override void SetTestClassCategories(TestClassGenerationContext generationContext, IEnumerable<string> featureCategories)
-        {
-            generationContext.CustomData["featureCategories"] = featureCategories.ToArray();
-        }
+		private const string OWNER_TAG = "owner=";
 
-        public override void SetTestMethod(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle)
-        {
-            base.SetTestMethod(generationContext, testMethod, scenarioTitle);
-            if (generationContext.CustomData.ContainsKey("featureCategories"))
-            {
-                var featureCategories = (string[])generationContext.CustomData["featureCategories"];
-                CodeDomHelper.AddAttributeForEachValue(testMethod, CATEGORY_ATTR, featureCategories);
-            }
-        }
+		public override void SetTestClassCategories(TestClassGenerationContext generationContext, IEnumerable<string> featureCategories)
+		{
+			generationContext.CustomData["featureCategories"] = GetNonMSTestSpecificTags(featureCategories).ToArray();
 
-        public override void SetTestMethodCategories(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, IEnumerable<string> scenarioCategories)
-        {
-            CodeDomHelper.AddAttributeForEachValue(testMethod, CATEGORY_ATTR, scenarioCategories);
-        }
-    }
+			IEnumerable<string> ownerTags = featureCategories.Where(t => t.StartsWith(OWNER_TAG, StringComparison.InvariantCultureIgnoreCase)).Select(t => t);
+			if(ownerTags.Count() > 0)
+			{
+				generationContext.CustomData[OWNER_TAG] = ownerTags.Select(t => t.Substring(OWNER_TAG.Length).Trim('\"')).FirstOrDefault();
+			}
+		}
+
+		public override void SetTestMethod(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle)
+		{
+			base.SetTestMethod(generationContext, testMethod, scenarioTitle);
+			if(generationContext.CustomData.ContainsKey("featureCategories"))
+			{
+				var featureCategories = (string[])generationContext.CustomData["featureCategories"];
+				CodeDomHelper.AddAttributeForEachValue(testMethod, CATEGORY_ATTR, featureCategories);
+			}
+
+			if(generationContext.CustomData.ContainsKey(OWNER_TAG))
+			{
+				string ownerName = generationContext.CustomData[OWNER_TAG] as string;
+				if(!String.IsNullOrEmpty(ownerName))
+				{
+					CodeDomHelper.AddAttribute(testMethod, OWNER_ATTR, ownerName);
+				}
+			}
+		}
+
+		public override void SetTestMethodCategories(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, IEnumerable<string> scenarioCategories)
+		{
+			IEnumerable<string> tags = scenarioCategories.ToList();
+
+			IEnumerable<string> ownerTags = tags.Where(t => t.StartsWith(OWNER_TAG, StringComparison.InvariantCultureIgnoreCase)).Select(t => t);
+			if(ownerTags.Count() > 0)
+			{
+				string ownerName = ownerTags.Select(t => t.Substring(OWNER_TAG.Length).Trim('\"')).FirstOrDefault();
+				if(!String.IsNullOrEmpty(ownerName))
+				{
+					CodeDomHelper.AddAttribute(testMethod, OWNER_ATTR, ownerName);
+				}
+			}
+
+			CodeDomHelper.AddAttributeForEachValue(testMethod, CATEGORY_ATTR, GetNonMSTestSpecificTags(tags));
+		}
+
+		private IEnumerable<string> GetNonMSTestSpecificTags(IEnumerable<string> tags)
+		{
+			return tags == null ? new string[0] : tags.Where(t =>
+				(!t.StartsWith(OWNER_TAG, StringComparison.InvariantCultureIgnoreCase)))
+				.Select(t => t);
+		}
+	}
 }
