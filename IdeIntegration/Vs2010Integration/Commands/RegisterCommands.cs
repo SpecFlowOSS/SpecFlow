23c69a7de36f488dc7a91f4a32b7d1567fafee2d
Gaspar Nagy <gaspar.nagy@gmail.com>
2012-07-23 14:27:32 +0200

    add missing registration for "go to step definition"

--- IdeIntegration/Vs2010Integration/Commands/RegisterCommands.cs
+++ IdeIntegration/Vs2010Integration/Commands/RegisterCommands.cs
@@ -15,6 +15,7 @@ namespace TechTalk.SpecFlow.Vs2010Integration
             container.RegisterTypeAs<RunScenariosCommand, MenuCommandHandler>(SpecFlowCmdSet.RunScenarios.ToString());
             container.RegisterTypeAs<DebugScenariosCommand, MenuCommandHandler>(SpecFlowCmdSet.DebugScenarios.ToString());
             container.RegisterTypeAs<ReGenerateAllCommand, MenuCommandHandler>(SpecFlowCmdSet.ReGenerateAll.ToString());
+            container.RegisterTypeAs<GoToStepDefinitionCommand, MenuCommandHandler>(SpecFlowCmdSet.GoToStepDefinition.ToString());
             container.RegisterTypeAs<GoToStepsCommand, MenuCommandHandler>(SpecFlowCmdSet.GoToSteps.ToString());
             container.RegisterTypeAs<ContextDependentNavigationCommand, MenuCommandHandler>(SpecFlowCmdSet.ContextDependentNavigation.ToString());
         }
