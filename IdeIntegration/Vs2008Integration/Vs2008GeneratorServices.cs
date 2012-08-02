f52ac019a94dd07abe11f8b448a44e8832b8c53a
Gaspar Nagy <gaspar.nagy@gmail.com>
2012-08-02 19:25:50 +0200

    add InstallerServices to VS2008 integration

--- IdeIntegration/Vs2008Integration/Vs2008GeneratorServices.cs
+++ IdeIntegration/Vs2008Integration/Vs2008GeneratorServices.cs
@@ -3,18 +3,33 @@ using TechTalk.SpecFlow.Generator;
 using TechTalk.SpecFlow.Generator.Configuration;
 using TechTalk.SpecFlow.Generator.Interfaces;
 using TechTalk.SpecFlow.IdeIntegration.Generator;
+using TechTalk.SpecFlow.IdeIntegration.Install;
 using TechTalk.SpecFlow.IdeIntegration.Tracing;
+using TechTalk.SpecFlow.Vs2010Integration.Install;
 
 namespace TechTalk.SpecFlow.Vs2008Integration
 {
     public class Vs2008GeneratorServices : GeneratorServices
     {
         private readonly Project project;
+        private static bool installerServiceInitialized = false;
 
         public Vs2008GeneratorServices(Project project) : base(
             new TestGeneratorFactory(), NullIdeTracer.Instance, true)
         {
             this.project = project;
+
+            if (!installerServiceInitialized)
+            {
+                InstallServices installerService =
+                    new InstallServices(new VsBrowserGuidanceNotificationService(project.DTE), tracer,
+                                        new WindowsFileAssociationDetector(tracer),
+                                        new RegistryStatusAccessor(tracer));
+
+                installerService.OnPackageLoad(IdeIntegration.Install.IdeIntegration.VisualStudio2008);
+                installerService.OnPackageUsed();
+                installerServiceInitialized = true;
+            }
         }
 
         protected override ProjectSettings LoadProjectSettings()
