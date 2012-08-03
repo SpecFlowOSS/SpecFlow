5a3c4d792c04a68f12a667f08b04b2149df2476b
Gaspar Nagy <gaspar.nagy@gmail.com>
2012-08-03 15:52:38 +0200

    generate pdb/source packages for vs integration

--- IdeIntegration/IdeIntegration/Properties/AssemblyInfo.cs
+++ IdeIntegration/IdeIntegration/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IdeIntegration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SpecFlow IDE integration library")]
 [assembly: AssemblyConfiguration("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
--- IdeIntegration/Vs2010Integration/Properties/AssemblyInfo.cs
+++ IdeIntegration/Vs2010Integration/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TechTalk.SpecFlow.Vs2010Integration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SpecFlow VS2010 and VS2012 integration library")]
 [assembly: AssemblyConfiguration("")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
