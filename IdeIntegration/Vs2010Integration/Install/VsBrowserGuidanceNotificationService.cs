9c05cf39213a9b0597bfc5d0e83a65177c5b2b32
Gaspar Nagy <gaspar.nagy@gmail.com>
2012-08-06 13:37:49 +0200

    fix guidance display

--- IdeIntegration/Vs2010Integration/Install/VsBrowserGuidanceNotificationService.cs
+++ IdeIntegration/Vs2010Integration/Install/VsBrowserGuidanceNotificationService.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Windows;
 using EnvDTE;
 using TechTalk.SpecFlow.IdeIntegration.Install;
+using Window = EnvDTE.Window;
 
 namespace TechTalk.SpecFlow.Vs2010Integration.Install
 {
@@ -16,10 +19,33 @@ namespace TechTalk.SpecFlow.Vs2010Integration.Install
             this.dte = dte;
         }
 
+        private System.Windows.Forms.Timer activationTimer;
+
         public bool ShowPage(string url)
         {
             var window = dte.ItemOperations.Navigate(url, vsNavigateOptions.vsNavigateOptionsNewWindow);
+
+            activationTimer = new System.Windows.Forms.Timer();
+            activationTimer.Tick += (sender, args) => ActivateWindow(window);
+            activationTimer.Interval = 5000;
+            activationTimer.Start();
+
             return true;
+        }
+
+        private void ActivateWindow(object state)
+        {
+            activationTimer.Stop();
+
+            try
+            {
+                Window window = (Window)state;
+                window.Activate();
+            }
+            catch (Exception)
+            {
+                // bad luck...
+            }
         }
     }
 }
