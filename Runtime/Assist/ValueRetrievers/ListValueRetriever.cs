cc1876114db68f063bebf4d9a622c64d666f490c
Konstantin Spirin <konstmsu@gmail.com>
2012-09-06 22:51:12 +0800

    Added ability to map List<T> properties

---
+++ Runtime/Assist/ValueRetrievers/ListValueRetriever.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechTalk.SpecFlow.Assist.ValueRetrievers
+{
+    public class ListValueRetriever
+    {
+        internal object GetValue(string values, Type listType)
+        {
+            var elementType = listType.GetGenericArguments().Single();
+
+            object list = Activator.CreateInstance(listType);
+            var add = listType.GetMethod("Add");
+            foreach (var value in values.Split(new[] { "," }, StringSplitOptions.None).Select(v => v.Trim()).Select(v => Convert.ChangeType(v, elementType)))
+                add.Invoke(list, new[] { value });
+
+            return list;
+        }
+    }
+}
