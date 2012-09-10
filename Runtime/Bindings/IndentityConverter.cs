a0d8f395ad8ce644c6586097d77a878e1319d5d3
Yazide Boujlil <github@naylis.net>
2012-09-10 13:16:02 +0200

    Split of StepArgumentTypeConverter logic
    Converted StepArgumentTypeConverter into a chain of responsibility
    delegating the conversion logic to:
    - IdentityConverter which returns the untouched value if it can be
    assigned to a variable of the target type (logic comes from
    StepDefinitionMatchService and TestExecutionEngine)
    - StepArgumentTransformationConverter which tries to perform the
    conversion using user defined step argument transformations
    - SimpleConverter which handle the simple conversions: enums, GUID and
    whatever is supported by System.Convert.ChangeType

---
+++ Runtime/Bindings/IndentityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using TechTalk.SpecFlow.Bindings.Reflection;
+
+namespace TechTalk.SpecFlow.Bindings
+{
+    internal class IndentityConverter : IStepArgumentTypeConverter
+    {
+        public object Convert(object value, IBindingType typeToConvertTo, CultureInfo cultureInfo)
+        {
+            return value;
+        }
+
+        public bool CanConvert(object value, IBindingType typeToConvertTo, CultureInfo cultureInfo)
+        {
+            return typeToConvertTo.IsAssignableTo(value.GetType());
+        }
+    }
+}
