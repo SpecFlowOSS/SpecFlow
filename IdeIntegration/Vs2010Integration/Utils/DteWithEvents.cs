d053c211dace7418f3b1c843bd0b3419864feb93
Gaspar Nagy <gaspar.nagy@gmail.com>
2012-10-11 17:23:52 +0200

    fix Issue 230: InvalidComObjectException: COM object that has been separated from its underlying RCW cannot be used.

--- IdeIntegration/Vs2010Integration/Utils/DteWithEvents.cs
+++ IdeIntegration/Vs2010Integration/Utils/DteWithEvents.cs
@@ -11,11 +11,38 @@ namespace TechTalk.SpecFlow.Vs2010Integration.Utils
     /// </summary>
     internal class DteWithEvents
     {
+        public event _dispBuildEvents_OnBuildDoneEventHandler OnBuildDone
+        {
+            add
+            {
+                try
+                {
+                    BuildEvents.Value.OnBuildDone += value;
+                }
+                catch (Exception e)
+                {
+                    tracer.Trace("OnBuildDone: {0}", this, e);
+                }
+            }
+            remove
+            {
+                try
+                {
+                    BuildEvents.Value.OnBuildDone -= value;
+                }
+                catch (Exception e)
+                {
+                    tracer.Trace("OnBuildDone: {0}", this, e);
+                }
+            }
+        }
+
         public readonly DTE DTE;
+        private readonly IIdeTracer tracer;
         public readonly SolutionEvents SolutionEvents;
         public readonly ProjectItemsEvents ProjectItemsEvents;
         public readonly DocumentEvents DocumentEvents;
-        public readonly BuildEvents BuildEvents;
+        private readonly Lazy<BuildEvents> BuildEvents;
         public readonly CodeModelEvents CodeModelEvents;
 
         public readonly SolutionEventsListener SolutionEventsListener;
@@ -24,10 +51,11 @@ namespace TechTalk.SpecFlow.Vs2010Integration.Utils
         public DteWithEvents(DTE dte, IIdeTracer tracer)
         {
             DTE = dte;
+            this.tracer = tracer;
             SolutionEvents = dte.Events.SolutionEvents;
             ProjectItemsEvents = ((Events2)dte.Events).ProjectItemsEvents;
             DocumentEvents = ((Events2) dte.Events).DocumentEvents;
-            BuildEvents = ((Events2) dte.Events).BuildEvents;
+            BuildEvents = new Lazy<BuildEvents>(() => ((Events2)dte.Events).BuildEvents, true);
             CodeModelEvents = ((Events2)dte.Events).CodeModelEvents;
 
             SolutionEventsListener = new SolutionEventsListener();
