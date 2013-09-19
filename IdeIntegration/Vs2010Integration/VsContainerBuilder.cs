60aeecc5e670d8a66ceea52c02936da59abcca2b
Gaspar Nagy <gaspar.nagy@gmail.com>
2013-09-19 12:31:43 +0200

    share files to Vs2010

---
+++ IdeIntegration/Vs2010Integration/VsContainerBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BoDi;
+using Microsoft.VisualStudio.Shell;
+
+namespace TechTalk.SpecFlow.Vs2010Integration
+{
+    public static class VsContainerBuilder
+    {
+        internal static DefaultDependencyProvider DefaultDependencyProvider = new DefaultDependencyProvider();
+
+        public static IObjectContainer CreateContainer(SpecFlowPackagePackage package)
+        {
+            var container = new ObjectContainer();
+
+            container.RegisterInstanceAs(package);
+            container.RegisterInstanceAs<IServiceProvider>(package);
+
+            RegisterDefaults(container);
+
+            BiDiContainerProvider.CurrentContainer = container; //TODO: avoid static field
+
+            return container;
+        }
+
+        private static void RegisterDefaults(IObjectContainer container)
+        {
+            DefaultDependencyProvider.RegisterDefaults(container);
+        }
+    }
+}
