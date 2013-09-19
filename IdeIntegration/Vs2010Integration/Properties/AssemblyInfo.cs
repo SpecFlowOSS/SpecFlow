f494980663de205fac538f5b477990405fefab53
Gaspar Nagy <gaspar.nagy@gmail.com>
2013-09-19 12:15:16 +0200

    step1: refactor vs2013 project

---
+++ IdeIntegration/Vs2010Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TechTalk.SpecFlow.Vs2010Integration")]
+[assembly: AssemblyDescription("SpecFlow VS2010 and VS2012 integration library")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
