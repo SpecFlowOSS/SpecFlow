bb19a66fbb3fe18ce69ef111dc19d23fdc9172bf
Gaspar Nagy <gaspar.nagy@gmail.com>
2013-09-23 17:11:33 +0200

    allow running specrun tests from context menu

---
+++ IdeIntegration/Vs2013Integration/VS2013/SpecRunTestMethodResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.VisualStudio.TestWindow.Data;
+using Microsoft.VisualStudio.TestWindow.Extensibility;
+
+namespace TechTalk.SpecFlow.VsIntegration.VS2013
+{
+    public static class SpecRunConstants
+    {
+        public const string ExecutorUriString = "executor://specrun.com/executor_v1.2";
+        public static Uri ExecutorUri = new Uri(ExecutorUriString);
+    }
+
+    [Export(typeof(ITestMethodResolver))]
+    public class SpecRunTestMethodResolver : ITestMethodResolver
+    {
+        private readonly IUnitTestStorage unitTestStorage;
+
+        public Uri ExecutorUri { get { return SpecRunConstants.ExecutorUri; } }
+
+        [ImportingConstructor]
+        public SpecRunTestMethodResolver(IUnitTestStorage unitTestStorage)
+        {
+            this.unitTestStorage = unitTestStorage;
+        }
+
+        public string GetCurrentTest(string filePath, int line, int lineCharOffset)
+        {
+            var testsToRun = unitTestStorage.ActiveUnitTestReader.GetAllTestsWithProperties()
+                .Where(t => string.Equals(t.FilePath, filePath, StringComparison.OrdinalIgnoreCase) && t.LineNumber >= line)
+                .OrderBy(t => t.LineNumber);
+
+            var testToRun = testsToRun.FirstOrDefault();
+
+            return testToRun != null && testToRun.ExecutorUri.Equals(SpecRunConstants.ExecutorUriString)  ? testToRun.FullyQualifiedName : null;
+        }
+
+    }
+}
