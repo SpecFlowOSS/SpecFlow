7d2ee75c5c6da390f79f21ef3a82dbbd2608907a
Gaspar Nagy <gaspar.nagy@gmail.com>
2013-09-26 14:48:26 +0200

    update assembly info

--- IdeIntegration/Vs2010Integration/Properties/AssemblyInfo.cs
+++ IdeIntegration/Vs2010Integration/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TechTalk.SpecFlow.Vs2010Integration")]
-[assembly: AssemblyDescription("SpecFlow VS2010 and VS2012 integration library")]
+[assembly: AssemblyTitle("TechTalk.SpecFlow.Vs2013Integration")]
+[assembly: AssemblyDescription("SpecFlow VS2013 integration library")]
 [assembly: AssemblyConfiguration("")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
