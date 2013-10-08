b696bafbbaeac95e8e56328a2f6bf194c4d543ed
joebuschmann <joseph.buschmann@outlook.com>
2013-10-08 17:56:50 -0500

    Updated the FillInstance() method so it doesn't use a generic argument. Instead it uses the runtime type of the object (instance.GetType()).

--- Runtime/Assist/TEHelpers.cs
+++ Runtime/Assist/TEHelpers.cs
@@ -21,7 +21,7 @@ namespace TechTalk.SpecFlow.Assist
             if (constructor == null)
                 throw new MissingMethodException(string.Format("Unable to find a suitable constructor to create instance of {0}", typeof(T).Name));
 
-            var propertiesThatNeedToBeSet = GetPropertiesThatNeedToBeSet<T>(table);
+            var propertiesThatNeedToBeSet = GetPropertiesThatNeedToBeSet(table, typeof(T));
 
             var constructorParameters = constructor.GetParameters();
             var parameterValues = new object[constructorParameters.Length];
@@ -69,19 +69,19 @@ namespace TechTalk.SpecFlow.Assist
 
         }
 
-        internal static void LoadInstanceWithKeyValuePairs<T>(Table table, T instance)
+        internal static void LoadInstanceWithKeyValuePairs(Table table, object instance)
         {
-            var propertiesThatNeedToBeSet = GetPropertiesThatNeedToBeSet<T>(table);
+            var propertiesThatNeedToBeSet = GetPropertiesThatNeedToBeSet(table, instance.GetType());
 
             propertiesThatNeedToBeSet.ToList()
                 .ForEach(x => instance.SetPropertyValue(x.PropertyName, x.Handler(x.Row)));
         }
 
-        internal static IEnumerable<PropertyHandler> GetPropertiesThatNeedToBeSet<T>(Table table)
+        internal static IEnumerable<PropertyHandler> GetPropertiesThatNeedToBeSet(Table table, Type type)
         {
-            var handlers = GetTypeHandlersForFieldValuePairs<T>();
+            var handlers = GetTypeHandlersForFieldValuePairs(type);
 
-            return from property in typeof (T).GetProperties()
+            return from property in type.GetProperties()
                    from key in handlers.Keys
                    from row in table.Rows
                    where ThisPropertyMatchesTheType(property, key) 
@@ -98,7 +98,7 @@ namespace TechTalk.SpecFlow.Assist
             return false;
         }
 
-        internal static Dictionary<Type, Func<TableRow, object>> GetTypeHandlersForFieldValuePairs<T>()
+        internal static Dictionary<Type, Func<TableRow, object>> GetTypeHandlersForFieldValuePairs(Type type)
         {
             return new Dictionary<Type, Func<TableRow, object>>
                        {
@@ -133,7 +133,7 @@ namespace TechTalk.SpecFlow.Assist
                            {typeof (DateTime?), (TableRow row) => new NullableDateTimeValueRetriever(v => new DateTimeValueRetriever().GetValue(v)).GetValue(row[1])},
                            {typeof (Guid), (TableRow row) => new GuidValueRetriever().GetValue(row[1])},
                            {typeof (Guid?), (TableRow row) => new NullableGuidValueRetriever(v => new GuidValueRetriever().GetValue(v)).GetValue(row[1])},
-                           {typeof (Enum), (TableRow row) => new EnumValueRetriever().GetValue(row[1], typeof (T).GetProperties().First(x => x.Name.MatchesThisColumnName(row[0])).PropertyType)},
+                           {typeof (Enum), (TableRow row) => new EnumValueRetriever().GetValue(row[1], type.GetProperties().First(x => x.Name.MatchesThisColumnName(row[0])).PropertyType)},
                        };
         }
 
@@ -144,9 +144,9 @@ namespace TechTalk.SpecFlow.Assist
             public Func<TableRow, object> Handler { get; set; }
         }
 
-        internal static Table GetTheProperInstanceTable<T>(Table table)
+        internal static Table GetTheProperInstanceTable(Table table, Type type)
         {
-            return ThisIsAVerticalTable<T>(table)
+            return ThisIsAVerticalTable(table, type)
                        ? table
                        : FlipThisHorizontalTableToAVerticalTable(table);
         }
@@ -156,11 +156,11 @@ namespace TechTalk.SpecFlow.Assist
             return new PivotTable(table).GetInstanceTable(0);
         }
 
-        private static bool ThisIsAVerticalTable<T>(Table table)
+        private static bool ThisIsAVerticalTable(Table table, Type type)
         {
             if (TheHeaderIsTheOldFieldValuePair(table))
                 return true;
-            return (table.Rows.Count() != 1) || (table.Header.Count() == 2 && TheFirstRowValueIsTheNameOfAProperty<T>(table));
+            return (table.Rows.Count() != 1) || (table.Header.Count() == 2 && TheFirstRowValueIsTheNameOfAProperty(table, type));
         }
 
         private static bool TheHeaderIsTheOldFieldValuePair(Table table)
@@ -168,10 +168,10 @@ namespace TechTalk.SpecFlow.Assist
             return table.Header.Count() == 2 && table.Header.First() == "Field" && table.Header.Last() == "Value";
         }
 
-        private static bool TheFirstRowValueIsTheNameOfAProperty<T>(Table table)
+        private static bool TheFirstRowValueIsTheNameOfAProperty(Table table, Type type)
         {
             var firstRowValue = table.Rows[0][table.Header.First()];
-            return typeof(T).GetProperties()
+            return type.GetProperties()
                 .Any(property => IsPropertyMatchingToColumnName(property, firstRowValue));
         }
     }
