8b8ab93e970c0bbfadae8ce0185639a8a5364c78
Sam Holder <Sam.Holder@Insurecom.co.uk>
2014-10-31 17:25:51 +0000

    Added a ScenarioStepContext which exposes the current step type and crrent step text so that these can be used in BeforeStep/AfterStep methods

---
+++ Runtime/StepInfo.cs
@@ -0,0 +1,21 @@
+namespace TechTalk.SpecFlow
+{
+    using Bindings;
+
+    public class StepInfo
+    {
+        public StepDefinitionType StepDefinitionType { get; private set; }
+
+        public string Text { get; private set; }
+        public Table Table { get; private set; }
+        public string MultilineText { get; private set; }
+
+        public StepInfo(StepDefinitionType stepDefinitionType, string text, Table table, string multilineText)
+        {
+            StepDefinitionType = stepDefinitionType;
+            Text = text;
+            Table = table;
+            MultilineText = multilineText;
+        }
+    }
+}
