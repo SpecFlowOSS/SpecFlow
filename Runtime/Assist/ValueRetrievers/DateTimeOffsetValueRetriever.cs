052ef9b3c49c07533059af63246d6e5947274c04
Tim Gebhardt <tgebhardt@spotlite.com>
2015-02-26 08:10:41 -0600

    Issue 193: Adding a new value retriever for DateTimeOffsets
    Table -> Object translation was not converting DateTimeOffset table
    values.  This should allow the Table Assist helpers to do so.

---
+++ Runtime/Assist/ValueRetrievers/DateTimeOffsetValueRetriever.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TechTalk.SpecFlow.Assist.ValueRetrievers
+{
+    internal class DateTimeOffsetValueRetriever
+    {
+        public virtual DateTimeOffset GetValue(string value)
+        {
+            var returnValue = DateTimeOffset.MinValue;
+            DateTimeOffset.TryParse(value, out returnValue);
+            return returnValue;
+        }
+    }
+}
