5ec55dda6c2f1ee66bcd3b0135fee73665f09551
sam holder <samholder@gmail.com>
2015-03-10 21:48:19 +0000

    added characterisation tests for build file scanning

---
+++ Generator/Project/MsBuildProjectFileExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Evaluation;
+
+namespace TechTalk.SpecFlow.Generator.Project
+{
+    static class MsBuildProjectFileExtensions
+    {
+        public static IEnumerable<ProjectItem> FeatureFiles(this Microsoft.Build.Evaluation.Project project)
+        {
+            
+            return project.AllEvaluatedItems.Where(x => x.ItemType == "None" &&
+                                                        x.EvaluatedInclude.EndsWith(".feature", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public static ProjectItem ApplicationConfigurationFile(this Microsoft.Build.Evaluation.Project project)
+        {
+            return project.AllEvaluatedItems.FirstOrDefault(x => x.ItemType == "Content" &&
+                                                                 Path.GetFileName(x.EvaluatedInclude).Equals("app.config", StringComparison.InvariantCultureIgnoreCase));
+
+        }
+    }
+}
---
+++ Tests/GeneratorTests/MsBuildProjectReaderTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TechTalk.SpecFlow.Generator.Project;
+
+namespace TechTalk.SpecFlow.GeneratorTests
+{
+    [TestFixture]
+    public class MsBuildProjectReaderTests
+    {
+        [Test]
+        public void Should_parse_csproj_file_correctly()
+        {
+            string text = Path.Combine(Directory.GetCurrentDirectory(), "Data\\sampleCsProjectfile.csproj");
+            SpecFlowProject specflowProjectfile = MsBuildProjectReader.LoadSpecFlowProjectFromMsBuild(text);
+            specflowProjectfile.FeatureFiles.Count.Should().Be(3);
+            specflowProjectfile.FeatureFiles.Single(x => x.ProjectRelativePath == @"Features\Login\SocialLogins.feature").Should().NotBeNull();
+            specflowProjectfile.FeatureFiles.Single(x => x.ProjectRelativePath == @"Features\WorkflowDefinition\CreateWorkflowDefinition.feature").Should().NotBeNull();
+            specflowProjectfile.FeatureFiles.Single(x => x.ProjectRelativePath == @"Features\WorkflowInstance\WorkflowInstance.feature").Should().NotBeNull();
+
+            specflowProjectfile.ProjectSettings.AssemblyName.Should().Be("Hacapp.Web.Tests.UI");
+            specflowProjectfile.ProjectSettings.DefaultNamespace.Should().Be("Hacapp.Web.Tests.UI");
+            specflowProjectfile.ProjectSettings.ProjectName.Should().Be("sampleCsProjectfile");
+
+            specflowProjectfile.ProjectSettings.ProjectPlatformSettings.Language.Should().Be("C#");
+            specflowProjectfile.ProjectSettings.ProjectPlatformSettings.LanguageVersion.Should().Be(new Version(3,0));
+            specflowProjectfile.ProjectSettings.ProjectPlatformSettings.Platform.Should().Be(".NET");
+            specflowProjectfile.ProjectSettings.ProjectPlatformSettings.PlatformVersion.Should().Be(new Version(3,5));
+
+            specflowProjectfile.Configuration.GeneratorConfiguration.AllowDebugGeneratedFiles.Should().BeFalse();
+            specflowProjectfile.Configuration.GeneratorConfiguration.AllowRowTests.Should().BeTrue();
+            specflowProjectfile.Configuration.GeneratorConfiguration.GenerateAsyncTests.Should().BeFalse();
+            specflowProjectfile.Configuration.GeneratorConfiguration.GeneratorUnitTestProvider.Should().Be("MSTest");
+            specflowProjectfile.Configuration.GeneratorConfiguration.FeatureLanguage.Name.Should().Be("en-US");            
+        }
+    }
+}
