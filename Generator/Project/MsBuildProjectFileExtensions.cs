e32d899fc9c97e6ff230c7326e73ce03974be9d7
sam holder <samholder@gmail.com>
2015-03-17 21:12:17 +0000

    Ensured that app.config files are parsed correctly from .csproj files by extending the item types that they can defined with.

--- Generator/Project/MsBuildProjectFileExtensions.cs
+++ Generator/Project/MsBuildProjectFileExtensions.cs
@@ -10,14 +10,19 @@ namespace TechTalk.SpecFlow.Generator.Project
     {
         public static IEnumerable<ProjectItem> FeatureFiles(this Microsoft.Build.Evaluation.Project project)
         {
-            
-            return project.AllEvaluatedItems.Where(x => x.ItemType == "None" &&
+
+            return project.AllEvaluatedItems.Where(x => IsNonCompilingItem(x) &&
                                                         x.EvaluatedInclude.EndsWith(".feature", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private static bool IsNonCompilingItem(ProjectItem x)
+        {
+            return (x.ItemType == "Content" || x.ItemType == "None");
         }
 
         public static ProjectItem ApplicationConfigurationFile(this Microsoft.Build.Evaluation.Project project)
         {
-            return project.AllEvaluatedItems.FirstOrDefault(x => x.ItemType == "Content" &&
+            return project.AllEvaluatedItems.FirstOrDefault(x => IsNonCompilingItem(x) &&
                                                                  Path.GetFileName(x.EvaluatedInclude).Equals("app.config", StringComparison.InvariantCultureIgnoreCase));
 
         }
