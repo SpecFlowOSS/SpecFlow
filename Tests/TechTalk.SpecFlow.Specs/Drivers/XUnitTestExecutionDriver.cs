4310d2ed9501b26b877c0d73e30efd8ddaf69fb1
Morten Brix Pedersen <morten@wtf.dk>
2015-04-16 16:45:33 +0200

    Add XUnitTestExecutionDriver.

---
+++ Tests/TechTalk.SpecFlow.Specs/Drivers/XUnitTestExecutionDriver.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace TechTalk.SpecFlow.Specs.Drivers
+{
+    public class XUnitTestExecutionDriver
+    {
+        private readonly InputProjectDriver inputProjectDriver;
+        private readonly TestExecutionResult testExecutionResult;
+
+        public XUnitTestExecutionDriver(InputProjectDriver inputProjectDriver, TestExecutionResult testExecutionResult)
+        {
+            this.inputProjectDriver = inputProjectDriver;
+            this.testExecutionResult = testExecutionResult;
+        }
+
+        public TestRunSummary Execute()
+        {
+            string resultFilePath = Path.Combine(inputProjectDriver.DeploymentFolder, "nunit-result.xml");
+            string logFilePath = Path.Combine(inputProjectDriver.DeploymentFolder, "nunit-result.txt");
+
+            var args = new List<string>
+            {
+                inputProjectDriver.CompiledAssemblyPath,
+                "/nunit",
+                resultFilePath
+            };
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("xunit.console.clr4.exe", string.Join(" ", args))
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                }
+            };
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            File.WriteAllText(logFilePath, output);
+
+            return ProcessNUnitResult(logFilePath, resultFilePath);
+        }
+
+        private TestRunSummary ProcessNUnitResult(string logFilePath, string resultFilePath)
+        {
+            XDocument resultFileXml = XDocument.Load(resultFilePath);
+
+            TestRunSummary summary = new TestRunSummary();
+
+            summary.Total = resultFileXml.XPathSelectElements("//test-case").Count();
+            summary.Succeeded = resultFileXml.XPathSelectElements("//test-case[@executed = 'True' and @success='True']").Count();
+            summary.Failed =
+                resultFileXml.XPathSelectElements("//test-case[@executed = 'True' and @success='False' and failure]").Count();
+            summary.Pending =
+                resultFileXml.XPathSelectElements("//test-case[@executed = 'True' and @success='False' and not(failure)]").Count
+                    ();
+            summary.Ignored = resultFileXml.XPathSelectElements("//test-case[@executed = 'False']").Count();
+
+            testExecutionResult.LastExecutionSummary = summary;
+            testExecutionResult.ExecutionLog = File.ReadAllText(logFilePath);
+
+            Console.WriteLine("NUnit (from xUnit) LOG:");
+            Console.WriteLine(testExecutionResult.ExecutionLog);
+
+            return summary;
+        }
+    }
+}
