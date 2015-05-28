7f667a914b3f2695d12167f90fdbe2102bd826c3
Gaspar Nagy <gaspar.nagy@gmail.com>
2015-05-28 21:55:17 +0200

    make hooks driver more robust

--- Tests/TechTalk.SpecFlow.Specs/Drivers/HooksDriver.cs
+++ Tests/TechTalk.SpecFlow.Specs/Drivers/HooksDriver.cs
@@ -4,32 +4,42 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TechTalk.SpecFlow.Specs.Drivers
 {
     public class HooksDriver
     {
-        private readonly InputProjectDriver inputProjectDriver;
-
         private Lazy<string> hookLog;
+        private bool isInitialized = false;
 
         public string HookLogPath { get; private set; }
         public string HookLog { get { return hookLog.Value; } }
 
         public HooksDriver(InputProjectDriver inputProjectDriver)
         {
-            this.inputProjectDriver = inputProjectDriver;
             HookLogPath = Path.Combine(inputProjectDriver.DeploymentFolder, "hooks.log");
-
-            hookLog = new Lazy<string>(() => File.ReadAllText(HookLogPath));
-
-            //reset file
-            File.WriteAllText(HookLogPath, "");
+            hookLog = new Lazy<string>(() =>
+            {
+                EnsureInitialized();
+                return File.ReadAllText(HookLogPath);
+            });
         }
 
         public string GetHookLogStatement(string methodName)
         {
+            EnsureInitialized();
             return string.Format(@"System.IO.File.AppendAllText(@""{1}"", ""-> hook: {0}"");", methodName, HookLogPath);
+        }
+
+        private void EnsureInitialized()
+        {
+            if (isInitialized)
+                return;
+
+            //reset file
+            File.WriteAllText(HookLogPath, "");
+            isInitialized = true;
         }
     }
 }
