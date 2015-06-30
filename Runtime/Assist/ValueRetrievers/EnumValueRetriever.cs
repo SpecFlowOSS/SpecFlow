85d41960312ba8777e1a0055667a71b018d93884
Darren Cauthon <darren@cauthon.com>
2015-06-30 06:54:11 -0500

    Enum retriever should check for enums.

--- Runtime/Assist/ValueRetrievers/EnumValueRetriever.cs
+++ Runtime/Assist/ValueRetrievers/EnumValueRetriever.cs
@@ -19,6 +19,11 @@ namespace TechTalk.SpecFlow.Assist.ValueRetrievers
             return GetValue(row[1], propertyType);
         }
 
+        public override bool CanRetrieve(Type type)
+        {
+            return type.IsEnum;
+        }
+
         public override IEnumerable<Type> TypesForWhichIRetrieveValues()
         {
             return new Type[]{ typeof(Enum) };
