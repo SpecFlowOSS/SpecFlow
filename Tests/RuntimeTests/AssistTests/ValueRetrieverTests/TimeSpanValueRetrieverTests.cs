35253ded1dd8a8f0f82ecdf33859b43609a9b439
Darren Cauthon <darren@cauthon.com>
2015-10-10 22:21:37 -0500

    Create a timespan value retriever.

---
+++ Tests/RuntimeTests/AssistTests/ValueRetrieverTests/TimeSpanValueRetrieverTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TechTalk.SpecFlow.Assist.ValueRetrievers;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.ValueRetrieverTests
+{
+    [TestFixture]
+    public class TimespanValueRetrieverTests
+    {
+        [Test]
+        public void Returns_an_integer_when_passed_an_integer_value()
+        {
+            var retriever = new TimeSpanValueRetriever();
+            retriever.GetValue("20:00:00").Should().Be(System.TimeSpan.Parse("20:00:00"));
+            retriever.GetValue("20:00:01").Should().Be(System.TimeSpan.Parse("20:00:01"));
+            retriever.GetValue("00:00:00").Should().Be(System.TimeSpan.Parse("00:00:00"));
+        }
+    }
+}
