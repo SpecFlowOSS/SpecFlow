5006d89184a4d453cabf429f3f17d4b02d2f68ac
Darren Cauthon <darren@cauthon.com>
2015-10-26 08:54:06 -0500

    Clean up the tests.

--- Tests/RuntimeTests/AssistTests/ValueRetrieverTests/StepTransformationValueRetrieverTests.cs
+++ Tests/RuntimeTests/AssistTests/ValueRetrieverTests/StepTransformationValueRetrieverTests.cs
@@ -14,33 +14,53 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.ValueRetrieverTests
     [TestFixture]
     public class StepTransformationValueRetrieverTests
     {
+
         [Test]
         public void Convert_will_return_the_value_from_the_step_argument_type_converter()
         {
-            var retriever = new StepTransformationValueRetriever();
+            var dateTimeResult = Subject().Retrieve(KeyValueFor("2009/10/06"), typeof(DateTime));
+            dateTimeResult.Should().Be(new DateTime(2009, 10, 6));
 
+            var stringResult = Subject().Retrieve(KeyValueFor("2009/10/06"), typeof(string));
+            stringResult.Should().Be("2009/10/06");
+        }
+
+        private KeyValuePair<string, string> KeyValueFor(string value)
+        {
+            // retrieving a value requires a key->value set, but this class
+            // does not need the key... so we pass nothing for our tests
+            return new System.Collections.Generic.KeyValuePair<string, string> ("", value);
+        }
+
+        private StepTransformationValueRetriever Subject()
+        {
+            // have to set up a container with a bunch of mocked stuff
+            // in order to build a StepArgumentTypeConverter
             var container = new BoDi.ObjectContainer();
+
             var provider = new TechTalk.SpecFlow.Infrastructure.DefaultDependencyProvider();
             provider.RegisterTestRunnerDefaults(container);
 
             Mock<IBindingRegistry> bindingRegistryStub = new Mock<IBindingRegistry>();
             List<IStepArgumentTransformationBinding> stepTransformations = new List<IStepArgumentTransformationBinding>();
             bindingRegistryStub.Setup(br => br.GetStepTransformations()).Returns(stepTransformations);
-
             Mock<IBindingInvoker> methodBindingInvokerStub = new Mock<IBindingInvoker>();
 
             var stepArgumentTypeConverter = new StepArgumentTypeConverter(new Mock<ITestTracer>().Object, bindingRegistryStub.Object, new Mock<IContextManager>().Object, methodBindingInvokerStub.Object);
+
+            // need a culture info as well
             var cultureInfo = new CultureInfo("en-US");
 
+            // load them into the container
             container.RegisterInstanceAs<IStepArgumentTypeConverter>(stepArgumentTypeConverter);
             container.RegisterInstanceAs<CultureInfo>(cultureInfo);
 
-            var hit = stepArgumentTypeConverter.Convert("testValue", typeof(string), cultureInfo);
+            // now we have what we need to get a our subject, loaded
+            // with a fake source for all of its dependencies
+            var retriever = new StepTransformationValueRetriever();
             retriever.Container = container;
-
-            var result = retriever.Retrieve(new System.Collections.Generic.KeyValuePair<string, string>("", "2009/10/06"), typeof(DateTime));
-
-            result.Should().Be(new DateTime(2009, 10, 6));
+            return retriever;
         }
+
     }
 }
