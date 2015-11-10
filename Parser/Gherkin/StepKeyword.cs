9fc209318debb5bd3a069b067d5bceb2b6de3f7a
Gaspar Nagy <gaspar.nagy@gmail.com>
2015-11-10 12:22:57 +0100

    Remove unused parser classes

---
+++ Parser/Gherkin/StepKeyword.cs
@@ -0,0 +1,11 @@
+﻿namespace TechTalk.SpecFlow.Parser.Gherkin
+{
+    public enum StepKeyword
+    {
+        Given = 1,
+        When = 2,
+        Then = 3,
+        And = 4,
+        But = 5
+    }
+}
