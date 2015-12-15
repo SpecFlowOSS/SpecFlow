680cad6f627b5db5007bf0a94d5dac4ff0991979
Darren Cauthon <darren@cauthon.com>
2015-12-15 06:54:28 -0600

    Fix the documentation for CanRetrieve.

--- Runtime/Assist/IValueRetriever.cs
+++ Runtime/Assist/IValueRetriever.cs
@@ -13,8 +13,8 @@ namespace TechTalk.SpecFlow.Assist
         /// </summary>
         /// <returns><c>true</c> if this instance can retrieve the specified key->value; otherwise, <c>false</c>.</returns>
         /// <param name="keyValuePair">Key value pair.</param>
-        /// <param name="type">The type of the object that is being built from the table.</param>
-        bool CanRetrieve(KeyValuePair<string, string> keyValuePair, Type type);
+        /// <param name="propertyType">The type of the property or member that is being set.</param>
+        bool CanRetrieve(KeyValuePair<string, string> keyValuePair, Type propertyType);
 
         /// <summary>
         /// Retrieve the value from a key-> value set, as the expected type on targetType.
