832deb0ed14ae43c374e8e36c4298800c1d80721
Darren Cauthon <darren@cauthon.com>
2015-12-23 06:48:37 -0600

    No need for that anymore.

--- Runtime/Assist/SetComparisonExtensionMethods.cs
+++ Runtime/Assist/SetComparisonExtensionMethods.cs
@@ -7,7 +7,6 @@ namespace TechTalk.SpecFlow.Assist
         public static void CompareToSet<T>(this Table table, IEnumerable<T> set)
         {
             var tableService = new TableService(Config.Instance);
-            var tableCreationLogic = new TableCreationLogic(Config.Instance);
             new TableComparisonLogic(tableService).CompareToSet(table, set);
         }
     }
