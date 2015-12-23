6658f6ed84fc3f45fa6ee64965d39b9d5c4a3fa2
Darren Cauthon <darren@cauthon.com>
2015-12-23 08:28:41 -0600

    Move this to its own file.

---
+++ Runtime/Assist/ITableService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechTalk.SpecFlow.Assist
+{
+    public interface ITableService
+    {
+        void CompareToSet<T>(Table table, IEnumerable<T> set);
+        void CompareToInstance<T>(Table table, T instance);
+        T CreateInstance<T>(Table table);
+        IEnumerable<T> CreateSet<T>(Table table, Func<T> methodToCreateEachInstance);
+        IEnumerable<T> CreateSet<T>(Table table);
+    }
+}
--- Runtime/Assist/TableService.cs
+++ Runtime/Assist/TableService.cs
@@ -6,15 +6,6 @@ using System.Text.RegularExpressions;
 
 namespace TechTalk.SpecFlow.Assist
 {
-    public interface ITableService
-    {
-        void CompareToSet<T>(Table table, IEnumerable<T> set);
-        void CompareToInstance<T>(Table table, T instance);
-        T CreateInstance<T>(Table table);
-        IEnumerable<T> CreateSet<T>(Table table, Func<T> methodToCreateEachInstance);
-        IEnumerable<T> CreateSet<T>(Table table);
-    }
-
     public class TableService : ITableService
     {
         private readonly Config config;
