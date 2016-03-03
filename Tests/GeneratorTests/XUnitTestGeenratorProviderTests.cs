4e9e3352ea494ee39fe7556563a062e71401c540
Thibault Nélis <t.nelis@csweng.com>
2016-03-03 10:16:43 +0100

    Set DisplayName argument of Xunit.Fact to friendly test name

--- Tests/GeneratorTests/XUnitTestGeenratorProviderTests.cs
+++ Tests/GeneratorTests/XUnitTestGeenratorProviderTests.cs
@@ -41,5 +41,57 @@ namespace TechTalk.SpecFlow.GeneratorTests
             Assert.That(primitiveExpression, Is.Not.Null);
             Assert.That(primitiveExpression.Value, Is.EqualTo(XUnitTestGeneratorProvider.SKIP_REASON));
         }
+
+        /*
+         * Based on w1ld's `Should_set_skip_attribute_for_theory`,
+         * refactor as appropriate.
+         */
+        [Test]
+        public void Should_set_displayname_attribute()
+        {
+            // Arrange
+            var provider = new XUnitTestGeneratorProvider(new CodeDomHelper(new CSharpCodeProvider())); // TODO: what about XUnit2TestGeneratorProvider ?
+            var context = new Generator.TestClassGenerationContext(
+                unitTestGeneratorProvider: null,
+                feature: new Parser.SpecFlowFeature(
+                    tags: null,
+                    location: null,
+                    language: null,
+                    keyword: null,
+                    name: "",
+                    description: null,
+                    background: null,
+                    scenarioDefinitions: null,
+                    comments: null,
+                    sourceFilePath: null),
+                ns: null,
+                testClass: null,
+                testRunnerField: null,
+                testClassInitializeMethod: null,
+                testClassCleanupMethod: null,
+                testInitializeMethod: null,
+                testCleanupMethod: null,
+                scenarioInitializeMethod: null,
+                scenarioCleanupMethod: null,
+                featureBackgroundMethod: null,
+                generateRowTests: false);
+            var codeMemberMethod = new CodeMemberMethod();
+
+            // Act
+            provider.SetTestMethod(context, codeMemberMethod, "Foo");
+
+            // Assert
+            var modifiedAttribute = codeMemberMethod.CustomAttributes.OfType<CodeAttributeDeclaration>()
+                .FirstOrDefault(a => a.Name == "Xunit.FactAttribute");
+
+            Assert.That(modifiedAttribute, Is.Not.Null);
+            var attribute = modifiedAttribute.Arguments.OfType<CodeAttributeArgument>()
+                .FirstOrDefault(a => a.Name == "DisplayName");
+            Assert.That(attribute, Is.Not.Null);
+
+            var primitiveExpression = attribute.Value as CodePrimitiveExpression;
+            Assert.That(primitiveExpression, Is.Not.Null);
+            Assert.That(primitiveExpression.Value, Is.EqualTo("Foo"));
+        }
     }
 }
