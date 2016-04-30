863e16f1adcfa259fab469203887be3f9bb2f1b1
Darren Cauthon <darren@cauthon.com>
2016-04-30 06:36:15 -0500

    Resharper refactors.

--- Runtime/Assist/FindInSetExtensionMethods.cs
+++ Runtime/Assist/FindInSetExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TechTalk.SpecFlow.Assist
 {
@@ -6,29 +7,17 @@ namespace TechTalk.SpecFlow.Assist
     {
         public static T FindInSet<T>(this Table table, IEnumerable<T> set)
         {
-            var instanceTable = TEHelpers.GetTheProperInstanceTable(table, typeof(T));
+            var instanceTable = TEHelpers.GetTheProperInstanceTable(table, typeof (T));
 
-            foreach (var instance in set)
-            {
-                if (InstanceMatchesTable(instance, instanceTable))
-                {
-                    return instance;
-                }
-            }
+            foreach (var instance in set.Where(instance => InstanceMatchesTable(instance, instanceTable)))
+                return instance;
 
             throw new ComparisonException("No instance in the set matches the table");
         }
 
         private static bool InstanceMatchesTable<T>(T instance, Table table)
         {
-            foreach (var row in table.Rows)
-            {
-                if (InstanceComparisonExtensionMethods.ThereIsADifference(instance, row))
-                {
-                    return false;
-                }
-            }
-            return true;
+            return table.Rows.All(row => !InstanceComparisonExtensionMethods.ThereIsADifference(instance, row));
         }
     }
 }
