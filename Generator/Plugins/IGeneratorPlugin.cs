5c58f2fbbf603e22d24188d099332b708b209d07
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-05-13 15:58:15 +0200

    cleanup IGeneratorPlugin

--- Generator/Plugins/IGeneratorPlugin.cs
+++ Generator/Plugins/IGeneratorPlugin.cs
@@ -1,14 +1,7 @@
-using BoDi;
-using TechTalk.SpecFlow.Generator.Configuration;
-
 namespace TechTalk.SpecFlow.Generator.Plugins
 {
     public interface IGeneratorPlugin
     {
         void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters);
-
-        //void RegisterDependencies(ObjectContainer container);
-        //void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration specFlowProjectConfiguration);
-        //void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration);
     }
 }
