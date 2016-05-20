ef3485b29bab5755df7dcda3c02d51bf4f79d245
Mike Jones <mike.jones@abellearning.com>
2016-05-20 10:29:10 +0100

    Update project to use the newer adding manifest system

---
+++ IdeIntegration/XamarinStudioIntegration/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"TechTalk.SpecFlow",
+	Namespace = "TechTalk.SpecFlow",
+	Version = "1.9.1.0"
+)]
+
+[assembly: AddinName("SpecFlow Support")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Integrates SpecFlow into the Xamarin Studio IDE.")]
+[assembly: AddinAuthor("TechTalk, SineSignal, Mike Jones")]
+
