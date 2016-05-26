204260816e5dd7eb0b0e6282a5d045bc8053e96b
Dirk Rombauts <dirk.rombauts@questmaster.net>
2016-05-26 20:36:35 +0200

    Remove SpecFlowTestProject after the test run

---
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/AfterScenarioHooks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Specs.Drivers;
+
+namespace TechTalk.SpecFlow.Specs.StepDefinitions
+{
+    [Binding]
+    public sealed class AfterScenarioHooks
+    {
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            var directoryForTestProjects = InputProjectDriver.DetermineDirectoryForTestProjects();
+
+            if (Directory.Exists(directoryForTestProjects))
+            {
+                Directory.Delete(directoryForTestProjects, true);
+            }
+        }
+    }
+}
