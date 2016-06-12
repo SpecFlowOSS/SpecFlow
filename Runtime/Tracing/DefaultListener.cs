ad19b5f2ae94d80ec91cd9119153ca74e9e19380
Darren Cauthon <darren@cauthon.com>
2016-06-12 07:30:26 -0500

    We may want to override how the default trace listener works, but without changing the instance in the container.

--- Runtime/Tracing/DefaultListener.cs
+++ Runtime/Tracing/DefaultListener.cs
@@ -5,14 +5,27 @@ namespace TechTalk.SpecFlow.Tracing
 {
     public class DefaultListener : ITraceListener
     {
+        private ITraceListener traceListener;
+
         public void WriteTestOutput(string message)
         {
-            Console.WriteLine(message);
+            if (traceListener == null)
+                Console.WriteLine(message);
+            else
+                traceListener.WriteTestOutput(message);
         }
 
         public void WriteToolOutput(string message)
         {
-            Console.WriteLine("-> " + message);
+            if (traceListener == null)
+                Console.WriteLine("-> " + message);
+            else
+                traceListener.WriteToolOutput("-> " + message);
+        }
+
+        public void OverrideWith(ITraceListener traceListener)
+        {
+            this.traceListener = traceListener;
         }
     }
 }
