7452c9028ee737de6cafc2c0150d54548d547652
William C. Smith <ocsurfnut@gmail.com>
2016-07-05 17:43:26 -0400

    #542 - Implement Pull Request Suggestions for Testing

--- Tests/TechTalk.SpecFlow.Specs/Features/AsyncSupport.feature
+++ Tests/TechTalk.SpecFlow.Specs/Features/AsyncSupport.feature
@@ -1,11 +1,106 @@
 ﻿Feature: AsyncSupport
 
+Background:
+	Given the following binding class
+		"""
+		using System.Threading.Tasks;
+		using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+		[Binding]
+		public class AsyncSteps
+		{
+			private readonly ScenarioContext scenarioContext;
+
+			public AsyncSteps(ScenarioContext scenarioContext)
+			{
+          
+				this.scenarioContext = scenarioContext;
+			}
+
+			[Given(@"I am calling an async step method")]
+			public async Task GivenIAmCallingAnAsyncStep()
+			{
+				await Task.Delay(250);
+
+				this.scenarioContext["GivenStepCompleted"] = DateTime.Now.Ticks;
+			}
+
+			[Given(@"I am testing a component with async methods")]
+			public void GivenIAmTestingAComponentWithAsyncMethods()
+			{
+				this.scenarioContext["Component"] = new AsyncComponent();
+			}
+
+			[When(@"I call the next async step method")]
+			public async Task WhenICallTheNextAsyncStepMethod()
+			{
+				await Task.Delay(100);
+
+				this.scenarioContext["WhenStepCompleted"] = DateTime.Now.Ticks;
+			}
+
+			[When(@"I call an async method that throws an exception")]
+			public async Task WhenICallAnAsyncMethodThatThrowsAnException()
+			{
+				try
+				{
+					await ((AsyncComponent)this.scenarioContext["Component"]).AsyncMethod();
+				}
+				catch (Exception ex)
+				{
+					this.scenarioContext["Exception"] = ex;
+				}
+			}
+
+			[Then(@"both step methods should have been called in order before I get to the last step method")]
+			public void ThenBothStepMethodsShouldHaveBeenCalledInOrderBeforeIGetToTheLastStepMethod()
+			{
+				Assert.IsTrue(this.scenarioContext.ContainsKey("GivenStepCompleted"));
+				Assert.IsTrue(this.scenarioContext.ContainsKey("WhenStepCompleted"));
+				Assert.IsTrue((long)this.scenarioContext["WhenStepCompleted"] > (long)this.scenarioContext["GivenStepCompleted"]);
+			}
+
+			[Then(@"the exception should be sent back to my step method")]
+			public void ThenTheExceptionShouldBeSentBackToMyStepMethod()
+			{
+				Assert.IsTrue(this.scenarioContext.ContainsKey("Exception"));
+				Assert.AreEqual("This exception should bubble up.", ((Exception)this.scenarioContext["Exception"]).Message);
+			}
+
+			private class AsyncComponent
+			{
+				public async Task AsyncMethod()
+				{
+					await Task.Delay(100);
+
+					throw new Exception("This exception should bubble up.");
+				}
+			}
+		}
+		"""
+
 Scenario: Should wait for aync step methods to complete before continuing to next step
-	Given I am calling an async step method
-	When I call the next async step method
-	Then both step methods should have been called in order before I get to the last step method
+	Given a scenario 'Wait for async step methods to complete before continuing to next step' as
+		"""
+		Given I am calling an async step method
+		When I call the next async step method
+		Then both step methods should have been called in order before I get to the last step method
+
+		"""
+	When I execute the tests
+	Then the execution summary should contain
+         | Succeeded |
+         | 1         |
 
 Scenario: Step method should recieve exception from async method call
-	Given I am testing a component with async methods
-	When I call an async method that throws an exception
-	Then the exception should be sent back to my step method
+	Given a scenario 'Step method should recieve exception from async method call' as
+		"""
+		Given I am testing a component with async methods
+		When I call an async method that throws an exception
+		Then the exception should be sent back to my step method
+
+		"""
+	When I execute the tests
+	Then the execution summary should contain
+         | Succeeded |
+         | 1         |
--- Tests/TechTalk.SpecFlow.Specs/Features/AsyncSupport.feature.cs
+++ Tests/TechTalk.SpecFlow.Specs/Features/AsyncSupport.feature.cs
@@ -64,20 +64,65 @@ namespace TechTalk.SpecFlow.Specs.Features
             testRunner.CollectScenarioErrors();
         }
         
+        public virtual void FeatureBackground()
+        {
+#line 3
+#line hidden
+#line 4
+ testRunner.Given("the following binding class", "using System.Threading.Tasks;\r\nusing Microsoft.VisualStudio.TestTools.UnitTesting" +
+                    ";\r\n\r\n[Binding]\r\npublic class AsyncSteps\r\n{\r\n\tprivate readonly ScenarioContext sc" +
+                    "enarioContext;\r\n\r\n\tpublic AsyncSteps(ScenarioContext scenarioContext)\r\n\t{\r\n     " +
+                    "   \r\n\t\tthis.scenarioContext = scenarioContext;\r\n\t}\r\n\r\n\t[Given(@\"I am calling an " +
+                    "async step method\")]\r\n\tpublic async Task GivenIAmCallingAnAsyncStep()\r\n\t{\r\n\t\tawa" +
+                    "it Task.Delay(250);\r\n\r\n\t\tthis.scenarioContext[\"GivenStepCompleted\"] = DateTime.N" +
+                    "ow.Ticks;\r\n\t}\r\n\r\n\t[Given(@\"I am testing a component with async methods\")]\r\n\tpubl" +
+                    "ic void GivenIAmTestingAComponentWithAsyncMethods()\r\n\t{\r\n\t\tthis.scenarioContext[" +
+                    "\"Component\"] = new AsyncComponent();\r\n\t}\r\n\r\n\t[When(@\"I call the next async step " +
+                    "method\")]\r\n\tpublic async Task WhenICallTheNextAsyncStepMethod()\r\n\t{\r\n\t\tawait Tas" +
+                    "k.Delay(100);\r\n\r\n\t\tthis.scenarioContext[\"WhenStepCompleted\"] = DateTime.Now.Tick" +
+                    "s;\r\n\t}\r\n\r\n\t[When(@\"I call an async method that throws an exception\")]\r\n\tpublic a" +
+                    "sync Task WhenICallAnAsyncMethodThatThrowsAnException()\r\n\t{\r\n\t\ttry\r\n\t\t{\r\n\t\t\tawai" +
+                    "t ((AsyncComponent)this.scenarioContext[\"Component\"]).AsyncMethod();\r\n\t\t}\r\n\t\tcat" +
+                    "ch (Exception ex)\r\n\t\t{\r\n\t\t\tthis.scenarioContext[\"Exception\"] = ex;\r\n\t\t}\r\n\t}\r\n\r\n\t" +
+                    "[Then(@\"both step methods should have been called in order before I get to the l" +
+                    "ast step method\")]\r\n\tpublic void ThenBothStepMethodsShouldHaveBeenCalledInOrderB" +
+                    "eforeIGetToTheLastStepMethod()\r\n\t{\r\n\t\tAssert.IsTrue(this.scenarioContext.Contain" +
+                    "sKey(\"GivenStepCompleted\"));\r\n\t\tAssert.IsTrue(this.scenarioContext.ContainsKey(\"" +
+                    "WhenStepCompleted\"));\r\n\t\tAssert.IsTrue((long)this.scenarioContext[\"WhenStepCompl" +
+                    "eted\"] > (long)this.scenarioContext[\"GivenStepCompleted\"]);\r\n\t}\r\n\r\n\t[Then(@\"the " +
+                    "exception should be sent back to my step method\")]\r\n\tpublic void ThenTheExceptio" +
+                    "nShouldBeSentBackToMyStepMethod()\r\n\t{\r\n\t\tAssert.IsTrue(this.scenarioContext.Cont" +
+                    "ainsKey(\"Exception\"));\r\n\t\tAssert.AreEqual(\"This exception should bubble up.\", ((" +
+                    "Exception)this.scenarioContext[\"Exception\"]).Message);\r\n\t}\r\n\r\n\tprivate class Asy" +
+                    "ncComponent\r\n\t{\r\n\t\tpublic async Task AsyncMethod()\r\n\t\t{\r\n\t\t\tawait Task.Delay(100" +
+                    ");\r\n\r\n\t\t\tthrow new Exception(\"This exception should bubble up.\");\r\n\t\t}\r\n\t}\r\n}", ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+        }
+        
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("Should wait for aync step methods to complete before continuing to next step")]
         public virtual void ShouldWaitForAyncStepMethodsToCompleteBeforeContinuingToNextStep()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Should wait for aync step methods to complete before continuing to next step", ((string[])(null)));
-#line 3
+#line 82
 this.ScenarioSetup(scenarioInfo);
-#line 4
- testRunner.Given("I am calling an async step method", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line 5
- testRunner.When("I call the next async step method", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 6
- testRunner.Then("both step methods should have been called in order before I get to the last step " +
-                    "method", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 3
+this.FeatureBackground();
+#line hidden
+#line 83
+ testRunner.Given("a scenario \'Wait for async step methods to complete before continuing to next ste" +
+                    "p\' as", "Given I am calling an async step method\r\nWhen I call the next async step method\r\n" +
+                    "Then both step methods should have been called in order before I get to the last" +
+                    " step method\r\n", ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 90
+ testRunner.When("I execute the tests", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+            TechTalk.SpecFlow.Table table1 = new TechTalk.SpecFlow.Table(new string[] {
+                        "Succeeded"});
+            table1.AddRow(new string[] {
+                        "1"});
+#line 91
+ testRunner.Then("the execution summary should contain", ((string)(null)), table1, "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
@@ -87,14 +132,24 @@ this.ScenarioSetup(scenarioInfo);
         public virtual void StepMethodShouldRecieveExceptionFromAsyncMethodCall()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Step method should recieve exception from async method call", ((string[])(null)));
-#line 8
+#line 95
 this.ScenarioSetup(scenarioInfo);
-#line 9
- testRunner.Given("I am testing a component with async methods", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line 10
- testRunner.When("I call an async method that throws an exception", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 11
- testRunner.Then("the exception should be sent back to my step method", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line 3
+this.FeatureBackground();
+#line hidden
+#line 96
+ testRunner.Given("a scenario \'Step method should recieve exception from async method call\' as", "Given I am testing a component with async methods\nWhen I call an async method tha" +
+                    "t throws an exception\nThen the exception should be sent back to my step method\r\n" +
+                    "", ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 103
+ testRunner.When("I execute the tests", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+            TechTalk.SpecFlow.Table table2 = new TechTalk.SpecFlow.Table(new string[] {
+                        "Succeeded"});
+            table2.AddRow(new string[] {
+                        "1"});
+#line 104
+ testRunner.Then("the execution summary should contain", ((string)(null)), table2, "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
