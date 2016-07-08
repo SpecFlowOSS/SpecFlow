39b3525c872e91ade20cc956840198d25adf7c49
Andreas Willich <Andreas.Willich@techtalk.at>
2016-07-08 21:09:33 +0200

    update to NUnit 3.4.0 to use dotnet-test-nunit

--- runtests.bat
+++ runtests.bat
@@ -1,10 +1,10 @@
-rem NuGet\custom\NUnit3-Runner\bin\nunit-console.exe Tests\GeneratorTests\bin\Debug\net45\TechTalk.SpecFlow.GeneratorTests.dll
-rem if ERRORLEVEL 1 goto :eof
-rem NuGet\custom\NUnit3-Runner\bin\nunit-console.exe Tests\RuntimeTests\bin\Debug\net45\TechTalk.SpecFlow.RuntimeTests.dll
-rem if ERRORLEVEL 1 goto :eof
-NuGet\custom\NUnit3-Runner\bin\nunit-console.exe Tests\TechTalk.SpecFlow.Specs\bin\Debug\net45\TechTalk.SpecFlow.Specs.dll
-rem if ERRORLEVEL 1 goto :eof
-rem NuGet\custom\NUnit3-Runner\bin\nunit-console.exe Tests\TechTalk.SpecFlow.IntegrationTests\bin\Debug\net45\TechTalk.SpecFlow.IntegrationTests.dll
-rem if ERRORLEVEL 1 goto :eof
+dotnet.exe test "C:\work\SpecFlow\Tests\GeneratorTests\project.json"
+if ERRORLEVEL 1 goto :eof
+dotnet.exe test "C:\work\SpecFlow\Tests\RuntimeTests\project.json"
+if ERRORLEVEL 1 goto :eof
+dotnet.exe test "C:\work\SpecFlow\Tests\TechTalk.SpecFlow.Specs\project.json"
+if ERRORLEVEL 1 goto :eof
+dotnet.exe test "C:\work\SpecFlow\Tests\TechTalk.SpecFlow.IntegrationTests\project.json"
+if ERRORLEVEL 1 goto :eof
 
-rem echo "All tests passed!"
+echo "All tests passed!"
