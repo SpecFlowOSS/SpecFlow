82f5be4679b9821af986fa1f45dde2820bc63fd1
Andreas Willich <andreas.willich@techtalk.at>
2016-11-04 11:46:34 +0100

    introduce PoCos for DataTable, DocString, TableRow and TableCell to can be serialized by XmlSerializer

---
+++ TechTalk.SpecFlow.Reporting/StepDefinitionReport/ReportElements/ReportStepArgument.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace TechTalk.SpecFlow.Reporting.StepDefinitionReport.ReportElements
+{
+    [XmlType(Namespace = StepDefinitionReport.XmlNamespace)]
+    [XmlInclude(typeof(TableArgument))]
+    [XmlInclude(typeof(DocStringArgument))]
+    public abstract class ReportStepArgument
+    {
+        
+    }
+}
