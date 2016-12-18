05b09e7014e1bc2ae5c5eec0b0bd6ae9135f75d8
Peter-B- <peter@butzhammer.de>
2016-12-18 20:31:25 +0100

    moved unit tests into separate file

--- Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/TableHelperExtensionMethods/CreateInstanceHelperMethodTestsWithFuncAndTableArgument.cs
+++ Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/TableHelperExtensionMethods/CreateInstanceHelperMethodTestsWithFuncAndTableArgument.cs
@@ -1,19 +1,21 @@
-﻿using NUnit.Framework;
+using System;
+using System.Linq;
 using FluentAssertions;
+using NUnit.Framework;
 using TechTalk.SpecFlow.Assist;
 using TechTalk.SpecFlow.RuntimeTests.AssistTests.ExampleEntities;
 
 namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.TableHelperExtensionMethods
 {
     [TestFixture]
-    public class CreateInstanceHelperMethodTests_WithFunc
+    public class CreateInstanceHelperMethodTestsWithFuncAndTableArgument
     {
         [Test]
         public void CreateInstance_returns_the_object_returned_from_the_func()
         {
             var table = new Table("Field", "Value");
             var expectedPerson = new Person();
-            var person = table.CreateInstance(() => expectedPerson);
+            var person = table.CreateInstance(tbl => expectedPerson);
             person.Should().Be(expectedPerson);
         }
 
@@ -25,10 +27,26 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.TableHelperExtensionMethods
             table.AddRow("LastName", "Galt");
 
             var expectedPerson = new Person { FirstName = "Ellsworth", LastName = "Toohey" };
-            var person = table.CreateInstance(() => expectedPerson);
+            var person = table.CreateInstance(tbl => expectedPerson);
 
             person.FirstName.Should().Be("John");
             person.LastName.Should().Be("Galt");
         }
+
+        [Test]
+        public void Create_instance_use_the_correct_table_in_callback()
+        {
+            var table = new Table("Field", "Value");
+
+            Table tableFromCallback = null;
+            var person = table.CreateInstance(tbl =>
+            {
+                tableFromCallback = tbl;
+                return new Person();
+            });
+
+            tableFromCallback.Should().Be(table);
+        }
+
     }
 }
