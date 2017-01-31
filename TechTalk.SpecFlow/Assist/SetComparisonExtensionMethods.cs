8f48b6a038b74209f7864022e2b52c730a27daa2
Gaspar Nagy <gaspar.nagy@gmail.com>
2017-01-31 16:39:43 +0100

    Add tests

--- TechTalk.SpecFlow/Assist/SetComparisonExtensionMethods.cs
+++ TechTalk.SpecFlow/Assist/SetComparisonExtensionMethods.cs
@@ -4,7 +4,7 @@ namespace TechTalk.SpecFlow.Assist
 {
     public static class SetComparisonExtensionMethods
     {
-        public static void CompareToSet<T>(this Table table, IEnumerable<T> set)
+        public static void CompareToSet<T>(this Table table, IEnumerable<T> set, bool sequentialEquality = false)
         {
             var checker = new SetComparer<T>(table);
             checker.CompareToSet(set);
