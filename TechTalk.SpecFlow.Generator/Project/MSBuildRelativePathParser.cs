bd1dec3d306b2eb1bcf7b85fa1c1ec58c4d33967
Andreas Willich <SabotageAndi@users.noreply.github.com>
2017-05-02 14:48:46 +0200

    Wildcard support for project reader (#843)
    * Wildcard support for project reader
    * fix tests for AppVeyor

---
+++ TechTalk.SpecFlow.Generator/Project/MSBuildRelativePathParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TechTalk.SpecFlow.Generator.Project
+{
+    public interface IMSBuildRelativePathParser
+    {
+        List<string> GetFiles(string projectFolder, string path);
+    }
+
+    class MSBuildRelativePathParser : IMSBuildRelativePathParser
+    {
+        public List<string> GetFiles(string projectFolder, string path)
+        {
+            var searchOption = path.Contains("**") ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            if (path.Contains("*"))
+            {
+                var split = path.Split(new [] { "*"}, StringSplitOptions.RemoveEmptyEntries);
+                var realPath = split.Take(split.Length - 1).Aggregate((current, next) => current + next).Replace("\\\\", "\\");
+                var fileName = split.Last();
+
+                return Directory.GetFiles(Path.Combine(projectFolder, realPath), "*" + fileName, searchOption)
+                    .Select(i => i.Replace(projectFolder + Path.DirectorySeparatorChar, ""))
+                    .ToList();
+            }
+
+            return new List<string>() {path};
+        }
+    }
+}
