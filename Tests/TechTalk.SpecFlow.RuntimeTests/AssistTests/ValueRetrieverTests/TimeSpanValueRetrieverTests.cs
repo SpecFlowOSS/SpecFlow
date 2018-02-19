312434c937041ae41a9d0a8d8ce664ac76b9be38
Thomas Fresneau <thom.fresneau@gmail.com>
2018-02-19 11:45:02 +0100

    Use current culture to retrieve TimeSpan value

--- Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/ValueRetrieverTests/TimeSpanValueRetrieverTests.cs
+++ Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/ValueRetrieverTests/TimeSpanValueRetrieverTests.cs
@@ -7,16 +7,24 @@ namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.ValueRetrieverTests
     [TestFixture]
     public class TimespanValueRetrieverTests
     {
-        [Test]
+        [Test, SetCulture("en-US")]
         public void Returns_a_timespan_when_passed_a_parsable_string_representation_of_a_timespan()
         {
             var retriever = new TimeSpanValueRetriever();
             retriever.GetValue("20:00:00").Should().Be(System.TimeSpan.Parse("20:00:00"));
             retriever.GetValue("20:00:01").Should().Be(System.TimeSpan.Parse("20:00:01"));
             retriever.GetValue("00:00:00").Should().Be(System.TimeSpan.Parse("00:00:00"));
-        }
+	        retriever.GetValue("6:12:14:45.3448").Should().Be(System.TimeSpan.Parse("6.12:14:45.3448000"));
+		}
 
-        [Test]
+	    [Test, SetCulture("fr-FR")]
+	    public void Returns_a_timespan_when_passed_a_parsable_string_representation_of_a_timespan_and_culture_is_fr_FR()
+	    {
+		    var retriever = new TimeSpanValueRetriever();
+		    retriever.GetValue("6:12:14:45,3448").Should().Be(System.TimeSpan.Parse("6.12:14:45.3448000"));
+	    }
+
+		[Test]
         public void It_handles_timespans()
         {
             var retriever = new TimeSpanValueRetriever();
