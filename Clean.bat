16623ece58f6be4c0466fcd699b238f8fc9390f6
Andreas Willich <andreas.willich@techtalk.at>
2018-04-19 09:08:22 +0200

    remove outdated files

--- Clean.bat
+++
@@ -1,2 +0,0 @@
-rem for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s /q "%%d"
-del /s project.lock.json
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-dotnet restore
-dotnet build Tools
-dotnet build Tests/GeneratorTests
-dotnet build Tests/RuntimeTests
-dotnet build Tests/TechTalk.SpecFlow.Specs
-dotnet build Tests/TechTalk.SpecFlow.IntegrationTests
