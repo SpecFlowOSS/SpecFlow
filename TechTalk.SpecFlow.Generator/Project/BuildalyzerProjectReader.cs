77d555e91c41883c44db9e4a1e65acc8d82fcab1
Andrey Kurdyumov <kant2002@gmail.com>
2018-04-25 13:48:29 +0600

    Rewrite MSBuild reader to use MSBuild API (#1125)
    * Rewrite MSBuild reader to use MSBuild API
    - Control usage of new implementation via SPECFLOW_USE_MSBUILDAPI to be able test both implementations side-by-side
    - Update TechTalk.SpecFlow.Generator to net46 to be able use MSBuild API, and propagate changes where required across projects which depends on the TechTalk.SpecFlow.Generator
    - Update MSBuild API to 15.6. This change may be not needed, but, I strongly suspect that it will play more nicely with all version of .NET Core up to not yet released .NET Core 2.1
    - Add tests on the exclude folder
    - Change test a bit to take into account that paths `.\filename` and `filename` appear as two items via MSBuild API, and I dont think that duplicate items is desired behavior
    - Implementation of MSBuildApiProjectReader is just old resurrected implementation using MSBuild API with small changes
    
    See #1029
    
    * Update Generator project to use net461 target framework
    
    * Comment out not working test projects
    
    * Use Buildalizer for loading projects
    
    * Remove XML project reader completely
    This is justified by fact that supporting it, means reimplementing whole MSBuild logic, which is not reasonable. Also for loading plugins would be needed read PackageReferences from the projects, and this would be problematic implement correctly. Current implementation is also flawled. See #1102, and wellknown fact that Exclude property on the MSBuild items is not supported

---
+++ TechTalk.SpecFlow.Generator/Project/BuildalyzerProjectReader.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Buildalyzer;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Interfaces;
+
+namespace TechTalk.SpecFlow.Generator.Project
+{
+    public class BuildalyzerProjectReader : ISpecFlowProjectReader
+    {
+        private readonly IGeneratorConfigurationProvider configurationLoader;
+
+        public BuildalyzerProjectReader(IGeneratorConfigurationProvider configurationLoader)
+        {
+            this.configurationLoader = configurationLoader;
+        }
+
+        public SpecFlowProject ReadSpecFlowProject(string projectFilePath)
+        {
+            var manager = new AnalyzerManager();
+            var analyzer = manager.GetProject(projectFilePath);
+            var project = analyzer.Project;
+
+            var projectFolder = Path.GetDirectoryName(projectFilePath);
+
+            var specFlowProject = new SpecFlowProject();
+            specFlowProject.ProjectSettings.ProjectFolder = projectFolder;
+            specFlowProject.ProjectSettings.ProjectName = Path.GetFileNameWithoutExtension(projectFilePath);
+            specFlowProject.ProjectSettings.AssemblyName = project.Properties.First(x => x.Name == "AssemblyName").EvaluatedValue;
+            specFlowProject.ProjectSettings.DefaultNamespace = project.Properties.First(x => x.Name == "RootNamespace").EvaluatedValue;
+            specFlowProject.ProjectSettings.ProjectPlatformSettings.Language = GetLanguage(project);
+
+            foreach (var item in project.FeatureFiles())
+            {
+                var featureFile = specFlowProject.GetOrCreateFeatureFile(item.EvaluatedInclude);
+                var ns = item.GetMetadataValue("CustomToolNamespace");
+                if (!String.IsNullOrEmpty(ns))
+                    featureFile.CustomNamespace = ns;
+                if (!specFlowProject.FeatureFiles.Contains(featureFile))
+                {
+                    specFlowProject.FeatureFiles.Add(featureFile);
+                }
+            }
+
+            var appConfigItem = project.ApplicationConfigurationFile();
+            if (appConfigItem != null)
+            {
+                var configFilePath = Path.Combine(projectFolder, appConfigItem.EvaluatedInclude);
+                var configFileContent = File.ReadAllText(configFilePath);
+                var configurationHolder = GetConfigurationHolderFromFileContent(configFileContent);
+                specFlowProject.ProjectSettings.ConfigurationHolder = configurationHolder;
+                specFlowProject.Configuration = configurationLoader.LoadConfiguration(configurationHolder);
+            }
+
+            return specFlowProject;
+        }
+
+        private string GetLanguage(Microsoft.Build.Evaluation.Project project)
+        {
+            if (project.Imports.Any(i => i.ImportingElement.Project.Contains("Microsoft.VisualBasic.targets")))
+                return GenerationTargetLanguage.VB;
+
+            if (project.Imports.Any(i => i.ImportingElement.Project.Contains("Microsoft.CSharp.targets")))
+                return GenerationTargetLanguage.CSharp;
+
+            return GenerationTargetLanguage.CSharp;
+        }
+
+        private static SpecFlowConfigurationHolder GetConfigurationHolderFromFileContent(string configFileContent)
+        {
+            try
+            {
+                var configDocument = new XmlDocument();
+                configDocument.LoadXml(configFileContent);
+
+                return new SpecFlowConfigurationHolder(configDocument.SelectSingleNode("/configuration/specFlow"));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex, "Config load error");
+                return new SpecFlowConfigurationHolder();
+            }
+        }
+    }
+}
