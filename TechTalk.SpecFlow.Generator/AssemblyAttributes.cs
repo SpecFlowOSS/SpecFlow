bfe3d009d3aaff0e0c491766a0f805ecbae5602b
Andreas Willich <andreas.willich@techtalk.at>
2018-04-26 11:42:59 +0200

    fix assembly version generation

--- TechTalk.SpecFlow.Generator/AssemblyAttributes.cs
+++ TechTalk.SpecFlow.Generator/AssemblyAttributes.cs
@@ -1,20 +1,5 @@
-﻿using System.Reflection;
+﻿
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TechTalk.SpecFlow.Generator")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("743e1609-8937-4775-9896-6e8757c6d98d")]
-
-
-
-
 
 #if SPECFLOW_ENABLE_STRONG_NAME_SIGNING
 [assembly: InternalsVisibleTo("TechTalk.SpecFlow.GeneratorTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009BD35D42479A68A533445360CA3149C96BF112221527828DCCC15604830999FAD6391912EDBDF591531C4DE9C45E437A3F648A2A3722D04E5A02BECE96522C71060081A14E1E775DF4B6F84D6DB609E3A20D15956D3FEDBFD77B2A9B0D941ACABCBF7C26B87F5696FE4AADAACA69DCB84E7C733D5FF0E9ECEF46656D19BF52A2")]
