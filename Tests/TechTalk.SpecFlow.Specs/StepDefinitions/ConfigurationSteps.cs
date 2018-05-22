2576b9e3a5dcc9c8301fb9a20fe5f1c71265826c
Andreas Willich <andreas.willich@techtalk.at>
2018-05-22 14:11:24 +0200

    adjust assert

--- Tests/TechTalk.SpecFlow.Specs/StepDefinitions/ConfigurationSteps.cs
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/ConfigurationSteps.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow.TestProjectGenerator.NewApi;
 using TechTalk.SpecFlow.TestProjectGenerator.NewApi.Driver;
 using TechTalk.SpecFlow.TestProjectGenerator.NewApi._5_TestRun;
 
@@ -9,17 +11,26 @@ namespace TechTalk.SpecFlow.Specs.StepDefinitions
     {
         private readonly VSTestExecutionDriver _vstestExecutionDriver;
         private readonly ConfigurationDriver _configurationDriver;
+        private readonly TestRunConfiguration _testRunConfiguration;
 
-        public ConfigurationSteps(VSTestExecutionDriver vstestExecutionDriver, ConfigurationDriver configurationDriver)
+        public ConfigurationSteps(VSTestExecutionDriver vstestExecutionDriver, ConfigurationDriver configurationDriver, TestRunConfiguration testRunConfiguration)
         {
             _vstestExecutionDriver = vstestExecutionDriver;
             _configurationDriver = configurationDriver;
+            _testRunConfiguration = testRunConfiguration;
         }
 
         [Then(@"the app\.config is used for configuration")]
         public void ThenTheApp_ConfigIsUsedForConfiguration()
         {
-            _vstestExecutionDriver.LastTestExecutionResult.Output.Should().Contain("Using app.config");
+            if (_testRunConfiguration.UnitTestProvider == TestProjectGenerator.UnitTestProvider.MSTest)
+            {
+                _vstestExecutionDriver.LastTestExecutionResult.TestResults.Where(tr => tr.StdOut.Contains("Using app.config")).Should().NotBeEmpty();
+            }
+            else
+            {
+                _vstestExecutionDriver.LastTestExecutionResult.Output.Should().Contain("Using app.config");
+            }
         }
 
         [Then(@"the specflow\.json is used for configuration")]
