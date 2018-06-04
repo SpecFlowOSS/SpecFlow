ff6fc713d4da3f970b4499d5af85487b2b0839b5
Andreas Willich <andreas.willich@techtalk.at>
2018-06-04 14:37:03 +0200

    bring specs again to work

---
+++ TechTalk.SpecFlow/VersionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTalk.SpecFlow
+{
+    public class VersionInfo
+    {
+        public static string AssemblyVersion => ThisAssembly.AssemblyVersion;
+        public static string AssemblyFileVersion => ThisAssembly.AssemblyFileVersion;
+        public static string AssemblyInformationalVersion => ThisAssembly.AssemblyInformationalVersion;
+        public static string NuGetVersion => ThisAssembly.AssemblyInformationalVersion.Replace("+", "-");
+    }
+}
--- Tests/TechTalk.SpecFlow.Specs/Support/Hooks.cs
+++ Tests/TechTalk.SpecFlow.Specs/Support/Hooks.cs
@@ -17,16 +17,19 @@ namespace TechTalk.SpecFlow.Specs.Support
     public class Hooks
     {
         private readonly ScenarioContext _scenarioContext;
-        
+        private readonly CurrentVersionDriver _currentVersionDriver;
 
-        public Hooks(ScenarioContext scenarioContext)
+
+        public Hooks(ScenarioContext scenarioContext, CurrentVersionDriver currentVersionDriver)
         {
             _scenarioContext = scenarioContext;
+            _currentVersionDriver = currentVersionDriver;
         }
 
         [BeforeScenario()]
         public void BeforeScenario()
         {
+            _currentVersionDriver.NuGetVersion = VersionInfo.NuGetVersion;
             _scenarioContext.ScenarioContainer.RegisterTypeAs<OutputConnector, IOutputWriter>();
         }
 
@@ -37,7 +40,7 @@ namespace TechTalk.SpecFlow.Specs.Support
             var folders = new Folders(appConfigDriver);
 
 
-            DeletePackageVersionFolders(folders);
+            DeletePackageVersionFolders();
             DeleteOldTestRunData(folders);
         }
 
@@ -53,15 +56,16 @@ namespace TechTalk.SpecFlow.Specs.Support
             }
         }
 
-        private static void DeletePackageVersionFolders(Folders folders)
+        private static void DeletePackageVersionFolders()
         {
-            
-            var currentVersionDriver = new CurrentVersionDriver(folders, new OutputConnector(new SpecFlowOutputHelper()));
+
+            var currentVersionDriver = new CurrentVersionDriver {NuGetVersion = VersionInfo.NuGetVersion};
+
             string[] packageNames = { "SpecFlow", "SpecFlow.CustomPlugin", "SpecFlow.MsTest", "SpecFlow.NUnit", "SpecFlow.NUnit.Runners", "SpecFlow.Tools.MsBuild.Generation", "SpecFlow.xUnit" };
             
             foreach (var name in packageNames)
             {
-                string hooksPath = Path.Combine(Environment.ExpandEnvironmentVariables("%USERPROFILE%"), ".nuget", "packages", name, currentVersionDriver.GitVersionInfo.NuGetVersion);
+                string hooksPath = Path.Combine(Environment.ExpandEnvironmentVariables("%USERPROFILE%"), ".nuget", "packages", name, currentVersionDriver.NuGetVersion);
                 FileSystemHelper.DeleteFolder(hooksPath);
             }
         }
