576229025022c6bd470a5789e8b2031f0b205410
Andreas Willich <andreas.willich@techtalk.at>
2018-06-06 13:58:47 +0200

    add specs for .NET Core

--- Tests/TechTalk.SpecFlow.Specs.Generator.SpecFlowPlugin/TestRunCombinations.cs
+++ Tests/TechTalk.SpecFlow.Specs.Generator.SpecFlowPlugin/TestRunCombinations.cs
@@ -13,14 +13,17 @@ namespace TechTalk.SpecFlow.Specs.Generator.SpecFlowPlugin
 #if XUNIT_SPECS
             new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "Old", TargetFramework = "Net452", UnitTestProvider = "XUnit"},
             new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "New", TargetFramework = "Net452", UnitTestProvider = "XUnit"},
+            new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "New", TargetFramework = "Netcoreapp20", UnitTestProvider = "XUnit"},
 #endif
 
 #if MSTEST_SPECS
             new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "New", TargetFramework = "Net452", UnitTestProvider = "MSTest"},
+            new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "New", TargetFramework = "Netcoreapp20", UnitTestProvider = "MSTest"},
 #endif
 
 #if NUNIT_SPECS
             new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "New", TargetFramework = "Net452", UnitTestProvider = "NUnit3"},
+            new Combination() {ProgrammingLanguage = "CSharp", ProjectFormat = "New", TargetFramework = "Netcoreapp20", UnitTestProvider = "NUnit3"},
 #endif
         };
     }
