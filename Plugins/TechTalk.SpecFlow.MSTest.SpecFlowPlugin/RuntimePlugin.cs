39f30ee18d0ef8ab7723500a90f25c7a4b83f95a
Balázs Epresi <epresibalazs@gmail.com>
2018-06-18 15:03:29 +0200

    resolves #1176 (#1178)

---
+++ Plugins/TechTalk.SpecFlow.MSTest.SpecFlowPlugin/RuntimePlugin.cs
@@ -0,0 +1,18 @@
+﻿using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.MSTest.SpecFlowPlugin;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.Tracing;
+
+
+[assembly: RuntimePlugin(typeof(RuntimePlugin))]
+
+namespace TechTalk.SpecFlow.MSTest.SpecFlowPlugin
+{
+    public class RuntimePlugin : IRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters)
+        {
+            
+        }
+    }
+}
---
+++ Plugins/TechTalk.SpecFlow.NUnit.SpecFlowPlugin/RuntimePlugin.cs
@@ -0,0 +1,18 @@
+﻿using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.NUnit.SpecFlowPlugin;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.Tracing;
+
+
+[assembly: RuntimePlugin(typeof(RuntimePlugin))]
+
+namespace TechTalk.SpecFlow.NUnit.SpecFlowPlugin
+{
+    public class RuntimePlugin : IRuntimePlugin
+    {
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters)
+        {
+            
+        }
+    }
+}
