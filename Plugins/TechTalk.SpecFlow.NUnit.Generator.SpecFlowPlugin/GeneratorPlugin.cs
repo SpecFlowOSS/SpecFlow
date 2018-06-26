7aa28be4b9a23d29c959ad813bc802165174a681
Balázs Epresi <epresibalazs@gmail.com>
2018-06-26 13:04:47 +0200

    Generator for NUnit (#1158)

---
+++ Plugins/TechTalk.SpecFlow.NUnit.Generator.SpecFlowPlugin/GeneratorPlugin.cs
@@ -0,0 +1,17 @@
+﻿using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.Plugins;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly:GeneratorPlugin(typeof(TechTalk.SpecFlow.NUnit.Generator.SpecFlowPlugin.GeneratorPlugin))]
+
+namespace TechTalk.SpecFlow.NUnit.Generator.SpecFlowPlugin
+{
+    public class GeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            unitTestProviderConfiguration.UseUnitTestProvider("nunit");
+        }
+    }
+}
