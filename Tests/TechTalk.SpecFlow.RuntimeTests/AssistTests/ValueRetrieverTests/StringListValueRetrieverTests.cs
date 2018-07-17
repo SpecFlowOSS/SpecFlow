402b08e46c4260587b95c6da72f2e58fac64adc8
inkinjo <jouni.inkinen@lindorff.com>
2018-07-17 11:26:59 +0300

    Array & List support for strings and enums when instantiating class from Specflow table. (#1018)
    * Populate string[] fields when creating instance from table
    
    * Populate List<string> fields when creating instance from table
    
    * Populate enum arrays when creating instance from table
    
    * Populate enum lists when creating instance from table
    
    * Switch to use the existing enum value retriever
    
    * Harden the can retrieve functionality
    
    * Add assertions for result array lengths
    
    * Add semicolon as supported separator
    
    * Update changelog for 2.3

---
+++ Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/ValueRetrieverTests/StringListValueRetrieverTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using TechTalk.SpecFlow.Assist.ValueRetrievers;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.ValueRetrieverTests
+{
+    [TestFixture]
+    public class StringListValueRetrieverTests
+    {
+        [Test]
+        public void Can_retrieve_string_list_properties()
+        {
+            var retriever = new StringListValueRetriever();
+            
+            retriever.CanRetrieve(new KeyValuePair<string, string>(), null, typeof(List<string>)).Should().BeTrue();
+        }
+
+        [Test]
+        public void Can_retrieve_string_ilist_properties()
+        {
+            var retriever = new StringListValueRetriever();
+
+            retriever.CanRetrieve(new KeyValuePair<string, string>(), null, typeof(IList<string>)).Should().BeTrue();
+        }
+
+        [Test]
+        public void Cannot_retrieve_other_properties()
+        {
+            var retriever = new StringListValueRetriever();
+
+            retriever.CanRetrieve(new KeyValuePair<string, string>(), null, typeof(string[])).Should().BeFalse();
+            retriever.CanRetrieve(new KeyValuePair<string, string>(), null, typeof(List<int>)).Should().BeFalse();
+            retriever.CanRetrieve(new KeyValuePair<string, string>(), null, typeof(string)).Should().BeFalse();
+        }
+
+        [Test]
+        public void Returns_lsit_from_comma_separated_list()
+        {
+            var retriever = new StringListValueRetriever();
+
+            var result = retriever.GetValue("A,B,C");
+
+            result[0].Should().Be("A");
+            result[1].Should().Be("B");
+            result[2].Should().Be("C");
+            result.Count.Should().Be(3);
+        }
+
+        [Test]
+        public void Trims_the_individual_values()
+        {
+            var retriever = new StringArrayValueRetriever();
+
+            var result = retriever.GetValue("A , B, C ");
+
+            result[0].Should().Be("A");
+            result[1].Should().Be("B");
+            result[2].Should().Be("C");
+            result.Length.Should().Be(3);
+        }
+    }
+}
