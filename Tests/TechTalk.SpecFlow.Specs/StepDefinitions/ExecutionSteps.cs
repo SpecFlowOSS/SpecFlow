23382c2d4898cc54b52317af50bf2df18d4a5a77
Balázs Epresi <epresibalazs@gmail.com>
2018-07-23 15:30:23 +0200

    Fix tagging tests

--- Tests/TechTalk.SpecFlow.Specs/StepDefinitions/ExecutionSteps.cs
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/ExecutionSteps.cs
@@ -28,7 +28,7 @@ namespace TechTalk.SpecFlow.Specs.StepDefinitions
         {
             _solutionDriver.CompileSolution();
             _solutionDriver.CheckSolutionShouldHaveCompiled();
-            _vsTestExecution.Filter = $"TestCategory={tag}";
+            _vsTestExecution.Filter = $"Category={tag}|TestCategory={tag}";
             _vsTestExecution.ExecuteTests();
         }
     }
