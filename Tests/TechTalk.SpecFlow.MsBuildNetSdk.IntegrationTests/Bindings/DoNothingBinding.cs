8e8040e77708d5f0b3c0bef7c2a5414f6389d0a4
Daniel Sack <daniel.sack@outlook.com>
2018-08-01 18:53:02 +0200

    adding integration-test project for net.sdk project support

---
+++ Tests/TechTalk.SpecFlow.MsBuildNetSdk.IntegrationTests/Bindings/DoNothingBinding.cs
@@ -0,0 +1,21 @@
+﻿namespace TechTalk.SpecFlow.MsBuildNetSdk.IntegrationTests.Features
+{
+    [Binding]
+    public class DoNothingBinding
+    {
+        [Given(".*"), When(".*"), Then(".*")]
+        public void EmptyStep()
+        {
+        }
+
+        [Given(".*"), When(".*"), Then(".*")]
+        public void EmptyStep(string multiLineStringParam)
+        {
+        }
+
+        [Given(".*"), When(".*"), Then(".*")]
+        public void EmptyStep(Table tableParam)
+        {
+        }
+    }
+}
