88c45ce7d6cbbc4e8b8649cb544ede785d6eaf76
Andreas Willich <andreas.willich@techtalk.at>
2018-08-17 09:57:35 +0200

    fix plugin frameworks
    fix build order
    use msbuild to compile

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ param (
  [string]$Configuration = "Debug"
 )
 
-dotnet msbuild /Restore ./TechTalk.SpecFlow.sln
+msbuild /Restore ./TechTalk.SpecFlow.sln
 
-dotnet msbuild ./TechTalk.SpecFlow.sln /property:Configuration=$Configuration /binaryLogger:msbuild.$Configuration.binlog /nodeReuse:false
+msbuild ./TechTalk.SpecFlow.sln /property:Configuration=$Configuration /binaryLogger:msbuild.$Configuration.binlog /nodeReuse:false
