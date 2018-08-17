10c2018eb648b7a10a4f0cd1d2e6788b097fc181
Sunil Buddala <Sunil.Buddala@fphcare.co.nz>
2018-08-17 15:14:28 +1200

    introduce constructor which takes pluginType as string and parse the type from it.

--- TechTalk.SpecFlow/Infrastructure/GeneratorPluginAttribute.cs
+++ TechTalk.SpecFlow/Infrastructure/GeneratorPluginAttribute.cs
@@ -3,15 +3,21 @@ using System;
 namespace TechTalk.SpecFlow.Infrastructure
 {
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
-    public class GeneratorPluginAttribute: Attribute
+    public class GeneratorPluginAttribute : Attribute
     {
         public Type PluginType { get; private set; }
 
         public GeneratorPluginAttribute(Type pluginType)
         {
-            if (pluginType == null) throw new ArgumentNullException("pluginType");
+            PluginType = pluginType ?? throw new ArgumentNullException(nameof(pluginType));
+        }
 
-            PluginType = pluginType;
+        public GeneratorPluginAttribute(string pluginType)
+        {
+            if (string.IsNullOrEmpty(pluginType)) throw new ArgumentNullException(nameof(pluginType));
+
+            var type = Type.GetType(pluginType);
+            PluginType = type ?? throw new ArgumentException(nameof(pluginType));
         }
     }
 }
