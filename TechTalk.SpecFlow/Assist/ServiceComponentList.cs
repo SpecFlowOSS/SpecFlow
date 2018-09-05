3373c2029f6ea3baff1d21cda0b8be49baaeff45
Szali Szali <szalicil@gmail.com>
2018-09-05 20:33:37 +0200

    created ServiceComponentList

---
+++ TechTalk.SpecFlow/Assist/ServiceComponentList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TechTalk.SpecFlow.Assist
+{
+    public class ServiceComponentList<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> components;
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => components.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)components).GetEnumerator();
+
+        public ServiceComponentList()
+        {
+            components = new List<T>();
+        }
+    }
+}
---
+++ Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/ServiceComponentListTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TechTalk.SpecFlow.Assist;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests
+{
+    [TestFixture]
+    public class ServiceComponentListTests
+    {
+        [Test]
+        public void Should_be_empty_when_created()
+        {
+            var sut = new ServiceComponentList<ITestComponent>();
+
+            sut.Should().BeEmpty();
+        }
+
+        private interface ITestComponent
+        {
+        }
+    }
+}
