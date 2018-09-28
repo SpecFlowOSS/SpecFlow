2c1d7130ee25e7fce243a1eb501b6a16a4e220b0
Szali Szali <szalicil@gmail.com>
2018-09-29 01:05:48 +0200

    remove AllowPartiallyTrustedCallers attribute
    - it has to be removed to allow Gherkin library reference
    - it has already been removed on the DotNetCore branch

--- TechTalk.SpecFlow/Properties/AssemblyInfo.cs
+++ TechTalk.SpecFlow/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -12,7 +11,5 @@ using System.Security;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cba254bb-2476-4e96-9a9f-6db1a1b5eb62")]
-
-[assembly: AllowPartiallyTrustedCallers]
 
 [assembly: InternalsVisibleTo("TechTalk.SpecFlow.RuntimeTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009BD35D42479A68A533445360CA3149C96BF112221527828DCCC15604830999FAD6391912EDBDF591531C4DE9C45E437A3F648A2A3722D04E5A02BECE96522C71060081A14E1E775DF4B6F84D6DB609E3A20D15956D3FEDBFD77B2A9B0D941ACABCBF7C26B87F5696FE4AADAACA69DCB84E7C733D5FF0E9ECEF46656D19BF52A2")]
