5466182a8d5d58af02ede142139e6ed8325167d9
Szali Szali <szalicil@gmail.com>
2018-09-29 10:13:51 +0200

    move SpecFlowGherkinDialectProvider to its own file

--- TechTalk.SpecFlow.Parser/SpecFlowGherkinParser.cs
+++ TechTalk.SpecFlow.Parser/SpecFlowGherkinParser.cs
@@ -8,32 +8,6 @@ using TechTalk.SpecFlow.Parser.SemanticValidators;
 
 namespace TechTalk.SpecFlow.Parser
 {
-    public class SpecFlowGherkinDialectProvider : GherkinDialectProvider
-    {
-        public SpecFlowGherkinDialectProvider(string defaultLanguage) : base(defaultLanguage)
-        {
-        }
-
-        public override GherkinDialect GetDialect(string language, Location location)
-        {
-            if (language.Contains("-"))
-            {
-                try
-                {
-                    return base.GetDialect(language, location);
-                }
-                catch (NoSuchLanguageException)
-                {
-                    var languageBase = language.Split('-')[0];
-                    var languageBaseDialect = base.GetDialect(languageBase, location);
-                    return new GherkinDialect(language, languageBaseDialect.FeatureKeywords, languageBaseDialect.BackgroundKeywords, languageBaseDialect.ScenarioKeywords, languageBaseDialect.ScenarioOutlineKeywords, languageBaseDialect.ExamplesKeywords, languageBaseDialect.GivenStepKeywords, languageBaseDialect.WhenStepKeywords, languageBaseDialect.ThenStepKeywords, languageBaseDialect.AndStepKeywords, languageBaseDialect.ButStepKeywords);
-                }
-            }
-
-            return base.GetDialect(language, location);
-        }
-    }
-
     public class SpecFlowGherkinParser : IGherkinParser
     {
         private readonly IGherkinDialectProvider dialectProvider;
