37800e44b80dec1660562567187d9fc84d698073
Andreas Willich <andreas.willich@techtalk.at>
2018-10-29 14:46:25 +0100

    use AssemblyLoadContextBuilder
    have breaking tests

--- Tests/TechTalk.SpecFlow.PluginTests/Generator/GeneratorPluginLoaderTests.cs
+++ Tests/TechTalk.SpecFlow.PluginTests/Generator/GeneratorPluginLoaderTests.cs
@@ -1,4 +1,8 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using SpecFlow.Tools.MsBuild.Generation.FrameworkDependent;
 using TechTalk.SpecFlow.Generator.Plugins;
 using TechTalk.SpecFlow.Plugins;
 using TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin;
@@ -21,5 +25,37 @@ namespace TechTalk.SpecFlow.PluginTests.Generator
             //ASSERT
             generatorPlugin.Should().BeOfType<GeneratorPlugin>();
         }
+
+#if !NETFRAMEWORK
+        [Fact]
+        public void LoadPlugin_LoadXUnit_WithCustomAssemblyLoader_Successfully()
+        {
+            //ARRANGE
+            string taskAssemblyPath = new Uri(typeof(GeneratorPluginLoader).Assembly.CodeBase).LocalPath;
+
+            var customAssemblyLoader = new AssemblyLoadContextBuilder()
+                .SetBaseDirectory(Path.GetDirectoryName(taskAssemblyPath))
+                .Build();
+
+            var loadFromAssemblyPath = customAssemblyLoader.LoadFromAssemblyPath(taskAssemblyPath);
+            Type innerTaskType = loadFromAssemblyPath.GetType(typeof(GeneratorPluginLoader).FullName);
+            
+
+            object innerTask = Activator.CreateInstance(innerTaskType);
+
+
+            //ACT
+            var pluginDescriptor = new PluginDescriptor("TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin", "TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin.dll", PluginType.Generator, "");
+         
+            //var generatorPlugin = innerTask.LoadPlugin(pluginDescriptor);
+            var executeInnerMethod = innerTaskType.GetMethod("LoadPlugin", BindingFlags.Instance | BindingFlags.Public);
+            var parameters = new object[] { pluginDescriptor };
+            var generatorPlugin = executeInnerMethod.Invoke(innerTask, parameters);
+
+
+            //ASSERT
+            generatorPlugin.Should().BeOfType<GeneratorPlugin>();
+        }
+#endif
     }
 }
