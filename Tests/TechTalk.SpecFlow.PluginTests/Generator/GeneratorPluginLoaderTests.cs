c64ad11086c0169a6b58263943d48148a4913df6
Andreas Willich <andreas.willich@techtalk.at>
2018-11-22 15:47:54 +0100

    remove obsolete failing tests

--- Tests/TechTalk.SpecFlow.PluginTests/Generator/GeneratorPluginLoaderTests.cs
+++ Tests/TechTalk.SpecFlow.PluginTests/Generator/GeneratorPluginLoaderTests.cs
@@ -26,36 +26,5 @@ namespace TechTalk.SpecFlow.PluginTests.Generator
             generatorPlugin.Should().BeOfType<GeneratorPlugin>();
         }
 
-#if !NETFRAMEWORK
-        [Fact]
-        public void LoadPlugin_LoadXUnit_WithCustomAssemblyLoader_Successfully()
-        {
-            //ARRANGE
-            string taskAssemblyPath = new Uri(typeof(GeneratorPluginLoader).Assembly.CodeBase).LocalPath;
-
-            var customAssemblyLoader = new AssemblyLoadContextBuilder()
-                .SetBaseDirectory(Path.GetDirectoryName(taskAssemblyPath))
-                .Build();
-
-            var loadFromAssemblyPath = customAssemblyLoader.LoadFromAssemblyPath(taskAssemblyPath);
-            Type innerTaskType = loadFromAssemblyPath.GetType(typeof(GeneratorPluginLoader).FullName);
-            
-
-            object innerTask = Activator.CreateInstance(innerTaskType);
-
-
-            //ACT
-            var pluginDescriptor = new PluginDescriptor("TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin", "TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin.dll", PluginType.Generator, "");
-         
-            //var generatorPlugin = innerTask.LoadPlugin(pluginDescriptor);
-            var executeInnerMethod = innerTaskType.GetMethod("LoadPlugin", BindingFlags.Instance | BindingFlags.Public);
-            var parameters = new object[] { pluginDescriptor };
-            var generatorPlugin = executeInnerMethod.Invoke(innerTask, parameters);
-
-
-            //ASSERT
-            generatorPlugin.Should().BeOfType<GeneratorPlugin>();
-        }
-#endif
     }
 }
