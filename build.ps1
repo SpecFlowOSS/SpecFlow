13ffc64288989f5d44013c20553f4b1cef2c0897
Andreas Willich <andreas.willich@techtalk.at>
2019-03-11 10:33:10 +0100

    switch to powershell core

--- build.ps1
+++ build.ps1
@@ -4,33 +4,7 @@ param (
 
 $msbuildPath = "msbuild"
 
-$_isWindows = $false
-$_isMacOS = $false
-
-if (($Env:HOME)) {
-  $_isMacOS = $true
-}
-else {
-  if (($Env:UserProfile))
-  {
-    $_isWindows = $true
-  }
-}
-
-if ($_isWindows -eq $TRUE){
-  Write-Host "Running on Windows"
-}
-else {
-  if ($_isMacOS -eq $TRUE) {
-    Write-Host "Running on Mac OS"
-  }
-  else {
-    Write-Host "No idea which OS I am running"
-  }  
-}
-
-
-if ($_isWindows){
+if ($IsWindows){
   $vswherePath = [System.Environment]::ExpandEnvironmentVariables("%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe")
   $vswhereParameters = @("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild",  "-property", "installationPath")
   
