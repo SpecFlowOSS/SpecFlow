5a5718d3b5b838a44b35600e12e3bd44ebe56d00
David Eiwen <david-e-winlive@live.at>
2019-05-09 15:54:47 +0200

    add tests for CucumberMessageFactory and CucumberMessageSender

--- Tests/TechTalk.SpecFlow.RuntimeTests/CucumberMessages/CucumberMessageFactoryTests.cs
+++ Tests/TechTalk.SpecFlow.RuntimeTests/CucumberMessages/CucumberMessageFactoryTests.cs
@@ -1,5 +1,6 @@
 using System;
 using FluentAssertions;
+using Google.Protobuf.WellKnownTypes;
 using TechTalk.SpecFlow.CucumberMessages;
 using Xunit;
 
@@ -12,13 +13,27 @@ namespace TechTalk.SpecFlow.RuntimeTests.CucumberMessages
         {
             // ARRANGE
             var cucumberMessageFactory = new CucumberMessageFactory();
-            var dateTime = new DateTime(2019, 5, 9, 14, 27, 48);
+            var dateTime = new DateTime(2019, 5, 9, 14, 27, 48, DateTimeKind.Utc);
 
             // ACT
             var actualTestRunStartedMessage = cucumberMessageFactory.BuildTestRunStartedMessage(dateTime);
 
             // ASSERT
             actualTestRunStartedMessage.Should().NotBe(null);
+        }
+
+        [Fact(DisplayName = @"BuildTestRunResultMessage should return a TestRunResult message object with the specified date and time")]
+        public void BuildTestRunResultMessage_DateTime_ShouldReturnTestRunResultMessageObjectWithSpecifiedDateAndTime()
+        {
+            // ARRANGE
+            var cucumberMessageFactory = new CucumberMessageFactory();
+            var dateTime = new DateTime(2019, 5, 9, 14, 27, 48, DateTimeKind.Utc);
+
+            // ACT
+            var actualTestRunStartedMessage = cucumberMessageFactory.BuildTestRunStartedMessage(dateTime);
+
+            // ASSERT
+            actualTestRunStartedMessage.Timestamp.ToDateTime().Should().Be(dateTime);
         }
     }
 }
