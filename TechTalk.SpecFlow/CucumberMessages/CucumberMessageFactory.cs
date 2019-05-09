9a6f92308036850fa57eaedaf03c4e0bed0d0856
David Eiwen <david-e-winlive@live.at>
2019-05-09 14:16:57 +0200

    add ICucumberMessageFactory

---
+++ TechTalk.SpecFlow/CucumberMessages/CucumberMessageFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Io.Cucumber.Messages;
+
+namespace TechTalk.SpecFlow.CucumberMessages
+{
+    public class CucumberMessageFactory : ICucumberMessageFactory
+    {
+        public TestRunStarted BuildTestRunStartedMessage(DateTime timeStamp)
+        {
+            return new TestRunStarted();
+        }
+    }
+}
