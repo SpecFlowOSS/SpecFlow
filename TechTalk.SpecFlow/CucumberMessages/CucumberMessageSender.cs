8bbcdbda5aa41c91d5f830381aba899844b9ef25
David Eiwen <david-e-winlive@live.at>
2019-05-09 14:17:27 +0200

    use ICucumberMessageFactory and ICucumberMessageSink in CucumberMessageSender

--- TechTalk.SpecFlow/CucumberMessages/CucumberMessageSender.cs
+++ TechTalk.SpecFlow/CucumberMessages/CucumberMessageSender.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using TechTalk.SpecFlow.Time;
 
 namespace TechTalk.SpecFlow.CucumberMessages
@@ -6,14 +6,23 @@ namespace TechTalk.SpecFlow.CucumberMessages
     public class CucumberMessageSender : ICucumberMessageSender
     {
         private readonly IClock _clock;
+        private readonly ICucumberMessageFactory _cucumberMessageFactory;
+        private readonly IEnumerable<ICucumberMessageSink> _cucumberMessageSinks;
 
-        public CucumberMessageSender(IClock clock)
+        public CucumberMessageSender(IClock clock, ICucumberMessageFactory cucumberMessageFactory, IEnumerable<ICucumberMessageSink> cucumberMessageSinks)
         {
             _clock = clock;
+            _cucumberMessageFactory = cucumberMessageFactory;
+            _cucumberMessageSinks = cucumberMessageSinks;
         }
 
         public void SendTestRunStarted()
         {
+            var testRunStartedMessage = _cucumberMessageFactory.BuildTestRunStartedMessage(_clock.GetNowDateAndTime());
+            foreach (var cucumberMessageSink in _cucumberMessageSinks)
+            {
+                cucumberMessageSink.SendMessage(testRunStartedMessage);
+            }
         }
     }
 }
