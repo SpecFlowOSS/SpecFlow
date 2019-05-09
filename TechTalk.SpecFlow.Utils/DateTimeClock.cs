92e839976c36abed2199bed699d8809ffd4b6989
David Eiwen <david-e-winlive@live.at>
2019-05-09 13:41:25 +0200

    add date and time abstraction

---
+++ TechTalk.SpecFlow.Utils/DateTimeClock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TechTalk.SpecFlow.Utils
+{
+    public class DateTimeClock : IClock
+    {
+        public DateTime GetToday()
+        {
+            return DateTime.Today;
+        }
+
+        public DateTime GetNowDateAndTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ TechTalk.SpecFlow.Utils/IClock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TechTalk.SpecFlow.Utils
+{
+    public interface IClock
+    {
+        DateTime GetToday();
+
+        DateTime GetNowDateAndTime();
+    }
+}
