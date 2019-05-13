3bb48172d4a80c8968ac3e979f297a3f5d13210d
David Eiwen <david-e-winlive@live.at>
2019-05-13 15:07:13 +0200

    set CucumberImplementation in CucumberMessageFactory

--- TechTalk.SpecFlow/CucumberMessages/CucumberMessageFactory.cs
+++ TechTalk.SpecFlow/CucumberMessages/CucumberMessageFactory.cs
@@ -6,11 +6,14 @@ namespace TechTalk.SpecFlow.CucumberMessages
 {
     public class CucumberMessageFactory : ICucumberMessageFactory
     {
+        private const string UsedCucumberImplementationString = @"SpecFlow";
+
         public TestRunStarted BuildTestRunStartedMessage(DateTime timeStamp)
         {
             return new TestRunStarted
             {
-                Timestamp = Timestamp.FromDateTime(timeStamp)
+                Timestamp = Timestamp.FromDateTime(timeStamp),
+                CucumberImplementation = UsedCucumberImplementationString
             };
         }
     }
