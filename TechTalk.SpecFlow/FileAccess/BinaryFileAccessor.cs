aa1c55c737ac867d92d9c1bcda8e25f1e3ee1c00
David Eiwen <david-e-winlive@live.at>
2019-05-14 15:00:44 +0200

    add IBinaryFileAccessor

---
+++ TechTalk.SpecFlow/FileAccess/BinaryFileAccessor.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using TechTalk.SpecFlow.CommonModels;
+
+namespace TechTalk.SpecFlow.FileAccess
+{
+    public class BinaryFileAccessor : IBinaryFileAccessor
+    {
+        public Result<Stream> OpenAppendOrCreateFile(string filePath)
+        {
+            try
+            {
+                var streamToReturn = File.Open(filePath, FileMode.Append, System.IO.FileAccess.Write, FileShare.Read);
+                return Result<Stream>.Success(streamToReturn);
+            }
+            catch (IOException)
+            {
+                return Result<Stream>.Failure();
+            }
+        }
+    }
+}
