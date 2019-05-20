243559fda979511b063e45d2059de378418ebf77
David Eiwen <david-e-winlive@live.at>
2019-05-20 13:18:53 +0200

    add PickleIdGenerator

---
+++ TechTalk.SpecFlow/CucumberMessages/PickleIdGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TechTalk.SpecFlow.CommonModels;
+
+namespace TechTalk.SpecFlow.CucumberMessages
+{
+    public class PickleIdGenerator : IPickleIdGenerator
+    {
+        public Result GeneratePickleId()
+        {
+            return Result.Success(Guid.NewGuid());
+        }
+    }
+}
