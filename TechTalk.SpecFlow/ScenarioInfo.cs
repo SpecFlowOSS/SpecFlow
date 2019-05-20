49e7f16aafa5bef349046412461c5a71c440ad8a
David Eiwen <david-e-winlive@live.at>
2019-05-20 13:43:48 +0200

    fix padding

--- TechTalk.SpecFlow/ScenarioInfo.cs
+++ TechTalk.SpecFlow/ScenarioInfo.cs
@@ -8,7 +8,7 @@ namespace TechTalk.SpecFlow
         public string Title { get; private set; }
         public string Description { get; private set; }
 
-    public ScenarioInfo(string title, string description, params string[] tags)
+        public ScenarioInfo(string title, string description, params string[] tags)
         {
             Title = title;
             Description = description;
