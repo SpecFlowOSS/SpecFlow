42c204ec3b6e99213a0fc6dec26c426424c88e81
David Eiwen <david-e-winlive@live.at>
2019-05-23 15:59:52 +0200

    add step definitions

---
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/CucumberMessages/TestRunStartedSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTalk.SpecFlow.Specs.StepDefinitions.CucumberMessages
+{
+    [Binding]
+    public class TestRunStartedSteps
+    {
+        [Then(@"a TestRunStarted message has been sent")]
+        public void ThenATestRunStartedMessageHasBeenSent()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the test suite is executed")]
+        public void WhenTheTestSuiteIsExecuted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the test suite is started at '(.*)'")]
+        public void WhenTheTestSuiteIsStartedAt(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the test suite was executed")]
+        public void WhenTheTestSuiteWasExecuted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/CucumberMessages/TestSuiteSteps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTalk.SpecFlow.Specs.StepDefinitions.CucumberMessages
+{
+    [Binding]
+    public class TestSuiteSteps
+    {
+        [Given(@"there are (\d+) feature files")]
+        public void GivenThereAreFeatureFiles(int featureFilesCount)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
