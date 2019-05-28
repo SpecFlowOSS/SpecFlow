dc49a9695a4ef3d05bc212ac92e7b78fb0f1f57d
David Eiwen <david-e-winlive@live.at>
2019-05-28 10:42:14 +0200

    refactor test suite setup into driver

--- Tests/TechTalk.SpecFlow.Specs/Drivers/CucumberMessages/TestSuiteSetupDriver.cs
+++ Tests/TechTalk.SpecFlow.Specs/Drivers/CucumberMessages/TestSuiteSetupDriver.cs
@@ -2,33 +2,8 @@
 using System.Text;
 using TechTalk.SpecFlow.TestProjectGenerator.Driver;
 
-namespace TechTalk.SpecFlow.Specs.StepDefinitions.CucumberMessages
+namespace TechTalk.SpecFlow.Specs.Drivers.CucumberMessages
 {
-    [Binding]
-    public class TestSuiteSteps
-    {
-        private readonly ProjectsDriver _projectsDriver;
-        private readonly TestSuiteSetupDriver _testSuiteSetupDriver;
-
-        public TestSuiteSteps(ProjectsDriver projectsDriver, TestSuiteSetupDriver testSuiteSetupDriver)
-        {
-            _projectsDriver = projectsDriver;
-            _testSuiteSetupDriver = testSuiteSetupDriver;
-        }
-
-        [Given(@"there are (\d+) feature files")]
-        public void GivenThereAreFeatureFiles(int featureFilesCount)
-        {
-            _testSuiteSetupDriver.AddFeatureFiles(featureFilesCount);
-        }
-
-        [Given(@"the cucumber implementation is (.*)")]
-        public void GivenTheCucumberImplementationIs(string cucumberImplementation)
-        {
-            _testSuiteSetupDriver.AddFeatureFiles(1);
-        }
-    }
-
     public class TestSuiteSetupDriver
     {
         private readonly ProjectsDriver _projectsDriver;
