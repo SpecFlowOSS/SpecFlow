e161056746d5da1d732c4aa17d9e453705cd8189
David Eiwen <david-e-winlive@live.at>
2019-05-28 15:34:44 +0200

    add IEnvironmentWrapper

---
+++ TechTalk.SpecFlow/EnvironmentAccess/IEnvironmentWrapper.cs
@@ -0,0 +1,13 @@
+﻿using TechTalk.SpecFlow.CommonModels;
+
+namespace TechTalk.SpecFlow.EnvironmentAccess
+{
+    public interface IEnvironmentWrapper
+    {
+        bool IsEnvironmentVariableSet(string name);
+
+        IResult<string> GetEnvironmentVariable(string name);
+
+        void SetEnvironmentVariable(string name, string value);
+    }
+}
