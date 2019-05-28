ffec2e143845c89b8e1fb0b6c1e05c4b0488bd55
David Eiwen <david-e-winlive@live.at>
2019-05-28 16:09:00 +0200

    add IProtobufFileNameResolver

---
+++ TechTalk.SpecFlow/CucumberMessages/Sinks/IProtobufFileNameResolver.cs
@@ -0,0 +1,9 @@
+﻿using TechTalk.SpecFlow.CommonModels;
+
+namespace TechTalk.SpecFlow.CucumberMessages.Sinks
+{
+    public interface IProtobufFileNameResolver
+    {
+        IResult<string> Resolve(string targetPath);
+    }
+}
---
+++ TechTalk.SpecFlow/CucumberMessages/Sinks/ProtobufFileNameResolver.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using TechTalk.SpecFlow.CommonModels;
+using TechTalk.SpecFlow.EnvironmentAccess;
+using TechTalk.SpecFlow.TestFramework;
+
+namespace TechTalk.SpecFlow.CucumberMessages.Sinks
+{
+    public class ProtobufFileNameResolver : IProtobufFileNameResolver
+    {
+        private readonly ITestRunContext _testRunContext;
+        private readonly IEnvironmentWrapper _environmentWrapper;
+
+        public ProtobufFileNameResolver(ITestRunContext testRunContext, IEnvironmentWrapper environmentWrapper)
+        {
+            _testRunContext = testRunContext;
+            _environmentWrapper = environmentWrapper;
+        }
+
+        public IResult<string> Resolve(string targetPath)
+        {
+            var resolveEnvironmentVariablesResult = _environmentWrapper.ResolveEnvironmentVariables(targetPath);
+            if (!(resolveEnvironmentVariablesResult is ISuccess<string> success))
+            {
+                return Result<string>.Failure($"Failed resolving environment variables from string '{targetPath}'");
+            }
+
+            if (Path.IsPathRooted(success.Result))
+            {
+                return Result<string>.Success(success.Result);
+            }
+
+            string combinedPath = Path.Combine( _testRunContext.TestDirectory, targetPath);
+            return Result<string>.Success(combinedPath);
+        }
+    }
+}
