e53601c64a39de3e81b7b5b1ebed300ca291808e
David Eiwen <david-e-winlive@live.at>
2019-05-29 12:54:57 +0200

    add step skeletons for TestCaseStarted

---
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/CucumberMessages/ScenarioSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TechTalk.SpecFlow.Specs.StepDefinitions.CucumberMessages
+{
+    [Binding]
+    public class ScenarioSteps
+    {
+        [Given(@"there are (\d+) scenarios")]
+        public void GivenThereAreScenarios(int scenarios)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"there is a scenario")]
+        public void GivenThereIsAScenario()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"there is a scenario with PickleId '(.*)'")]
+        public void GivenThereIsAScenarioWithPickleId(Guid pickleId)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
---
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/CucumberMessages/TestCaseStartedSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TechTalk.SpecFlow.Specs.StepDefinitions.CucumberMessages
+{
+    [Binding]
+    public class TestCaseStartedSteps
+    {
+        [When(@"the scenario is executed")]
+        public void WhenTheScenarioIsExecuted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the scenario is started at '(.*)'")]
+        public void WhenTheScenarioIsStartedAt(DateTime startTime)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+        [Then(@"(\d+) TestCaseStarted messages have been sent")]
+        public void ThenTestCaseStartedMessagesHaveBeenSent(int numberOfTestCaseStartedMessages)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"a TestCaseStarted message has been sent with the following attributes")]
+        public void ThenATestCaseStartedMessageHasBeenSentWithTheFollowingAttributes(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
