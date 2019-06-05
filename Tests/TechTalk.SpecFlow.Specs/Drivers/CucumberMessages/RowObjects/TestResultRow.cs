0548fdb76c027e5bcd55d7f9e54eee25c9c74093
David Eiwen <david-e-winlive@live.at>
2019-06-05 13:08:01 +0200

    add missing file

---
+++ Tests/TechTalk.SpecFlow.Specs/Drivers/CucumberMessages/RowObjects/TestResultRow.cs
@@ -0,0 +1,9 @@
+﻿using Io.Cucumber.Messages;
+
+namespace TechTalk.SpecFlow.Specs.Drivers.CucumberMessages.RowObjects
+{
+    public class TestResultRow
+    {
+        public Status? Status { get; set; }
+    }
+}
