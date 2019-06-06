ecd894e7a48b996d21b76a9296d896628d025446
David Eiwen <david-e-winlive@live.at>
2019-06-06 14:28:22 +0200

    add unit tests

--- Tests/TechTalk.SpecFlow.RuntimeTests/CucumberMessages/TestResultFactoryTests.cs
+++ Tests/TechTalk.SpecFlow.RuntimeTests/CucumberMessages/TestResultFactoryTests.cs
@@ -180,6 +180,57 @@ namespace TechTalk.SpecFlow.RuntimeTests.CucumberMessages
                             .Result.Message.Should().Be(expectedMessage);
         }
 
+        [Fact(DisplayName = @"BuildAmbiguousResult should return a TestResult with status Ambiguous")]
+        public void BuildAmbiguousResult_ValidParameters_ShouldReturnTestResultWithStatusAmbiguous()
+        {
+            // ARRANGE
+            var errorProviderMock = GetErrorProviderMock();
+
+            var testResultFactory = new TestResultFactory(new TestErrorMessageFactory(), new TestPendingMessageFactory(errorProviderMock.Object));
+            const Status expectedStatus = Status.Ambiguous;
+
+            // ACT
+            var actualTestResult = testResultFactory.BuildAmbiguousResult(10Lu, "Ambiguous test");
+
+            // ASSERT
+            actualTestResult.Should().BeAssignableTo<ISuccess<TestResult>>().Which
+                            .Result.Status.Should().Be(expectedStatus);
+        }
+
+        [Fact(DisplayName = @"BuildAmbiguousResult should return a TestResult with the passed nanoseconds duration")]
+        public void BuildAmbiguousResult_Nanoseconds_ShouldReturnTestResultWithNanoseconds()
+        {
+            // ARRANGE
+            var errorProviderMock = GetErrorProviderMock();
+
+            var testResultFactory = new TestResultFactory(new TestErrorMessageFactory(), new TestPendingMessageFactory(errorProviderMock.Object));
+            const ulong expectedNanoseconds = 15Lu;
+
+            // ACT
+            var actualTestResult = testResultFactory.BuildAmbiguousResult(expectedNanoseconds, "Ambiguous test");
+
+            // ASSERT
+            actualTestResult.Should().BeAssignableTo<ISuccess<TestResult>>().Which
+                            .Result.DurationNanoseconds.Should().Be(expectedNanoseconds);
+        }
+
+        [Fact(DisplayName = @"BuildAmbiguousResult should return a TestResult with the passed message")]
+        public void BuildAmbiguousResult_Message_ShouldReturnTestResultWithMessage()
+        {
+            // ARRANGE
+            var errorProviderMock = GetErrorProviderMock();
+
+            var testResultFactory = new TestResultFactory(new TestErrorMessageFactory(), new TestPendingMessageFactory(errorProviderMock.Object));
+            const string expectedMessage = "This is a test message";
+
+            // ACT
+            var actualTestResult = testResultFactory.BuildAmbiguousResult(10Lu, expectedMessage);
+
+            // ASSERT
+            actualTestResult.Should().BeAssignableTo<ISuccess<TestResult>>().Which
+                            .Result.Message.Should().Be(expectedMessage);
+        }
+
         public Mock<IErrorProvider> GetErrorProviderMock()
         {
             var errorProviderMock = new Mock<IErrorProvider>();
