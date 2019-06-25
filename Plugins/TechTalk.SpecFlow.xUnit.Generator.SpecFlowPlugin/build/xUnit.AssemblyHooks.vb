2ed3a2d49439505a6441a4520a71375bac79f80d
David Eiwen <david-e-winlive@live.at>
2019-06-25 09:32:54 +0200

    add assembly hooks for xUnit

---
+++ Plugins/TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin/build/xUnit.AssemblyHooks.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports Xunit
+Imports TechTalk.SpecFlow
+
+Namespace TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin.build
+
+    Public Class XUnitAssemblyHooksFixture
+        Implements IDisposable
+
+        Public Sub New()
+            TestRunnerManager.GetTestRunner().OnTestRunStart()
+        End Sub
+
+        Public Sub Dispose()
+            TestRunnerManager.OnTestRunEnd()
+        End Sub
+
+    End Class
+
+    Public Class XUnitAssemblyHooksCollectionDefinition
+        Implements ICollectionFixture(Of XUnitAssemblyHooksFixture)
+    End Class
+End Namespace
