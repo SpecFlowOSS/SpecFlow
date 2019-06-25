05a2fc395a9e280fff4ff9a1ce3fcdef988311e3
David Eiwen <david-e-winlive@live.at>
2019-06-25 09:29:48 +0200

    add assembly hooks for MSTest

---
+++ Plugins/TechTalk.SpecFlow.MSTest.Generator.SpecFlowPlugin/build/MSTest.AssemblyHooks.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports TechTalk.SpecFlow
+
+Namespace TechTalk.SpecFlow.MSTest.Generator.SpecFlowPlugin.build
+
+    Public NotInheritable Class MSTestAssemblyHooks
+        <AssemblyInitialize>
+        Public Shared Sub AssemblyInitialize()
+            TestRunnerManager.GetTestRunner().OnTestRunStart()
+        End Sub
+
+        <AssemblyCleanup>
+        Public Shared Sub AssemblyCleanup()
+            TestRunnerManager.OnTestRunEnd()
+        End Sub
+
+    End Class
+End Namespace
