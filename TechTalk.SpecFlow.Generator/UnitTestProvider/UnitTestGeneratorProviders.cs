74dfa250b7a35caadda3a4febc9349249bda3b6b
David Eiwen <david-e-winlive@live.at>
2019-07-18 14:57:28 +0200

    remove MbUnit test generator providers

--- TechTalk.SpecFlow.Generator/UnitTestProvider/UnitTestGeneratorProviders.cs
+++ TechTalk.SpecFlow.Generator/UnitTestProvider/UnitTestGeneratorProviders.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using BoDi;
+﻿using BoDi;
 using TechTalk.SpecFlow.Generator.UnitTestProvider;
 
 namespace TechTalk.SpecFlow.Generator
@@ -13,8 +9,6 @@ namespace TechTalk.SpecFlow.Generator
         {
             container.RegisterTypeAs<NUnitTestGeneratorProvider, IUnitTestGeneratorProvider>("nunit.2");
             container.RegisterTypeAs<NUnit3TestGeneratorProvider, IUnitTestGeneratorProvider>("nunit");
-            container.RegisterTypeAs<MbUnitTestGeneratorProvider, IUnitTestGeneratorProvider>("mbunit");
-            container.RegisterTypeAs<MbUnit3TestGeneratorProvider, IUnitTestGeneratorProvider>("mbunit.3");
             container.RegisterTypeAs<XUnitTestGeneratorProvider, IUnitTestGeneratorProvider>("xunit.1");
             container.RegisterTypeAs<XUnit2TestGeneratorProvider, IUnitTestGeneratorProvider>("xunit");
             container.RegisterTypeAs<MsTestV2GeneratorProvider, IUnitTestGeneratorProvider>("mstest");
