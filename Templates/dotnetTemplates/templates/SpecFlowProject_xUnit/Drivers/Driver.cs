72e1fa1501a55a1296eb868728bb36819f39f50e
Manuel Eiwen <39438030+broteinheit@users.noreply.github.com>
2019-08-09 13:20:38 +0200

    Add SpecFlow Templates for .NET Core tooling (#1698)
    * Add SpecFlow Templates for .NET
    
    * Refactor Templates

---
+++ Templates/dotnetTemplates/templates/SpecFlowProject_xUnit/Drivers/Driver.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Template.Drivers
+{
+    public class Driver
+    {
+        
+        public void CreateBackground() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteAction() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckCondition() 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Templates/dotnetTemplates/templates/specflow-feature/Template.feature
@@ -0,0 +1,9 @@
+Feature: Put feature-title here
+	Put feature description here
+
+Background:
+    Given Put your Background here
+
+Scenario: Put your Scenario here
+	When Put your Action here
+    Then Put your Condition here
