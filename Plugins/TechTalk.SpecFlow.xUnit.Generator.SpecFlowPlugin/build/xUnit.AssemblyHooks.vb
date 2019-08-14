0622c027ba7e96a79e34125da8e7f7fa7cc0d911
David Eiwen <david-e-winlive@live.at>
2019-08-14 11:39:20 +0200

    fix VBNET code

--- Plugins/TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin/build/xUnit.AssemblyHooks.vb
+++ Plugins/TechTalk.SpecFlow.xUnit.Generator.SpecFlowPlugin/build/xUnit.AssemblyHooks.vb
@@ -1,4 +1,4 @@
-﻿<Assembly: Global.TechTalk.SpecFlow.xUnit.SpecFlowPlugin.AssemblyFixture(GetType(Global.InternalSpecFlow.XUnitAssemblyFixture))>
+﻿<Assembly: Global.TechTalk.SpecFlow.xUnit.SpecFlowPlugin.AssemblyFixture(GetType(InternalSpecFlow.XUnitAssemblyFixture))>
 
 Namespace InternalSpecFlow
 
@@ -8,12 +8,12 @@ Namespace InternalSpecFlow
         Private ReadOnly _currentAssembly As Global.System.Reflection.Assembly
 
         Public Sub New()
-            _currentAssembly = GetType(Global.InternalSpecFlow.XUnitAssemblyFixture).Assembly
+            _currentAssembly = GetType(XUnitAssemblyFixture).Assembly
             Global.TechTalk.SpecFlow.TestRunnerManager.OnTestRunStart(_currentAssembly)
         End Sub
 
 
-        Public Sub Dispose()
+        Public Sub Dispose() Implements Global.System.IDisposable.Dispose
             Global.TechTalk.SpecFlow.TestRunnerManager.OnTestRunEnd(_currentAssembly)
         End Sub
     End Class
