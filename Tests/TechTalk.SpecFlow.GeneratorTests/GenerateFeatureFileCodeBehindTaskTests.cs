59bb74d36a421361bc055e5b5b813510458ae900
Manuel Eiwen <manuel.eiwen@techtalk.at>
2019-08-22 10:53:04 +0200

    Fix Generator Tests for Linux

--- Tests/TechTalk.SpecFlow.GeneratorTests/GenerateFeatureFileCodeBehindTaskTests.cs
+++ Tests/TechTalk.SpecFlow.GeneratorTests/GenerateFeatureFileCodeBehindTaskTests.cs
@@ -7,15 +7,38 @@ namespace TechTalk.SpecFlow.GeneratorTests
 {
     public class GenerateFeatureFileCodeBehindTaskTests
     {
-        [Theory(DisplayName = "FilePathGenerator should generate correct path.")]
+        [SkippableTheory(DisplayName = "FilePathGenerator should generate correct path (Windows).")]
         [InlineData(@"C:\temp\Project1", "Features", "Some.feature", "Some.feature.cs", @"C:\temp\Project1\Features\Some.feature.cs")]
-        public void FilePathGenerator_GenerateFilePath_ShouldReturnCorrectPath(
+        public void FilePathGenerator_GenerateFilePath_ShouldReturnCorrectPath_Windows(
             string projectDir,
             string relativeOutputDir,
             string featureFile,
             string generatedCodeBehindName,
             string expected)
         {
+            Skip.IfNot(Environment.OSVersion.Platform == PlatformID.Win32NT);
+
+            // ARRANGE
+            var filePathGenerator = new FilePathGenerator();
+
+            // ACT
+            string actual = filePathGenerator.GenerateFilePath(projectDir, relativeOutputDir, featureFile, generatedCodeBehindName);
+
+            // ASSERT
+            actual.Should().Be(expected);
+        }
+
+        [SkippableTheory(DisplayName = "FilePathGenerator should generate correct path (Linux).")]
+        [InlineData(@"/temp/Project1", "Features", "Some.feature", "Some.feature.cs", @"/temp/Project1/Features/Some.feature.cs")]
+        public void FilePathGenerator_GenerateFilePath_ShouldReturnCorrectPath_Linux(
+            string projectDir,
+            string relativeOutputDir,
+            string featureFile,
+            string generatedCodeBehindName,
+            string expected)
+        {
+            Skip.IfNot(Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX);
+
             // ARRANGE
             var filePathGenerator = new FilePathGenerator();
 
