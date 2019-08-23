a96641924fc7032941de1fbcbccb65b3143833cb
Andreas Willich <SabotageAndi@users.noreply.github.com>
2019-08-23 08:52:26 +0200

    use xunit integration to trigger AfterTestRun hook (#1710)
    * add code for xUnit AssemblyFixtureAttribute
    
    * adapt XUnitAssemblyFixture
    
    * update assembly hooks and remove static constructor hack
    
    * fix VBNET code
    
    * use global namespaces
    
    * add parameter for assembly fixture
    
    * use test framework on assembly
    
    * fix whitespaces
    
    * compile xUnit plugin for .NET 4.5.2
    
    * fix unit test
    
    * use latest xUnit version
    
    build everything xUnit related with .NET 4.5.2
    
    * fix VB.NET xUnit Integration
    
    * update Cucumber.Messages to 4.0.0
    
    * update submodule
    
    * update submodule
    
    * fix build error

---
+++ Plugins/TechTalk.SpecFlow.xUnit.SpecFlowPlugin/XunitTestFrameworkExecutorWithAssemblyFixture.cs
@@ -0,0 +1,26 @@
+﻿// Contains code from https://github.com/xunit/samples.xunit
+// originally published under Apache 2.0 license
+// For more information see aforementioned repository
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace TechTalk.SpecFlow.xUnit.SpecFlowPlugin
+{
+    public class XunitTestFrameworkExecutorWithAssemblyFixture : XunitTestFrameworkExecutor
+    {
+        public XunitTestFrameworkExecutorWithAssemblyFixture(AssemblyName assemblyName, ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink)
+            : base(assemblyName, sourceInformationProvider, diagnosticMessageSink)
+        {
+        }
+
+        protected override async void RunTestCases(IEnumerable<IXunitTestCase> testCases, IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+        {
+            using (var assemblyRunner = new XunitTestAssemblyRunnerWithAssemblyFixture(TestAssembly, testCases, DiagnosticMessageSink, executionMessageSink, executionOptions))
+            {
+                await assemblyRunner.RunAsync();
+            }
+        }
+    }
+}
