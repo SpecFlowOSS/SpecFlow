819440a1d22b46e1fadce5b8c1a6d30c59ec4d99
Zsolt Olah <zsol@corp.techtalk.at>
2019-11-07 16:20:17 +0100

    Extend Specs - Cover the scenario of "Two projects with the same unit test provider"

---
+++ Tests/TechTalk.SpecFlow.Specs/Features/MultipleSpecsProjects.feature
@@ -0,0 +1,13 @@
+﻿Feature: Multiple Specs Projects
+
+Scenario Outline: Two projects with the same unit test provider
+	Given I have Specs.Project.A and Specs.Project.B using the same unit test provider
+	And Specs.Project.B references Specs.Project.A
+	When I build the solution using <BuildTool> with treat warnings as errors enabled
+	Then the build should succeed
+
+	Examples: 
+	| BuildTool     |
+	| MSBuild       |
+	| DotnetBuild   |
+	| DotnetMSBuild |
---
+++ Tests/TechTalk.SpecFlow.Specs/StepDefinitions/MultipleSpecsProjectsSteps.cs
@@ -0,0 +1,44 @@
+﻿
+using TechTalk.SpecFlow.TestProjectGenerator.Driver;
+
+namespace TechTalk.SpecFlow.Specs.StepDefinitions
+{
+    [Binding]
+    public class MultipleSpecsProjectsSteps
+    {
+        private readonly ProjectsDriver _projectsDriver;
+        private readonly SolutionDriver _solutionDriver;
+
+        public MultipleSpecsProjectsSteps(ProjectsDriver projectsDriver, SolutionDriver solutionDriver)
+        {
+            _projectsDriver = projectsDriver;
+            _solutionDriver = solutionDriver;
+        }
+
+        [Given("I have (Specs.Project.[A-Z]) and (Specs.Project.[A-Z]) using the same unit test provider")]
+        public void GivenIHaveTwoSpecsProjectsWithTheSameUnitTestProvider(string projectName1, string projectName2)
+        {
+            _projectsDriver.CreateProject(projectName1, "C#");
+            _projectsDriver.CreateProject(projectName2, "C#");
+        }
+
+        [Given(@"(Specs.Project.[A-Z]) references (Specs.Project.[A-Z])")]
+        public void GivenSpecsProjectOneReferencesSpecsProjectTwo(string targetProjectName, string referencedProjectName)
+        {
+            _projectsDriver.AddProjectReference(referencedProjectName, targetProjectName);
+        }
+
+        [When(@"I build the solution using (\w+) with treat warnings as errors enabled")]
+        public void WhenIBuildWithTreatWarningsAsErrorsEnabled(BuildTool buildTool)
+        {
+            _solutionDriver.CompileSolution(buildTool, true);
+        }
+
+        [Then(@"the build should succeed")]
+        public void ThenTheBuildShouldSucceed()
+        {
+            _solutionDriver.CheckSolutionShouldHaveCompiled();
+        }
+
+    }
+}
