b84ea080b6146d29e26770be506258979c5831b0
David Eiwen <david-e-winlive@live.at>
2019-11-19 15:35:14 +0100

    remove old msbuild search and wrap appInsightsInstrumentationKey in quotes

--- build.ps1
+++ build.ps1
@@ -8,26 +8,12 @@ $additionalOptions = ""
 
 Write-Host ($appInsightsInstrumentationKey -eq "")
 
-if ($IsWindows){
-  $vswherePath = [System.Environment]::ExpandEnvironmentVariables("%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe")
-  $vswhereParameters = @("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild",  "-property", "installationPath", "-prerelease")
-  
-  $vsPath = & $vswherePath $vswhereParameters
-  
-  Write-Host $path
-  
-  if ($vsPath) {
-    $msbuildPath = join-path $vsPath 'MSBuild\Current\Bin\MSBuild.exe'
-  }
-  
-  Write-Host $msbuildPath
-}
 if ($IsLinux) {
   $additionalOptions = "-p:EnableSourceControlManagerQueries=false -p:EnableSourceLink=false -p:DeterministicSourcePaths=false"
 }
 
 if ($appInsightsInstrumentationKey) {
-  $additionalOptions = "$($additionalOptions) -p:AppInsightsInstrumentationKey=$($appInsightsInstrumentationKey)"
+  $additionalOptions = "$($additionalOptions) `"-property:AppInsightsInstrumentationKey=$($appInsightsInstrumentationKey)`""
 }
 
 & dotnet build ./TechTalk.SpecFlow.sln -property:Configuration=$Configuration -bl:msbuild.$Configuration.binlog /nodeReuse:false -v n --no-incremental $additionalOptions
