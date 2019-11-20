959adc883655906f80f35ceb36a80e8f021e6a41
Andreas Willich <andreas.willich@techtalk.at>
2019-11-20 17:22:40 +0100

    fix build script for windows

--- build.ps1
+++ build.ps1
@@ -3,17 +3,19 @@ param (
  [string]$appInsightsInstrumentationKey = ""
 )
 
-$msbuildPath = "msbuild"
 $additionalOptions = ""
-
-Write-Host ($appInsightsInstrumentationKey -eq "")
 
 if ($IsLinux) {
   $additionalOptions = "-p:EnableSourceControlManagerQueries=false -p:EnableSourceLink=false -p:DeterministicSourcePaths=false"
 }
 
 if ($appInsightsInstrumentationKey) {
-  $additionalOptions = "$($additionalOptions) -property:AppInsightsInstrumentationKey=`"$($appInsightsInstrumentationKey)`""
+  if ($additionalOptions){
+    $additionalOptions = "$($additionalOptions) -property:AppInsightsInstrumentationKey=$($appInsightsInstrumentationKey)"
+  }
+  else {
+    $additionalOptions = "-property:AppInsightsInstrumentationKey=$($appInsightsInstrumentationKey)"
+  }
 }
 
 Write-Host "dotnet build ./TechTalk.SpecFlow.sln -property:Configuration=$Configuration -bl:msbuild.$Configuration.binlog -nodeReuse:false -v n --no-incremental $additionalOptions"
