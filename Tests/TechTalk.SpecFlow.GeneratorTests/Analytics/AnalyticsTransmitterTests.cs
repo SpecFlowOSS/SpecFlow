3fc69a0194f180ff65cf3778eaa887cf7eeb7f1b
David Eiwen <david-e-winlive@live.at>
2019-12-02 09:45:55 +0100

    make tests async

--- Tests/TechTalk.SpecFlow.GeneratorTests/Analytics/AnalyticsTransmitterTests.cs
+++ Tests/TechTalk.SpecFlow.GeneratorTests/Analytics/AnalyticsTransmitterTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
 using TechTalk.SpecFlow.Analytics;
 using TechTalk.SpecFlow.CommonModels;
@@ -9,7 +10,7 @@ namespace TechTalk.SpecFlow.GeneratorTests.Analytics
     public class AnalyticsTransmitterTests
     {
         [Fact]
-        public void TryTransmitEvent_AnalyticsDisabled_ShouldReturnSuccess()
+        public async Task TryTransmitEvent_AnalyticsDisabled_ShouldReturnSuccess()
         {
             // ARRANGE
             var analyticsTransmitterSinkMock = new Mock<IAnalyticsTransmitterSink>();
@@ -21,14 +22,14 @@ namespace TechTalk.SpecFlow.GeneratorTests.Analytics
             var analyticsTransmitter = new AnalyticsTransmitter(analyticsTransmitterSinkMock.Object, environmentSpecFlowTelemetryCheckerMock.Object);
 
             // ACT
-            var result = analyticsTransmitter.TransmitEvent(analyticsEventMock.Object);
+            var result = await analyticsTransmitter.TransmitEvent(analyticsEventMock.Object);
 
             // ASSERT
             result.Should().BeAssignableTo<ISuccess>();
         }
 
         [Fact]
-        public void TryTransmitEvent_AnalyticsDisabled_ShouldNotCallSink()
+        public async Task TryTransmitEvent_AnalyticsDisabled_ShouldNotCallSink()
         {
             // ARRANGE
             var analyticsTransmitterSinkMock = new Mock<IAnalyticsTransmitterSink>();
@@ -40,14 +41,14 @@ namespace TechTalk.SpecFlow.GeneratorTests.Analytics
             var analyticsTransmitter = new AnalyticsTransmitter(analyticsTransmitterSinkMock.Object, environmentSpecFlowTelemetryCheckerMock.Object);
 
             // ACT
-            analyticsTransmitter.TransmitEvent(analyticsEventMock.Object);
+            await analyticsTransmitter.TransmitEvent(analyticsEventMock.Object);
 
             // ASSERT
-            analyticsTransmitterSinkMock.Verify(m => m.TransmitEvent(It.IsAny<IAnalyticsEvent>()), Times.Never);
+            analyticsTransmitterSinkMock.Verify(sink => sink.TransmitEvent(It.IsAny<IAnalyticsEvent>()), Times.Never);
         }
 
         [Fact]
-        public void TransmitSpecFlowProjectCompilingEvent_AnalyticsEnabled_ShouldCallSink()
+        public async Task TransmitSpecFlowProjectCompilingEvent_AnalyticsEnabled_ShouldCallSink()
         {
             // ARRANGE
             var analyticsTransmitterSinkMock = new Mock<IAnalyticsTransmitterSink>();
@@ -59,15 +60,10 @@ namespace TechTalk.SpecFlow.GeneratorTests.Analytics
             var analyticsTransmitter = new AnalyticsTransmitter(analyticsTransmitterSinkMock.Object, environmentSpecFlowTelemetryCheckerMock.Object);
 
             // ACT
-            analyticsTransmitter.TransmitSpecFlowProjectCompilingEvent(specFlowProjectCompilingEvent);
+            await analyticsTransmitter.TransmitSpecFlowProjectCompilingEvent(specFlowProjectCompilingEvent);
 
             // ASSERT
             analyticsTransmitterSinkMock.Verify(m => m.TransmitEvent(It.IsAny<IAnalyticsEvent>()), Times.Once);
         }
-    }
-
-    public class AnalyticsEventProviderTests
-    {
-
     }
 }
