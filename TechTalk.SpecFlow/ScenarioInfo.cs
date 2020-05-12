e0e91c01241eaa8422da5bb545be654033b254be
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-05-12 14:57:41 +0300

    Inject scenario arguments to ScenarioInfo

--- TechTalk.SpecFlow/ScenarioInfo.cs
+++ TechTalk.SpecFlow/ScenarioInfo.cs
@@ -1,18 +1,21 @@
 using System;
+using System.Collections.Specialized;
 
 namespace TechTalk.SpecFlow
 {
     public class ScenarioInfo
     {
         public string[] Tags { get; private set; }
+        public IOrderedDictionary Arguments { get; }
         public string Title { get; private set; }
         public string Description { get; private set; }
 
-        public ScenarioInfo(string title, string description, params string[] tags)
+        public ScenarioInfo(string title, string description, string[] tags, IOrderedDictionary arguments)
         {
             Title = title;
             Description = description;
             Tags = tags ?? new string[0];
+            Arguments = arguments;
         }
     }
 }
