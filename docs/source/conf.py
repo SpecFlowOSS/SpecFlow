aa3b7ce8661c6fdc342d85ddf7ba3d86367d7c17
Andreas Willich <andreas@willich.io>
2020-06-05 10:02:57 +0200

    add master_doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,3 +49,4 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+master_doc = 'index'
