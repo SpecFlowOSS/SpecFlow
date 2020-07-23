535e25d51f0f1d945bd4d737b1046190641876dc
Andreas Willich <andreas@willich.io>
2020-07-23 09:33:14 +0200

    add in #1928 removed NullValueRetriever back

---
+++ TechTalk.SpecFlow/Assist/ValueRetrievers/NullValueRetriever.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechTalk.SpecFlow.Assist.ValueRetrievers
+{
+    public class NullValueRetriever : IValueRetriever
+    {
+        private readonly string _nullText;
+
+        public NullValueRetriever(string nullText)
+        {
+            _nullText = nullText;
+        }
+
+        public bool CanRetrieve(KeyValuePair<string, string> keyValuePair, Type targetType, Type propertyType)
+        {
+            return IsNullableType(propertyType) &&
+                   keyValuePair.Value != null &&
+                   string.Compare(keyValuePair.Value.Trim(), _nullText, StringComparison.InvariantCultureIgnoreCase) == 0;
+        }
+
+        public object Retrieve(KeyValuePair<string, string> keyValuePair, Type targetType, Type propertyType)
+        {
+            return null;
+        }
+
+        private static bool IsNullableType(Type type)
+        {
+            return !type.IsValueType || 
+                (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+    }
+}
