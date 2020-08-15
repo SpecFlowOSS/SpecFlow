5fb209ce86b5a0a0f307c30d75d99f6c772e4e06
Per Gårdebrink <per.gardebrink@gmail.com>
2020-08-15 12:20:36 +0200

    Only write contents when it's changed

--- SpecFlow.Tools.MsBuild.Generation/ReplaceTokenInFileTask.cs
+++ SpecFlow.Tools.MsBuild.Generation/ReplaceTokenInFileTask.cs
@@ -12,12 +12,23 @@ namespace SpecFlow.Tools.MsBuild.Generation
         [Required] public string OutputFile { get; set; }
         [Required] public string TextToReplace { get; set; }
         [Required] public string TextToReplaceWith { get; set; }
+        public bool WriteOnlyWhenChanged { get; set; }
         public override bool Execute()
         {
             try
             {
                 var fileContent = File.ReadAllText(InputFile);
                 var replacedContent = fileContent.Replace(TextToReplace, TextToReplaceWith);
+                if (WriteOnlyWhenChanged && File.Exists(OutputFile))
+                {
+                    var existingContent = File.ReadAllText(OutputFile);
+                    if (replacedContent.Equals(existingContent))
+                    {
+                        Log.LogMessage("Skipping unchanged file {0}", OutputFile);
+                        return true;
+                    }
+                }
+
                 var dir = Path.GetDirectoryName(OutputFile);
                 if (!Directory.Exists(dir))
                 {
