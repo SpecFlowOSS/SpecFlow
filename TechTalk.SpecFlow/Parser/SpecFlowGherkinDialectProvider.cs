70cffa526cd0c70c7455ebff9faf52ac36f3e01f
Andreas Willich <andreas@willich.io>
2020-09-24 16:42:23 +0200

    revert delete back

---
+++ TechTalk.SpecFlow/Parser/SpecFlowGherkinDialectProvider.cs
@@ -0,0 +1,31 @@
+﻿using Gherkin;
+using Gherkin.Ast;
+
+namespace TechTalk.SpecFlow.Parser
+{
+    public class SpecFlowGherkinDialectProvider : GherkinDialectProvider
+    {
+        public SpecFlowGherkinDialectProvider(string defaultLanguage) : base(defaultLanguage)
+        {
+        }
+
+        public override GherkinDialect GetDialect(string language, Location location)
+        {
+            if (language.Contains("-"))
+            {
+                try
+                {
+                    return base.GetDialect(language, location);
+                }
+                catch (NoSuchLanguageException)
+                {
+                    var languageBase = language.Split('-')[0];
+                    var languageBaseDialect = base.GetDialect(languageBase, location);
+                    return new GherkinDialect(language, languageBaseDialect.FeatureKeywords, languageBaseDialect.RuleKeywords, languageBaseDialect.BackgroundKeywords, languageBaseDialect.ScenarioKeywords, languageBaseDialect.ScenarioOutlineKeywords, languageBaseDialect.ExamplesKeywords, languageBaseDialect.GivenStepKeywords, languageBaseDialect.WhenStepKeywords, languageBaseDialect.ThenStepKeywords, languageBaseDialect.AndStepKeywords, languageBaseDialect.ButStepKeywords);
+                }
+            }
+
+            return base.GetDialect(language, location);
+        }
+    }
+}
