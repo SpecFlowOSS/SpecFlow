95bb95b116343caa466a06c89708946388c74875
Zsolt Olah <56628780+ozslab@users.noreply.github.com>
2020-11-11 10:43:25 +0100

    9861 result collection bug (#2201)
    Co-authored-by: Andreas Willich <andreas@willich.io>

--- Plugins/TechTalk.SpecFlow.NUnit.Generator.SpecFlowPlugin/build/NUnit.AssemblyHooks.template.cs
+++ Plugins/TechTalk.SpecFlow.NUnit.Generator.SpecFlowPlugin/build/NUnit.AssemblyHooks.template.cs
@@ -5,12 +5,14 @@ using System.CodeDom.Compiler;
 using System.Diagnostics;
 using global::NUnit.Framework;
 using global::TechTalk.SpecFlow;
+using global::System.Runtime.CompilerServices;
 
 [GeneratedCode("SpecFlow", "SPECFLOW_VERSION")]
 [SetUpFixture]
 public class PROJECT_ROOT_NAMESPACE_NUnitAssemblyHooks
 {
     [OneTimeSetUp]
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public void AssemblyInitialize()
     {
         var currentAssembly = typeof(PROJECT_ROOT_NAMESPACE_NUnitAssemblyHooks).Assembly;
@@ -19,6 +21,7 @@ public class PROJECT_ROOT_NAMESPACE_NUnitAssemblyHooks
     }
 
     [OneTimeTearDown]
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public void AssemblyCleanup()
     {
         var currentAssembly = typeof(PROJECT_ROOT_NAMESPACE_NUnitAssemblyHooks).Assembly;
