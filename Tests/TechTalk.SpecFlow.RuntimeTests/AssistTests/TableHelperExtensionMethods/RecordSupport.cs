197bfe85dcf9e3e23deddf4772a4f0bb8f542ce2
Andreas Willich <SabotageAndi@users.noreply.github.com>
2020-11-16 02:34:55 -0800

    Record support (#2206)

---
+++ Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/TableHelperExtensionMethods/RecordSupport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.TableHelperExtensionMethods
+{
+#if NET5_0
+    public class RecordSupport
+    {
+        public record RecordType(string Property);
+
+        [Fact]
+        public void Works_With_Records()
+        {
+            var table = new Table("Property");
+            table.AddRow("Row1");
+
+            var record = table.CreateSet<RecordType>();
+            record.Count().Should().Be(1);
+        }
+
+
+    }
+#endif
+}
