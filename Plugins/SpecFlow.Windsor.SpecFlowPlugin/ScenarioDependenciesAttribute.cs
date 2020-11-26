d80c65ceb5d60ae2dc3040054dc33055d22e913a
Robert Coltheart <13191652+robertcoltheart@users.noreply.github.com>
2020-11-26 22:41:53 +0800

    Remove Windsor scoping and add binding parameter (#2217)

--- Plugins/SpecFlow.Windsor.SpecFlowPlugin/ScenarioDependenciesAttribute.cs
+++ Plugins/SpecFlow.Windsor.SpecFlowPlugin/ScenarioDependenciesAttribute.cs
@@ -5,5 +5,6 @@ namespace SpecFlow.Windsor
     [AttributeUsage(AttributeTargets.Method)]
     public class ScenarioDependenciesAttribute : Attribute
     {
+        public bool AutoRegisterBindings { get; set; } = true;
     }
 }
