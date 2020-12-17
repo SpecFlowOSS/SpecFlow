71b1417c2b12a487378928064d1132915f0ac54f
Andreas Willich <andreas@willich.io>
2020-12-17 13:38:42 +0100

    add unit test for existing functionality

---
+++ Tests/TechTalk.SpecFlow.RuntimeTests/Exceptions/PendingStepExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace TechTalk.SpecFlow.RuntimeTests.Exceptions
+{
+    public class PendingStepExceptionTests
+    {
+        [Fact]
+        public void Constructor_Message_AlwaysSet()
+        {
+            var pendingStepException = new PendingStepException();
+            pendingStepException.Message.Should().NotBeNullOrWhiteSpace();
+        }
+        
+        
+    }
+}
