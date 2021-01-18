9780c50371e4d78481a6d8635a32189632255d08
Balázs Epresi <epresib@gmail.com>
2021-01-18 14:40:58 +0100

    Search for RuntimePlugins in the TestAssembly

--- Tests/TechTalk.SpecFlow.PluginTests/RuntimePluginLocatorTests.cs
+++ Tests/TechTalk.SpecFlow.PluginTests/RuntimePluginLocatorTests.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
 using FluentAssertions;
+using Moq;
+using TechTalk.SpecFlow.Infrastructure;
 using TechTalk.SpecFlow.Plugins;
 using Xunit;
 
@@ -8,16 +15,93 @@ namespace TechTalk.SpecFlow.PluginTests
     public class RuntimePluginLocatorTests
     {
         [Fact]
-        public void LoadPlugins_Find_All_5_Referenced_RuntimePlugins()
+        public void LoadPlugins_Find_TestAssembly()
         {
+            var testAssemblyProvider = new Mock<ITestAssemblyProvider>();
+            var testAssembly = Assembly.GetExecutingAssembly();
+            testAssemblyProvider.Setup(t => t.TestAssembly).Returns(testAssembly);
+
             //ARRANGE
-            var runtimePluginLocator = new RuntimePluginLocator(new RuntimePluginLocationMerger(), new SpecFlowPath());
+            var runtimePluginLocator = new RuntimePluginLocator(new RuntimePluginLocationMerger(), new SpecFlowPath(), testAssemblyProvider.Object);
 
             //ACT
             var plugins = runtimePluginLocator.GetAllRuntimePlugins();
 
             //ASSERT
-            plugins.Count.Should().Be(5, $"{string.Join(",", plugins)} were found");
+            plugins.Any(plugin => plugin.Equals(testAssembly.Location)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void LoadPlugins_Find_All_Referenced_Plugins()
+        {
+            var testAssemblyProvider = new Mock<ITestAssemblyProvider>();
+            var testAssembly = Assembly.GetExecutingAssembly();
+            testAssemblyProvider.Setup(t => t.TestAssembly).Returns(testAssembly);
+
+            //ARRANGE
+            var runtimePluginLocator = new RuntimePluginLocator(new RuntimePluginLocationMerger(), new SpecFlowPath(), testAssemblyProvider.Object);
+
+            //ACT
+            var plugins = runtimePluginLocator.GetAllRuntimePlugins();
+
+            //ASSERT
+            var projectReferences = GetProjectReferencesDlls();
+            var allProjectReferenceFoundByPluginLocator = projectReferences.All(pr => plugins.Any(plugin => plugin.Contains(pr)));
+            allProjectReferenceFoundByPluginLocator.Should().BeTrue();
+
+            var numberOfGeneratorPlugins = NumberOfGeneratorPluginsReferenced(projectReferences);
+            var generatorPluginsFound = plugins.Where(p => p.Contains("Generator")).ToList();
+            generatorPluginsFound.Count.Should().Be(numberOfGeneratorPlugins);
+
+            var numberOfRuntimePlugins = NumberOfRuntimePluginsReferenced(projectReferences);
+            var runtimePluginsFound = plugins.Where(p => !p.Equals(testAssembly.Location))
+                                        .Except(generatorPluginsFound).ToList();
+            runtimePluginsFound.Count.Should().Be(numberOfRuntimePlugins, $"{string.Join(",", runtimePluginsFound)} were found");
+        }
+
+        private int NumberOfGeneratorPluginsReferenced(List<string> projectReferences)
+        {
+            return projectReferences.Count(p => p.Contains("Generator"));
+        }
+
+        private int NumberOfRuntimePluginsReferenced(List<string> projectReferences)
+        {
+            return projectReferences.Count(p => !p.Contains("Generator"));
+        }
+
+        private List<string> GetProjectReferencesDlls()
+        {
+            var projectReferences = GetProjectReferences();
+
+            var projectDlls = new List<string>();
+            foreach (string projectReference in projectReferences)
+            {
+                //real ref, not test dependent
+                if (projectReference.Contains("TechTalk.SpecFlow.Generator.csproj"))
+                    continue;
+
+                var index = projectReference.LastIndexOf("\\", StringComparison.InvariantCulture);
+                var projectName = projectReference.Substring(index + 1);
+                projectDlls.Add(projectName.Replace("csproj", "dll"));
+            }
+
+            return projectDlls;
+        }
+
+        private List<string> GetProjectReferences()
+        {
+            var dir = Environment.CurrentDirectory;
+            var csprojPath = Path.Combine(dir, "..", "..", "..", "TechTalk.SpecFlow.PluginTests.csproj");
+            var project = XDocument.Load(csprojPath);
+
+            var projectReferences = project.Element("Project")
+                                           .Elements("ItemGroup")
+                                           .Elements("ProjectReference")
+                                           .Attributes("Include")
+                                           .Select(i => i.Value)
+                                           .ToList();
+
+            return projectReferences;
         }
 
     }
