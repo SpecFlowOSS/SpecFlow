b5560a235f6e30da734c6360214822e2e059d341
Rami <icnocop@users.noreply.github.com>
2021-02-12 06:30:13 -0800

    Automatically include TechTalk.SpecFlow.MSTest.SpecFlowPlugin.dll as a deployment item for MSTest V2 (#2301)
    Fixes #2076
    
    Co-authored-by: Andreas Willich <SabotageAndi@users.noreply.github.com>

---
+++ Tests/TechTalk.SpecFlow.GeneratorTests/UnitTestProvider/MsTestV2GeneratorProviderTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow.Generator.CodeDom;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Parser;
+using Xunit;
+
+namespace TechTalk.SpecFlow.GeneratorTests.UnitTestProvider
+{
+    public class MsTestV2GeneratorProviderTests
+    {
+        private const string TestDeploymentItemAttributeName = "Microsoft.VisualStudio.TestTools.UnitTesting.DeploymentItemAttribute";
+
+        [Fact]
+        public void MsTestV2GeneratorProvider_WithFeatureWithoutDeploymentItem_GeneratedClassShouldNotIncludeDeploymentItemForPlugin()
+        {
+            // ARRANGE
+            var document = ParseDocumentFromString(@"
+            Feature: Sample feature file
+
+            Scenario: Simple scenario
+                Given there is something
+                When I do something
+                Then something should happen");
+            var sampleTestGeneratorProvider = new MsTestV2GeneratorProvider(new CodeDomHelper(CodeDomProviderLanguage.CSharp));
+            var converter = sampleTestGeneratorProvider.CreateUnitTestConverter();
+
+            // ACT
+            var code = converter.GenerateUnitTestFixture(document, "TestClassName", "Target.Namespace");
+
+            // ASSERT
+            code.Class().CustomAttributes().Any(a => a.Name == TestDeploymentItemAttributeName).Should().BeFalse();
+        }
+
+        [Fact]
+        public void MsTestV2GeneratorProvider_WithFeatureContainingDeploymentItem_GeneratedClassShouldIncludeDeploymentItemForPlugin()
+        {
+            // ARRANGE
+            var document = ParseDocumentFromString(@"
+            @MsTest:DeploymentItem:DeploymentItemTestFile.txt
+            Feature: Sample feature file
+
+            Scenario: Simple scenario
+                Given there is something
+                When I do something
+                Then something should happen");
+
+            var sampleTestGeneratorProvider = new MsTestV2GeneratorProvider(new CodeDomHelper(CodeDomProviderLanguage.CSharp));
+            var converter = sampleTestGeneratorProvider.CreateUnitTestConverter();
+
+            // ACT
+            var code = converter.GenerateUnitTestFixture(document, "TestClassName", "Target.Namespace");
+
+            // ASSERT
+            var deploymentItemAttributeForClass = code.Class().CustomAttributes().Single(a => a.Name == TestDeploymentItemAttributeName);
+            deploymentItemAttributeForClass.ArgumentValues().First().Should().Be("TechTalk.SpecFlow.MSTest.SpecFlowPlugin.dll");
+        }
+
+        public SpecFlowDocument ParseDocumentFromString(string documentSource, CultureInfo parserCultureInfo = null)
+        {
+            var parser = new SpecFlowGherkinParser(parserCultureInfo ?? new CultureInfo("en-US"));
+            using (var reader = new StringReader(documentSource))
+            {
+                var document = parser.Parse(reader, null);
+                document.Should().NotBeNull();
+                return document;
+            }
+        }
+    }
+}
