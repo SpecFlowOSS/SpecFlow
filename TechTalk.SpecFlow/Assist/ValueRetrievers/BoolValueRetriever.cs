c4d82f77c57e7c5c946933955eb88c1116c600d7
RuslanUvarov <79268984+RuslanUvarov@users.noreply.github.com>
2021-03-01 12:20:39 +0200

    Gh1901 Add support multilingual bool value (#2332)

--- TechTalk.SpecFlow/Assist/ValueRetrievers/BoolValueRetriever.cs
+++ TechTalk.SpecFlow/Assist/ValueRetrievers/BoolValueRetriever.cs
@@ -1,10 +1,18 @@
-﻿namespace TechTalk.SpecFlow.Assist.ValueRetrievers
-{
-    public class BoolValueRetriever : StructRetriever<bool>
-    {
-        protected override bool GetNonEmptyValue(string value)
-        {
-            return value == "True" || value == "true" || value == "1";
+﻿using System.Collections.Generic;
+
+namespace TechTalk.SpecFlow.Assist.ValueRetrievers {
+
+    public class BoolValueRetriever : StructRetriever<bool> {
+        private static readonly List<string> MultiLingualTrueValues = new List<string> {
+            "true", "1", "thật", "to'g'ri", "to'g'ri", "سچ ہے", "правда", "doğru", "จริง", "నిజం", "дөрес", "உண்மை",
+            "sann", "verdadera", "verdadero", "prav", "pravda", "istina", "истинито", "истина", "adevărat", "verdade",
+            "prawdziwe", "درست است، واقعی", "ਸੱਚ ਹੈ", "hold", "ekte", "үнэн", "benar", "richteg", "motina", "taisnība",
+            "진실", "teH", "ನಿಜ", "bener", "vera", "vero", "satt", "igaz", "सच", "נָכוֹן", "સાચું", "αληθής", "stimmt", "certo",
+            "vraise", "totta", "tõsi", "vera", "waark", "Sandt", "vre", "真", "真", "veritable", "истински", "istinito", "ճիշտ", "صحيح", "wier"
+        };
+
+        protected override bool GetNonEmptyValue(string value) {
+            return !string.IsNullOrWhiteSpace(value) && MultiLingualTrueValues.Contains(value.ToLower());
         }
     }
 }
