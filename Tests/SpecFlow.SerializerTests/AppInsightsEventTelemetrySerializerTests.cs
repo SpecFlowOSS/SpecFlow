b27c66a739c4afc355df8afda48ebaa3c91b6ec4
Balázs Epresi <epresib@gmail.com>
2021-03-12 14:09:24 +0100

    Add for inline json parser

---
+++ Tests/SpecFlow.SerializerTests/AppInsightsEventTelemetrySerializerTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Analytics;
+using TechTalk.SpecFlow.Analytics.AppInsights;
+using Xunit;
+
+namespace SerializerTests
+{
+    public class AppInsightsEventTelemetrySerializerTests
+    {
+        [Fact]
+        public void AppInsightsEventSerializationResultsTheSameString()
+        {
+            var eventTelemetry = CreateSampleAppInsinghtsEvent();
+
+            var utf8JsonResult = Utf8Json.JsonSerializer.ToJsonString(eventTelemetry);
+            var tinyJsonResult = eventTelemetry.ToJson();
+
+            Assert.Equal(utf8JsonResult, tinyJsonResult);
+        }
+
+        [Fact]
+        public void AppInsightsEventSerializationResultsTheSameByteArray()
+        {
+            var eventTelemetry = CreateSampleAppInsinghtsEvent();
+
+            var utf8JsonResult = Utf8Json.JsonSerializer.Serialize(eventTelemetry);
+            var tinyJsonResult = Encoding.UTF8.GetBytes(eventTelemetry.ToJson()); 
+
+            Assert.Equal(utf8JsonResult, tinyJsonResult);
+        }
+
+        private AppInsightsEventTelemetry CreateSampleAppInsinghtsEvent()
+        {
+            var analyticsEvent = new SpecFlowProjectCompilingEvent(DateTime.Now, Guid.NewGuid().ToString(),
+                                     "Windows", "Microsoft Windows 10.0.19041", "3.7.13+7faab2b5c0",
+                                     "xunit", "", "852c8c2dbf14a96a81045593b3ff011d",
+                                     "netcoreapp3.1;net5.0", "netcoreapp3.1", "16.6.0", "", false);
+
+            return new AppInsightsEventTelemetry(analyticsEvent, Guid.NewGuid().ToString());
+        }
+    }
+}
---
+++ Tests/SpecFlow.SerializerTests/ExternalDataTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TechTalk.SpecFlow;
+using Utf8Json;
+using Xunit;
+using Xunit.Sdk;
+
+namespace SerializerTests
+{
+    public class ExternalDataTests
+    {
+        [Fact]
+        public void TestDataDeserializationResultsTheSameObject()
+        {
+            var content = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "testdata.json"));
+
+            var utf8JsonResult = JsonSerializer.Deserialize<dynamic>(content);
+            var tinyJsonResult = content.FromJson<dynamic>();
+
+            var utf8Dict = utf8JsonResult as Dictionary<string, object>;
+            var tinyDict = tinyJsonResult as Dictionary<string, object>;
+
+            Assert.NotNull(utf8Dict);
+            Assert.NotNull(tinyDict);
+
+            var dictEqual = DictionariesEqual(utf8Dict, tinyDict);
+            Assert.True(dictEqual);
+        }
+
+        private bool DictionariesEqual(Dictionary<string, object> dict1, Dictionary<string, object> dict2)
+        {
+            foreach (var pair in dict1)
+            {
+                if (!dict2.ContainsKey(pair.Key)) throw new XunitException($"missing key: {pair.Key}");
+
+                var dict2Value = dict2[pair.Key];
+
+                if (pair.Value.GetType() == typeof(Dictionary<string, object>))
+                {
+                    DictionariesEqual(pair.Value as Dictionary<string, object>, dict2Value as Dictionary<string, object>);
+                }
+                else if (pair.Value.GetType() == typeof(List<object>))
+                {
+                    if (!(pair.Value as List<object>).SequenceEqual(dict2Value as List<object>))
+                        throw new XunitException($"values not equal. Value1: {pair.Value} - Value2: {dict2Value}");
+                }
+                else
+                {
+                    if (!pair.Value.Equals(dict2Value))
+                        throw new XunitException($"values not equal. Value1: {pair.Value} - Value2: {dict2Value}");
+                }
+            }
+            return true;
+        }
+    }
+}
