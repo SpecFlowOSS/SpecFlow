fcab8d3c472c71676b929cc6efe311fd1945217e
bollhals <boli89@gmail.com>
2021-04-26 22:47:37 +0200

    introduce and use TagHelper in UnitTestGeneration

---
+++ TechTalk.SpecFlow/TagHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace TechTalk.SpecFlow
+{
+    #nullable enable
+    /// <summary>
+    /// Provides helper methods around tags.
+    /// </summary>
+    public static class TagHelper
+    {
+        /// <summary>
+        /// Checks whether the supplied tags contain the ignore tag
+        /// </summary>
+        /// <param name="tags">The tags to check.</param>
+        /// <returns>A boolean that indicates whether or not the ignore tag is present.</returns>
+        public static bool ContainsIgnoreTag(string[]? tags)
+        {
+            if (tags is null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < tags.Length; i++)
+            {
+                if (string.Equals(tags[i], "ignored", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
