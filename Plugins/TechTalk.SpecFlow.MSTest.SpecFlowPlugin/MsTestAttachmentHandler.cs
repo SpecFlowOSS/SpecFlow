ae68d64518c06ad8bbb2f11381275d51ef69f5a7
Balázs Epresi <epresib@gmail.com>
2021-05-05 13:25:03 +0200

    Update MsTest.TestFramework to 2.1.2 to be able to use TestContext's AddResultFile method

---
+++ Plugins/TechTalk.SpecFlow.MSTest.SpecFlowPlugin/MsTestAttachmentHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.Tracing;
+
+namespace TechTalk.SpecFlow.MSTest.SpecFlowPlugin
+{
+    public class MSTestAttachmentHandler : SpecFlowAttachmentHandler
+    {
+        private readonly TestContext _testContext;
+
+        public MSTestAttachmentHandler(ITraceListener traceListener, TestContext testContext) : base(traceListener)
+        {
+            _testContext = testContext;
+        }
+
+        public override void AddAttachment(string filePath)
+        {
+            try
+            {
+                _testContext.AddResultFile(filePath);
+            }
+            catch (Exception)
+            {
+                base.AddAttachment(filePath);
+            }
+        }
+    }
+}
