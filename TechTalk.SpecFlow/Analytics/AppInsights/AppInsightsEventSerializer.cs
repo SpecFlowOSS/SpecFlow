8a0c811b903de452136b3a6180c717b21a347514
Balázs Epresi <epresib@gmail.com>
2021-06-08 07:05:19 +0200

    Replace Utf8Json with SpecFlow.Internal.Json (#2436)
    * Replace Utf8Json to SpecFlow.Internal.Json
    
    * Fix ValueTuple reference in csproj
    
    Utf8Json had a dependency on it so we didn't have to use PackageReference before
    
    * Update SpecFlow nuspec
    
    * Update version
    
    * Update changelog
    
    * Replace Newtonsoft.Json in Specs
    
    * Update TestProjectGenerator
    
    * Add SpecFlow MyGet to the nuget sources
    
    * Update submodule
    
    * Update submodule

--- TechTalk.SpecFlow/Analytics/AppInsights/AppInsightsEventSerializer.cs
+++ TechTalk.SpecFlow/Analytics/AppInsights/AppInsightsEventSerializer.cs
@@ -1,11 +1,14 @@
-﻿namespace TechTalk.SpecFlow.Analytics.AppInsights
+﻿using System.Text;
+using SpecFlow.Internal.Json;
+
+namespace TechTalk.SpecFlow.Analytics.AppInsights
 {
     public class AppInsightsEventSerializer : IAppInsightsEventSerializer
     {
         public byte[] SerializeAnalyticsEvent(IAnalyticsEvent analyticsEvent, string instrumentationKey)
         {
             var eventTelemetry = new AppInsightsEventTelemetry(analyticsEvent, instrumentationKey);
-            return Utf8Json.JsonSerializer.Serialize(eventTelemetry);
+            return Encoding.UTF8.GetBytes(eventTelemetry.ToJson());
         }
     }
 }
