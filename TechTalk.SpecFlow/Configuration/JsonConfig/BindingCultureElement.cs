ad3196beef314c32fdfd2a05d5af679133115151
Balázs Epresi <epresib@gmail.com>
2021-06-11 12:58:04 +0200

    BindingCulture default should not be set

--- TechTalk.SpecFlow/Configuration/JsonConfig/BindingCultureElement.cs
+++ TechTalk.SpecFlow/Configuration/JsonConfig/BindingCultureElement.cs
@@ -8,7 +8,6 @@ namespace TechTalk.SpecFlow.Configuration.JsonConfig
     public class BindingCultureElement
     {
         //[JsonProperty("name", DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
-        [DefaultValue("en-US")]
         [DataMember(Name="name")]
         public string Name { get; set; }
     }
