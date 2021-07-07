cdb85911215fa4437ce64829eff676c46f100e17
Alexandra Horváth-Fung <fung.alexandra@gmail.com>
2021-07-07 11:36:53 +0200

    remove disabling the warning - no warning anymore

--- TechTalk.SpecFlow/Configuration/ObsoleteBehavior.cs
+++ TechTalk.SpecFlow/Configuration/ObsoleteBehavior.cs
@@ -1,14 +1,10 @@
 ﻿namespace TechTalk.SpecFlow.Configuration
 {
-
-#pragma warning disable CS0419 // Ambiguous reference in cref attribute
     /// <summary>
     /// Describes possibilities what to do when a test is marked with <see cref="System.ObsoleteAttribute"/>
     /// The default is <see cref="Warn"/> 
     /// </summary>
-    //TODO: ask Andi
     public enum ObsoleteBehavior
-#pragma warning restore CS0419 // Ambiguous reference in cref attribute
     {
         None = 0,
         Warn = 1,
