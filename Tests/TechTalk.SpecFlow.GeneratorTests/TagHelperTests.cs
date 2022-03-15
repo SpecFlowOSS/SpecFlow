c2a0ed1acaa949790f1b91dab5a63b429688a6e2
Josh Keegan <JoshKeegan@users.noreply.github.com>
2022-03-15 09:03:34 +0000

    Bug fix: @ignore tag check was looking for the wrong value (#2572)

---
+++ Tests/TechTalk.SpecFlow.GeneratorTests/TagHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace TechTalk.SpecFlow.GeneratorTests
+{
+    public class TagHelperTests
+    {
+        public static object[][] ContainsIgnoreTag_Examples_Returns_Expected_Data = new[]
+        {
+            new object[] { new[] { "ignore" }, true },
+            new object[] { new[] { "Ignore" }, true },
+            new object[] { new[] { "IGNORE" }, true },
+            new object[] { new[] { "anything", "ignore" }, true },
+            new object[] { null, false },
+            new object[] { new[] { "ignored" }, false },
+            new object[] { Array.Empty<string>(), false },
+        };
+
+        [Theory]
+        [MemberData(nameof(ContainsIgnoreTag_Examples_Returns_Expected_Data))]
+        public void ContainsIgnoreTag_Examples_Returns_Expected(string[] tags, bool expected)
+        {
+            bool actual = TagHelper.ContainsIgnoreTag(tags);
+            actual.Should().Be(expected);
+        }
+    }
+}
