5e14699d37795ff38669559befc0e6c93bdef11a
delta-emil <delta.emil@gmail.com>
2022-04-12 14:26:16 +0300

    expand property aliases support (#1384) (#2581)
    Co-authored-by: Emil Tzvetkov <e.tzvetkov@fio.de>

---
+++ Tests/TechTalk.SpecFlow.RuntimeTests/AssistTests/TestInfrastructure/AliasedTestObject.cs
@@ -0,0 +1,34 @@
+﻿using TechTalk.SpecFlow.Assist.Attributes;
+
+namespace TechTalk.SpecFlow.RuntimeTests.AssistTests.TestInfrastructure
+{
+    public class AliasedTestObject
+    {
+        [TableAliases("Alias[ ]*One", "First[ ]?Name", "^Known As$")]
+        public string PropertyOne { get; set; }
+
+        [TableAliases("Alias[ ]*Two", "Middle[ ]?Name", "^Never Known As$")]
+        public string PropertyTwo { get; set; }
+
+        [TableAliases("AliasThree")]
+        [TableAliases("Surname")]
+        [TableAliases("Last[ ]?name")]
+        [TableAliases("Dad's Last Name")]
+        public string PropertyThree { get; set; }
+
+#pragma warning disable 649
+        [TableAliases("FieldAliasOne")]
+        public string FieldOne;
+        [TableAliases("FieldAliasTwo")]
+        public string FieldTwo;
+        [TableAliases("FieldAliasThree")]
+        public string FieldThree;
+#pragma warning restore 649
+
+        [TableAliases("AliasOne")]
+        public string AnotherPropertyWithSameAlias { get; set; }
+
+        [TableAliases("AliasOne")]
+        public long AliasedButTypeMismatch { get; set; }
+    }
+}
