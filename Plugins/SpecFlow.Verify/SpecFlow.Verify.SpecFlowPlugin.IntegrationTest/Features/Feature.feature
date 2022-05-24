14c488232e46f6824244c75c5fbbf530151c4428
Andreas Willich <a.willich@tricentis.com>
2022-05-24 09:41:49 +0200

    create plugin skeleton

---
+++ Plugins/SpecFlow.Verify/SpecFlow.Verify.SpecFlowPlugin.IntegrationTest/Features/Feature.feature
@@ -0,0 +1,9 @@
+﻿Feature: Put feature-title here
+	Put feature description here
+
+Background:
+    Given Put your Background here
+
+Scenario: Put your Scenario here
+	When Put your Action here
+    Then Put your Condition here
---
+++ Plugins/SpecFlow.Verify/SpecFlow.Verify.SpecFlowPlugin/VerifyDecorator.cs
@@ -0,0 +1,28 @@
+﻿using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.CodeDom;
+using TechTalk.SpecFlow.Generator.UnitTestConverter;
+
+namespace SpecFlow.Verify.SpecFlowPlugin;
+
+public class VerifyDecorator : ITestClassTagDecorator
+{
+    private readonly CodeDomHelper _codeDomHelper;
+
+    public VerifyDecorator(CodeDomHelper codeDomHelper)
+    {
+        _codeDomHelper = codeDomHelper;
+    }
+
+    public int Priority { get; }
+
+    public bool RemoveProcessedTags { get; }
+
+    public bool ApplyOtherDecoratorsForProcessedTags { get; }
+
+    public bool CanDecorateFrom(string tagName, TestClassGenerationContext generationContext) => true;
+
+    public void DecorateFrom(string tagName, TestClassGenerationContext generationContext)
+    {
+        _codeDomHelper.AddAttribute(generationContext.TestClass, "UsesVerify");
+    }
+}
