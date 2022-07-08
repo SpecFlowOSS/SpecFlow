a69707eaef9c87b3d454d60598a1fe45292d579d
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-07-08 08:55:01 +0200

    Cleanup ScenarioInfo

--- TechTalk.SpecFlow/ScenarioInfo.cs
+++ TechTalk.SpecFlow/ScenarioInfo.cs
@@ -4,22 +4,37 @@ using System.Linq;
 
 namespace TechTalk.SpecFlow
 {
+    /// <summary>
+    /// Contains information about the scenario currently being executed.
+    /// </summary>
     public class ScenarioInfo
     {
         /// <summary>
-        /// Direct tags of the scenario.
+        /// Direct tags of the scenario, including tags of the examples block.
         /// </summary>
-        public string[] Tags { get; private set; }
+        public string[] Tags { get; }
         /// <summary>
         /// Contains direct tags and tags inherited from the feature and the rule.
         /// </summary>
         public string[] CombinedTags { get; private set; }
 
-        [Obsolete($"Deprecated. Use '{nameof(CombinedTags)}' instead.")]
+        [Obsolete($"Deprecated. Use '{nameof(CombinedTags)}' instead.", false)]
         public string[] ScenarioAndFeatureTags => CombinedTags;
+        
+        /// <summary>
+        /// The arguments used to execute a scenario outline example.
+        /// </summary>
         public IOrderedDictionary Arguments { get; }
-        public string Title { get; private set; }
-        public string Description { get; private set; }
+
+        /// <summary>
+        /// The title (name) of the scenario.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// The description of the scenario.
+        /// </summary>
+        public string Description { get; }
 
         public ScenarioInfo(string title, string description, string[] tags, IOrderedDictionary arguments, string[] inheritedTags = null)
         {
