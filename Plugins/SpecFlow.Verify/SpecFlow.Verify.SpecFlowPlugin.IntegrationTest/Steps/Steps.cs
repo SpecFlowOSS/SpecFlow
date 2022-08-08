acf4d7a389ac4de934e09076ce6f01f2966beaa3
Andreas Willich <a.willich@tricentis.com>
2022-08-08 13:33:20 +0200

    adjust filename generation

--- Plugins/SpecFlow.Verify/SpecFlow.Verify.SpecFlowPlugin.IntegrationTest/Steps/Steps.cs
+++ Plugins/SpecFlow.Verify/SpecFlow.Verify.SpecFlowPlugin.IntegrationTest/Steps/Steps.cs
@@ -1,22 +1,60 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections;
 using TechTalk.SpecFlow;
-using VerifyXunit;
+using System.Linq;
 
-namespace SpecFlow.Verify.SpecFlowPlugin.IntegrationTest.Steps
+namespace SpecFlow.Verify.SpecFlowPlugin.IntegrationTest.Steps;
+
+[Binding]
+internal class Steps
 {
-    [Binding]
-    internal class Steps
-    {
-        [When("I try Verify with SpecFlow")]
-        public async Task ITryVerifyWithSpecFlow()
-        {
-            await Verifier.Verify("value");
-        }
+    private readonly FeatureContext _featureContext;
+    private readonly ScenarioContext _scenarioContext;
 
-        [Then("it works")]
-        public void ItWorks()
-        {
-            
-        }
+    public Steps(ScenarioContext scenarioContext, FeatureContext featureContext)
+    {
+        _scenarioContext = scenarioContext;
+        _featureContext = featureContext;
+    }
+
+    [BeforeScenario]
+    public void SetupVerify()
+    {
+        VerifierSettings.DerivePathInfo(
+            (sourceFile, projectDirectory, type, method) =>
+            {
+                string scenarioInfoTitle = _scenarioContext.ScenarioInfo.Title;
+
+                if (_scenarioContext.ScenarioInfo.Arguments.Count > 0)
+                {
+                    scenarioInfoTitle += "_";
+
+                    foreach (DictionaryEntry scenarioInfoArgument in _scenarioContext.ScenarioInfo.Arguments)
+                    {
+                        scenarioInfoTitle += "_" + scenarioInfoArgument.Value;
+                    }
+                }
+
+                return new PathInfo(
+                    Path.Combine(projectDirectory, _featureContext.FeatureInfo.FolderPath),
+                    _featureContext.FeatureInfo.Title,
+                    scenarioInfoTitle);
+            });
+    }
+
+    [When("I try Verify with SpecFlow")]
+    public async Task ITryVerifyWithSpecFlow()
+    {
+        await Verifier.Verify("value");
+    }
+
+    [When(@"I try Verify with SpecFlow for Parameter '([^']*)'")]
+    public async Task WhenITryVerifyWithSpecFlowForParameter(string p0)
+    {
+        await Verifier.Verify("value");
+    }
+
+    [Then("it works")]
+    public void ItWorks()
+    {
     }
 }
