8787ada12c9bf3c2d9f9ee06979a97ef134576c0
Andreas Willich <SabotageAndi@users.noreply.github.com>
2022-08-10 17:06:56 +0200

    Support for Verify (#2602)

---
+++ Plugins/SpecFlow.Verify/SpecFlow.Verify.SpecFlowPlugin.IntegrationTest/Features/Feature.feature
@@ -0,0 +1,14 @@
+﻿Feature: Verify Test
+
+Scenario: Check if Verify is working
+	When I try Verify with SpecFlow
+	Then it works
+    
+Scenario Outline: Check if Verify is working with Example Tables
+	When I try Verify with SpecFlow for Parameter '<Parameter>'
+	Then it works
+
+Examples: 
+	| Parameter |
+	| 1         |
+	| 2         |
---
+++ Plugins/SpecFlow.Verify/SpecFlow.Verify.SpecFlowPlugin.IntegrationTest/Steps/Steps.cs
@@ -0,0 +1,24 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlow.Verify.SpecFlowPlugin.IntegrationTest.Steps;
+
+[Binding]
+internal class Steps
+{
+    [When("I try Verify with SpecFlow")]
+    public async Task ITryVerifyWithSpecFlow()
+    {
+        await Verifier.Verify("value");
+    }
+
+    [When(@"I try Verify with SpecFlow for Parameter '([^']*)'")]
+    public async Task WhenITryVerifyWithSpecFlowForParameter(string p0)
+    {
+        await Verifier.Verify("value");
+    }
+
+    [Then("it works")]
+    public void ItWorks()
+    {
+    }
+}
