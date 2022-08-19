b735e9dbfd22fc7d35472fcaba18b1a6f7b43d1c
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2022-08-19 13:57:53 -0500

    #2626 Code Cleanup for PR
    Modified location of variable declaration in EnumerableValueRetriever to make the code cleaner.
    Added this change to the changelog.txt

--- TechTalk.SpecFlow/Assist/ValueRetrievers/EnumerableValueRetriever.cs
+++ TechTalk.SpecFlow/Assist/ValueRetrievers/EnumerableValueRetriever.cs
@@ -36,11 +36,10 @@ namespace TechTalk.SpecFlow.Assist.ValueRetrievers
 
         private IEnumerable GetItems(string[] strings, KeyValuePair<string, string> keyValuePair, Type targetType, Type itemType)
         {
-            IValueRetriever retriever = null;
             foreach (var splitValue in strings)
             {
                 var itemKeyValuePair = new KeyValuePair<string, string>(keyValuePair.Key, splitValue.Trim());
-                retriever = GetValueRetriever(itemKeyValuePair, targetType, itemType);
+                var retriever = GetValueRetriever(itemKeyValuePair, targetType, itemType);
                 yield return retriever?.Retrieve(itemKeyValuePair, targetType, itemType);
             }
         }
