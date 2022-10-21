e0225d137e87559e3401519a68a2c6fcdddf06b8
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-10-21 22:05:02 +0200

    first idea to fix #2600

--- TechTalk.SpecFlow/Bindings/BindingDelegateInvoker.cs
+++ TechTalk.SpecFlow/Bindings/BindingDelegateInvoker.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TechTalk.SpecFlow.Bindings
 {
     public class BindingDelegateInvoker : IBindingDelegateInvoker
     {
+#if NETCOREAPP
+        private ExecutionContext _executionContext = null;
+#endif
+
         public virtual async Task<object> InvokeDelegateAsync(Delegate bindingDelegate, object[] invokeArgs)
         {
-            if (AsyncMethodHelper.IsAwaitable(bindingDelegate.Method.ReturnType))
-                return await InvokeBindingDelegateAsync(bindingDelegate, invokeArgs);
-            return InvokeBindingDelegateSync(bindingDelegate, invokeArgs);
+#if NETCOREAPP
+            if (_executionContext == null)
+                _executionContext = ExecutionContext.Capture();
+            else
+                ExecutionContext.Restore(_executionContext);
+#endif        
+            try
+            {
+                if (AsyncMethodHelper.IsAwaitable(bindingDelegate.Method.ReturnType))
+                    return await InvokeBindingDelegateAsync2(bindingDelegate, invokeArgs);
+                return InvokeBindingDelegateSync(bindingDelegate, invokeArgs);
+            }
+            finally
+            {
+#if NETCOREAPP
+                _executionContext = ExecutionContext.Capture();
+#endif
+            }
         }
 
         protected virtual object InvokeBindingDelegateSync(Delegate bindingDelegate, object[] invokeArgs)
@@ -23,6 +43,18 @@ namespace TechTalk.SpecFlow.Bindings
             if (AsyncMethodHelper.IsAwaitableAsTask(result, out var task))
                 await task;
             return result;
+        }
+
+        protected virtual Task<object> InvokeBindingDelegateAsync2(Delegate bindingDelegate, object[] invokeArgs)
+        {
+            var result = bindingDelegate.DynamicInvoke(invokeArgs);
+            if (AsyncMethodHelper.IsAwaitableAsTask(result, out var task))
+            {
+                if (task is Task<object> taskOfObj)
+                    return taskOfObj;
+                return task.ContinueWith(_ => (object)null);
+            }
+            return Task.FromResult(result);
         }
     }
 }
