39abf3cac3f323f3c6f026ded6673e44eda9373d
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-10-24 08:06:04 +0200

    Add BindingInvokerTests with a sample test to show usage

---
+++ Tests/TechTalk.SpecFlow.RuntimeTests/Bindings/BindingInvokerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+using BoDi;
+using FluentAssertions;
+using Moq;
+using TechTalk.SpecFlow.Bindings;
+using TechTalk.SpecFlow.Bindings.Reflection;
+using TechTalk.SpecFlow.Configuration;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.RuntimeTests.ErrorHandling;
+using TechTalk.SpecFlow.Tracing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TechTalk.SpecFlow.RuntimeTests.Bindings;
+
+public class BindingInvokerTests
+{
+    private static ITestOutputHelper _testOutputHelperInstance;
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public BindingInvokerTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+        _testOutputHelperInstance = _testOutputHelper; // to be able to access it from step def classes
+    }
+
+    private BindingInvoker CreateSut()
+    {
+        return new BindingInvoker(ConfigurationLoader.GetDefault(), new StubErrorProvider(), new BindingDelegateInvoker());
+    }
+
+    class SampleStepDefClass
+    {
+        public string CapturedValue = null;
+
+        public void SampleSyncStepDef(int someParam)
+        {
+            _testOutputHelperInstance.WriteLine($"some info here: {someParam}");
+        }
+
+        public async Task SampleAsyncStepDef(string otherParam)
+        {
+            _testOutputHelperInstance.WriteLine($"some info here: {otherParam}");
+            CapturedValue = otherParam;
+        }
+    }
+
+    class TestableMethodBinding : MethodBinding
+    {
+        public TestableMethodBinding(IBindingMethod bindingMethod) : base(bindingMethod)
+        {
+        }
+    }
+
+    private async Task InvokeBindingAsync(BindingInvoker sut, IContextManager contextManager, Type stepDefType, string methodName, params object[] args)
+    {
+        var testTracerMock = new Mock<ITestTracer>();
+        var setMethodBinding = new TestableMethodBinding(new RuntimeBindingMethod(stepDefType.GetMethod(methodName)));
+        await sut.InvokeBindingAsync(setMethodBinding, contextManager, args, testTracerMock.Object, new DurationHolder());
+    }
+
+    private IContextManager CreateContextManagerWith()
+    {
+        var contextManagerMock = new Mock<IContextManager>();
+        var scenarioContainer = new ObjectContainer();
+        var scenarioContext = new ScenarioContext(scenarioContainer, new ScenarioInfo("S1", null, null, null), new TestObjectResolver());
+        contextManagerMock.SetupGet(ctx => ctx.ScenarioContext).Returns(scenarioContext);
+        var contextManager = contextManagerMock.Object;
+        return contextManager;
+    }
+
+    [Fact]
+    public async Task Sample_binding_invoker_test()
+    {
+        _testOutputHelper.WriteLine("starting sample test");
+        var sut = CreateSut();
+        var contextManager = CreateContextManagerWith();
+
+        // call step definition methods
+        await InvokeBindingAsync(sut, contextManager, typeof(SampleStepDefClass), nameof(SampleStepDefClass.SampleSyncStepDef), 42);
+        await InvokeBindingAsync(sut, contextManager, typeof(SampleStepDefClass), nameof(SampleStepDefClass.SampleAsyncStepDef), "42");
+
+        // this is how to get THE instance of the step definition class
+        var stepDefClass = contextManager.ScenarioContext.ScenarioContainer.Resolve<SampleStepDefClass>();
+        stepDefClass.CapturedValue.Should().Be("42");
+    }
+}
