117c0084282ceac731bd1d07d7a6dba338c4d1ff
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-10-26 14:51:17 +0200

    Improve idea to work on .NET 4

--- TechTalk.SpecFlow/Bindings/BindingDelegateInvoker.cs
+++ TechTalk.SpecFlow/Bindings/BindingDelegateInvoker.cs
@@ -6,30 +6,51 @@ namespace TechTalk.SpecFlow.Bindings
 {
     public class BindingDelegateInvoker : IBindingDelegateInvoker
     {
-#if NETCOREAPP
         private ExecutionContext _executionContext = null;
-#endif
 
         public virtual async Task<object> InvokeDelegateAsync(Delegate bindingDelegate, object[] invokeArgs)
         {
-#if NETCOREAPP
-            if (_executionContext == null)
-                _executionContext = ExecutionContext.Capture();
-            else
-                ExecutionContext.Restore(_executionContext);
-#endif        
+            // To be able to simulate the behavior of sequential async or sync steps in a test, we need to ensure that
+            // the next step continues with the ExecutionContext that the previous step finished with. 
+            //
+            // Without preserving the ExecutionContext this would not happen because the async methods all the way up to the 
+            // generated test method (for example this method) are discarding the ExecutionContext changes at the end, so the 
+            // next step would start with an "empty" ExecutionContext again.
+            //
+            // It is important that no methods from here (until the user's binding method) is marked with 'async' otherwise that
+            // would again discard the ExecutionContext.
+            //
+            // The ExecutionContext only flows down, so async binding methods cannot directly change it, but even if all binding method
+            // is async the constructor of the binding classes are run in sync, so they should be able to change the ExecutionContext. 
+            // (The binding classes are created as part of the 'bindingDelegate' this method receives.
+
             try
             {
-                if (AsyncMethodHelper.IsAwaitable(bindingDelegate.Method.ReturnType))
-                    return await InvokeBindingDelegateAsync2(bindingDelegate, invokeArgs);
-                return InvokeBindingDelegateSync(bindingDelegate, invokeArgs);
+                return await InvokeInExecutionContext(_executionContext, () => CreateDelegateInvocationTask(bindingDelegate, invokeArgs));
             }
             finally
             {
-#if NETCOREAPP
                 _executionContext = ExecutionContext.Capture();
-#endif
             }
+        }
+
+        private Task<object> InvokeInExecutionContext(ExecutionContext executionContext, Func<Task<object>> callback)
+        {
+            if (executionContext == null)
+                return callback();
+
+            Task<object> result = Task.FromResult((object)null);
+            ExecutionContext.Run(executionContext, _ => { result = callback(); }, null);
+            return result;
+        }
+
+        // this method must not be async because that would discard the ExecutionContext changes during execution!
+        private Task<object> CreateDelegateInvocationTask(Delegate bindingDelegate, object[] invokeArgs)
+        {
+            if (AsyncMethodHelper.IsAwaitable(bindingDelegate.Method.ReturnType))
+                return InvokeBindingDelegateAsync(bindingDelegate, invokeArgs);
+
+            return Task.FromResult(InvokeBindingDelegateSync(bindingDelegate, invokeArgs));
         }
 
         protected virtual object InvokeBindingDelegateSync(Delegate bindingDelegate, object[] invokeArgs)
@@ -37,15 +58,8 @@ namespace TechTalk.SpecFlow.Bindings
             return bindingDelegate.DynamicInvoke(invokeArgs);
         }
 
-        protected virtual async Task<object> InvokeBindingDelegateAsync(Delegate bindingDelegate, object[] invokeArgs)
-        {
-            var result = bindingDelegate.DynamicInvoke(invokeArgs);
-            if (AsyncMethodHelper.IsAwaitableAsTask(result, out var task))
-                await task;
-            return result;
-        }
-
-        protected virtual Task<object> InvokeBindingDelegateAsync2(Delegate bindingDelegate, object[] invokeArgs)
+        // this method must not be async because that would discard the ExecutionContext changes during execution!
+        private Task<object> InvokeBindingDelegateAsync(Delegate bindingDelegate, object[] invokeArgs)
         {
             var result = bindingDelegate.DynamicInvoke(invokeArgs);
             if (AsyncMethodHelper.IsAwaitableAsTask(result, out var task))
