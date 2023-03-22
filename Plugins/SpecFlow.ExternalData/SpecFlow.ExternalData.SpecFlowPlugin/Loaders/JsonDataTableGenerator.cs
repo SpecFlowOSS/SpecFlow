cb8122f645aef08090618b070baa1d533cf1d0f9
Martin Harwig <m.harwig@ellips.com>
2023-03-22 10:53:13 +0100

    Don't use scoped namespace to match codestyle with the rest of the solution

--- Plugins/SpecFlow.ExternalData/SpecFlow.ExternalData.SpecFlowPlugin/Loaders/JsonDataTableGenerator.cs
+++ Plugins/SpecFlow.ExternalData/SpecFlow.ExternalData.SpecFlowPlugin/Loaders/JsonDataTableGenerator.cs
@@ -3,103 +3,102 @@ using System.Linq;
 using Newtonsoft.Json.Linq;
 using SpecFlow.ExternalData.SpecFlowPlugin.DataSources;
 
-namespace SpecFlow.ExternalData.SpecFlowPlugin.Loaders;
-
-public class JsonDataTableGenerator
+namespace SpecFlow.ExternalData.SpecFlowPlugin.Loaders
 {
-    public DataTable FlattenDataSetToDataTable(JObject originalJson, string[] objectPath)
+
+    public class JsonDataTableGenerator
     {
-        try
+        public DataTable FlattenDataSetToDataTable(JObject originalJson, string[] objectPath)
         {
-            var header = GenerateHeader(originalJson, objectPath);
-            var dataTable = new DataTable(header.ToArray());
-            var records = GenerateRecordsFromNestedObjects(originalJson, objectPath, new List<DataRecord>());
-            foreach (var record in records)
-                dataTable.Items.Add(record);
+            try
+            {
+                var header = GenerateHeader(originalJson, objectPath);
+                var dataTable = new DataTable(header.ToArray());
+                var records = GenerateRecordsFromNestedObjects(originalJson, objectPath, new List<DataRecord>());
+                foreach (var record in records) dataTable.Items.Add(record);
 
-            return dataTable;
-        }
-        catch (System.NullReferenceException)
-        {
-            throw new ExternalDataPluginException($"Unable to flatten {objectPath} into DataTable");
-        }
-    }
-
-    private static List<string> GenerateHeader(JObject originalJson, string[] objectPath)
-    {
-        var currentObject = originalJson;
-      
-        List<string> header = new();
-        while (objectPath.Length > 0)
-        {
-            var currentObjectArray = currentObject[objectPath.First()]?.ToObject<JArray>();
-
-            if(currentObjectArray == null)
-                throw new ExternalDataPluginException($"Expected {objectPath.First()} to be in json object");
-
-            objectPath = objectPath.Skip(1).ToArray();
-        
-            if(currentObjectArray.First == null)
-                throw new ExternalDataPluginException("Empty object arrays are not supported");
-
-            currentObject = currentObjectArray.First.ToObject<JObject>();
-            header.AddRange(GetPropertiesExcludingToBeFlattenedArrayObject(currentObject, objectPath).Select(p => p.Name));
+                return dataTable;
+            }
+            catch (System.NullReferenceException)
+            {
+                throw new ExternalDataPluginException($"Unable to flatten {objectPath} into DataTable");
+            }
         }
 
-        return header;
-    }
-
-    private List<DataRecord> GenerateRecordsFromNestedObjects(JObject currentObject, string[] dataSetObjectPath,
-        List<DataRecord> currentRecords)
-    {
-        if (dataSetObjectPath.Length == 0)
-            return currentRecords;
-
-        var objectArrayPropertyName = dataSetObjectPath.First();
-        var remainingObjectArrays = dataSetObjectPath.Skip(1).ToArray();
-
-        var objectArray = currentObject[objectArrayPropertyName]?.ToObject<JArray>();
-        if (objectArray == null) 
-            throw new ExternalDataPluginException($"Expected object array property {objectArrayPropertyName} inside {currentObject}");
-        
-        var result = new List<DataRecord>();
-        foreach (var jObject in objectArray.Select(i => i.ToObject<JObject>()))
+        private static List<string> GenerateHeader(JObject originalJson, string[] objectPath)
         {
-            
-            var objectProperties = GetPropertiesExcludingToBeFlattenedArrayObject(jObject, remainingObjectArrays);
+            var currentObject = originalJson;
 
-            var objectRecord = new DataRecord();
-            foreach (var property in objectProperties)
-                objectRecord.Fields[property.Name] = new DataValue(property.Value);
+            List<string> header = new();
+            while (objectPath.Length > 0)
+            {
+                var currentObjectArray = currentObject[objectPath.First()]?.ToObject<JArray>();
 
-            List<DataRecord> intermediateResult;
-            if (currentRecords.Any())
-                intermediateResult = AppendFieldsToCurrentDataRecords(currentRecords, objectRecord);
-            else
-                intermediateResult = new List<DataRecord> { objectRecord };
+                if (currentObjectArray == null) throw new ExternalDataPluginException($"Expected {objectPath.First()} to be in json object");
 
-            result.AddRange(GenerateRecordsFromNestedObjects(jObject, remainingObjectArrays, intermediateResult));
+                objectPath = objectPath.Skip(1).ToArray();
+
+                if (currentObjectArray.First == null) throw new ExternalDataPluginException("Empty object arrays are not supported");
+
+                currentObject = currentObjectArray.First.ToObject<JObject>();
+                header.AddRange(GetPropertiesExcludingToBeFlattenedArrayObject(currentObject, objectPath).Select(p => p.Name));
+            }
+
+            return header;
         }
 
-        return result;
-    }
-
-    private static List<DataRecord> AppendFieldsToCurrentDataRecords(List<DataRecord> currentRecords, DataRecord objectRecord)
-    {
-        var result = new List<DataRecord>();
-        foreach (var record in currentRecords)
+        private List<DataRecord> GenerateRecordsFromNestedObjects(
+            JObject currentObject,
+            string[] dataSetObjectPath,
+            List<DataRecord> currentRecords)
         {
-            var updatedRecord = new DataRecord(record.Fields);
-            objectRecord.Fields.ToList().ForEach(x => updatedRecord.Fields.Add(x.Key, x.Value));
-            result.Add(updatedRecord);
+            if (dataSetObjectPath.Length == 0) return currentRecords;
+
+            var objectArrayPropertyName = dataSetObjectPath.First();
+            var remainingObjectArrays = dataSetObjectPath.Skip(1).ToArray();
+
+            var objectArray = currentObject[objectArrayPropertyName]?.ToObject<JArray>();
+            if (objectArray == null) throw new ExternalDataPluginException($"Expected object array property {objectArrayPropertyName} inside {currentObject}");
+
+            var result = new List<DataRecord>();
+            foreach (var jObject in objectArray.Select(i => i.ToObject<JObject>()))
+            {
+
+                var objectProperties = GetPropertiesExcludingToBeFlattenedArrayObject(jObject, remainingObjectArrays);
+
+                var objectRecord = new DataRecord();
+                foreach (var property in objectProperties) objectRecord.Fields[property.Name] = new DataValue(property.Value);
+
+                List<DataRecord> intermediateResult;
+                if (currentRecords.Any())
+                    intermediateResult = AppendFieldsToCurrentDataRecords(currentRecords, objectRecord);
+                else
+                    intermediateResult = new List<DataRecord> { objectRecord };
+
+                result.AddRange(GenerateRecordsFromNestedObjects(jObject, remainingObjectArrays, intermediateResult));
+            }
+
+            return result;
         }
 
-        return result;
-    }
-    
-    private static List<JProperty> GetPropertiesExcludingToBeFlattenedArrayObject(JObject childJson,
-        string[] remainingDataSets)
-    {
-        return childJson.Properties().Where(p => p.Name != remainingDataSets.FirstOrDefault()).ToList();
+        private static List<DataRecord> AppendFieldsToCurrentDataRecords(List<DataRecord> currentRecords, DataRecord objectRecord)
+        {
+            var result = new List<DataRecord>();
+            foreach (var record in currentRecords)
+            {
+                var updatedRecord = new DataRecord(record.Fields);
+                objectRecord.Fields.ToList().ForEach(x => updatedRecord.Fields.Add(x.Key, x.Value));
+                result.Add(updatedRecord);
+            }
+
+            return result;
+        }
+
+        private static List<JProperty> GetPropertiesExcludingToBeFlattenedArrayObject(
+            JObject childJson,
+            string[] remainingDataSets)
+        {
+            return childJson.Properties().Where(p => p.Name != remainingDataSets.FirstOrDefault()).ToList();
+        }
     }
 }
--- Plugins/SpecFlow.ExternalData/SpecFlow.ExternalData.SpecFlowPlugin/Loaders/JsonLoader.cs
+++ Plugins/SpecFlow.ExternalData/SpecFlow.ExternalData.SpecFlowPlugin/Loaders/JsonLoader.cs
@@ -4,74 +4,74 @@ using Newtonsoft.Json.Linq;
 using SpecFlow.ExternalData.SpecFlowPlugin.DataSources;
 using Newtonsoft.Json;
 
-namespace SpecFlow.ExternalData.SpecFlowPlugin.Loaders;
-
-public class JsonLoader : FileBasedLoader
+namespace SpecFlow.ExternalData.SpecFlowPlugin.Loaders
 {
-    public JsonLoader() : base("Json", ".json")
+
+    public class JsonLoader : FileBasedLoader
     {
-                
-    }
-
-    protected override DataSource LoadDataSourceFromFilePath(string filePath, string sourceFilePath)
-    {
-        var fileContent = ReadTextFileContent(filePath);
-
-        return LoadJsonDataSource(fileContent, sourceFilePath);
-    }
-
-    private List<string> DetermineDataSets(JObject jsonObject, string dataSetPrepend)
-    {
-        var dataSets = new HashSet<string>();
-        foreach (var array in GetArraysFromObject(jsonObject))
-        { 
-            var firstArrayObject = array.Children().First().ToObject<JObject>();
-            if (firstArrayObject == null)
-              throw new ExternalDataPluginException("Empty arrays are not supported, as they would lead to sparse scenario examples");
-
-            var dataSetPath = string.IsNullOrWhiteSpace(dataSetPrepend) ? array.Path : $"{dataSetPrepend}.{array.Path}";
-                
-            dataSets.Add(dataSetPath);
-
-            foreach (var nestedDataSetPath in DetermineDataSets(firstArrayObject, dataSetPath)) 
-                dataSets.Add(nestedDataSetPath); 
-        }
-
-        return dataSets.ToList();
-    }
-
-    private static IEnumerable<JToken> GetArraysFromObject(JObject jsonObject)
-    {
-        return jsonObject.Properties().Where(p => p.Value.Type == JTokenType.Array).Select(a => a.Value);
-    }
-
-    private DataSource LoadJsonDataSource(string fileContent, string sourceFilePath)
-    {
-        JObject fileJson = ParseJson(fileContent);
-        var dataSets = DetermineDataSets(fileJson, "");
-        var dataSetsRecord = new DataRecord();
-        var jsonDataTableGenerator = new JsonDataTableGenerator();
-        foreach (var dataSetPath in dataSets)
+        public JsonLoader() : base("Json", ".json")
         {
-            var dataTable = jsonDataTableGenerator.FlattenDataSetToDataTable(fileJson, dataSetPath.Split('.'));
-            dataSetsRecord.Fields[dataSetPath] = new DataValue(dataTable);
-        }
-            
-        return new DataSource(dataSetsRecord, dataSets.First());
-    }
 
-    private static JObject ParseJson(string fileContent)
-    {
-        JObject fileJson;
-        try
-        {
-            fileJson = JObject.Parse(fileContent);
-        }
-        catch (JsonReaderException jsonReaderException)
-        {
-            throw new ExternalDataPluginException($"Failed to parse json file: {jsonReaderException}");
         }
 
-        return fileJson;
+        protected override DataSource LoadDataSourceFromFilePath(string filePath, string sourceFilePath)
+        {
+            var fileContent = ReadTextFileContent(filePath);
+
+            return LoadJsonDataSource(fileContent, sourceFilePath);
+        }
+
+        private List<string> DetermineDataSets(JObject jsonObject, string dataSetPrepend)
+        {
+            var dataSets = new HashSet<string>();
+            foreach (var array in GetArraysFromObject(jsonObject))
+            {
+                var firstArrayObject = array.Children().First().ToObject<JObject>();
+                if (firstArrayObject == null) throw new ExternalDataPluginException("Empty arrays are not supported, as they would lead to sparse scenario examples");
+
+                var dataSetPath = string.IsNullOrWhiteSpace(dataSetPrepend) ? array.Path : $"{dataSetPrepend}.{array.Path}";
+
+                dataSets.Add(dataSetPath);
+
+                foreach (var nestedDataSetPath in DetermineDataSets(firstArrayObject, dataSetPath)) dataSets.Add(nestedDataSetPath);
+            }
+
+            return dataSets.ToList();
+        }
+
+        private static IEnumerable<JToken> GetArraysFromObject(JObject jsonObject)
+        {
+            return jsonObject.Properties().Where(p => p.Value.Type == JTokenType.Array).Select(a => a.Value);
+        }
+
+        private DataSource LoadJsonDataSource(string fileContent, string sourceFilePath)
+        {
+            JObject fileJson = ParseJson(fileContent);
+            var dataSets = DetermineDataSets(fileJson, "");
+            var dataSetsRecord = new DataRecord();
+            var jsonDataTableGenerator = new JsonDataTableGenerator();
+            foreach (var dataSetPath in dataSets)
+            {
+                var dataTable = jsonDataTableGenerator.FlattenDataSetToDataTable(fileJson, dataSetPath.Split('.'));
+                dataSetsRecord.Fields[dataSetPath] = new DataValue(dataTable);
+            }
+
+            return new DataSource(dataSetsRecord, dataSets.First());
+        }
+
+        private static JObject ParseJson(string fileContent)
+        {
+            JObject fileJson;
+            try
+            {
+                fileJson = JObject.Parse(fileContent);
+            }
+            catch (JsonReaderException jsonReaderException)
+            {
+                throw new ExternalDataPluginException($"Failed to parse json file: {jsonReaderException}");
+            }
+
+            return fileJson;
+        }
     }
 }
