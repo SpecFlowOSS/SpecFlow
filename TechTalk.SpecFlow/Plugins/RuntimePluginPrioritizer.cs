d4538bed874c2da5be5b6861fc5b02606761b70b
moertlsa <alfred.moertlseder@roche.com>
2023-04-18 10:10:03 +0200

    optimize if pluginList parameter is already a List

--- TechTalk.SpecFlow/Plugins/RuntimePluginPrioritizer.cs
+++ TechTalk.SpecFlow/Plugins/RuntimePluginPrioritizer.cs
@@ -7,6 +7,12 @@ public class RuntimePluginPrioritizer : IRuntimePluginPrioritizer
 {
     public IReadOnlyList<string> Prioritize(IReadOnlyList<string> pluginList)
     {
+        if (pluginList is List<string> list)
+        {
+            list.Sort();
+            return list;
+        }
+
         return pluginList.OrderBy(x => x).ToList();
     }
 }
