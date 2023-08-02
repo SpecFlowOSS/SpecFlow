cda619c871f24d56469013affc6f66efbcaac833
Lehonti Ramos <john@doe>
2023-08-02 23:52:35 +0200

    Fixed compilation errors

--- Plugins/TechTalk.SpecFlow.xUnit.SpecFlowPlugin/XUnitParallelWorkerTracker.cs
+++ Plugins/TechTalk.SpecFlow.xUnit.SpecFlowPlugin/XUnitParallelWorkerTracker.cs
@@ -9,7 +9,7 @@ public class XUnitParallelWorkerTracker
     public static readonly XUnitParallelWorkerTracker Instance = new();
 
     private readonly ConcurrentBag<string> _availableWorkers = new();
-    private readonly int _workerCount = 0;
+    private int _workerCount = 0;
 
     private XUnitParallelWorkerTracker() { }
 
